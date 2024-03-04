045b298318289d7e8371eb40a94e615724386b66
Nino Akut <akutnino@gmail.com>
2024-03-05 02:34:18 +0800

    learned and implemented the controlled elements concept in the From component

--- 05-travel-list/starter/travel-list/src/App.js
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: false },
   { id: 2, description: 'Socks', quantity: 12, packed: false },
@@ -20,21 +22,52 @@ function Logo(props = {}) {
 }
 
 function Form(props = {}) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   const handleSumbit = function (event) {
     event.preventDefault();
+
+    if (description === '') return;
+
+    const newItem = {
+      description,
+      quantity,
+      package: false,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  };
+
+  const handleInput = function (event) {
+    const targetValue = event.target.value;
+    setDescription((currentState) => (currentState = targetValue));
+  };
+
+  const handleSelect = function (event) {
+    const targetValue = event.target.value;
+    setQuantity((currentState) => (currentState = Number(targetValue)));
   };
 
   return (
     <form className="add-form" onSubmit={handleSumbit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select value={quantity} onChange={handleSelect}>
         {Array.from({ length: 10 }, (ele, index) => index + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={handleInput}
+      />
       <button>Add</button>
     </form>
   );
