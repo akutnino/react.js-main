a057cdc5a629504c00ed7b39efdff52898ac0beb
Nino Akut <akutnino@gmail.com>
2024-03-07 02:02:01 +0800

    implemented logic to the stats component through the Derived State concept

--- 05-travel-list/starter/travel-list/src/App.js
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -34,7 +34,7 @@ export default function App(props = {}) {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -59,9 +59,7 @@ function Form(props = {}) {
       package: false,
       id: Date.now(),
     };
-    console.log(newItem);
     onAddItems(newItem);
-
     setDescription('');
     setQuantity(1);
   };
@@ -135,9 +133,32 @@ function Item(props = {}) {
 }
 
 function Stats(props = {}) {
+  const { items } = props;
+  const listIsEmpty = items.length === 0;
+
+  if (listIsEmpty) {
+    return (
+      <p className="stats">
+        <em>Start packin boi!</em>
+      </p>
+    );
+  }
+
+  const totalItems = items.length;
+  const packedItems = items.reduce(
+    (acc, curr) => (curr.package ? (acc += 1) : acc),
+    0
+  );
+  const percentage = Math.round((packedItems / totalItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed (X%)</em>
+      <em>
+        {percentage === 100
+          ? 'You are done packing boi :)'
+          : `👜 You have ${totalItems} items on your list, and you already packed
+        ${packedItems} (${percentage})%`}
+      </em>
     </footer>
   );
 }
