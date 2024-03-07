432ee0832435c5edc325005dc4b5da7b7d026f26
Nino Akut <akutnino@gmail.com>
2024-03-07 20:42:49 +0800

    implemented the sorting functionality on the Packing List component

--- 05-travel-list/starter/travel-list/src/App.js
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -97,11 +97,28 @@ function Form(props = {}) {
 
 function PackingList(props = {}) {
   const { items, onDeleteItem, onToggleItem } = props;
+  const [sortBy, setSortBy] = useState('input');
+  let sortedItems;
+
+  if (sortBy === 'input') sortedItems = items;
+  if (sortBy === 'description')
+    sortedItems = items.toSorted((a, b) =>
+      a.description > b.description ? 1 : -1
+    );
+  if (sortBy === 'packed')
+    sortedItems = items.toSorted(
+      (a, b) => Number(a.package) - Number(b.package)
+    );
+
+  const handleSelectChange = function (event) {
+    const targetValue = event.target.value;
+    setSortBy(targetValue);
+  };
 
   return (
     <div className="list">
       <ul>
-        {items.map((element) => (
+        {sortedItems.map((element) => (
           <Item
             item={element}
             key={element.id}
@@ -110,6 +127,14 @@ function PackingList(props = {}) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={handleSelectChange}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
