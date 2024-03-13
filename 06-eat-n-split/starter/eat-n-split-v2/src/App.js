639303cb3c3ebc53b2567da6ffe12aa6f2d77259
Nino Akut <akutnino@gmail.com>
2024-03-13 21:48:50 +0800

    create the components to statically render the list of friends

--- 06-eat-n-split/starter/eat-n-split-v2/src/App.js
+++ 06-eat-n-split/starter/eat-n-split-v2/src/App.js
@@ -20,5 +20,64 @@ const initialFriends = [
 ];
 
 export default function App(props) {
-	return <div className='app'>Hello</div>;
+	return (
+		<div className='app'>
+			<div className='sidebar'>
+				<FriendsList />
+			</div>
+		</div>
+	);
+}
+
+function FriendsList(props) {
+	const friendsArray = initialFriends;
+
+	return (
+		<ul>
+			{friendsArray.map((friend) => (
+				<FriendItem
+					friend={friend}
+					key={friend.id}
+				/>
+			))}
+		</ul>
+	);
+}
+
+function FriendItem(props) {
+	const { friend } = props;
+
+	return (
+		<li>
+			<img
+				src={friend.image}
+				alt={friend.name}
+			/>
+			<h3>{friend.name}</h3>
+
+			{friend.balance > 0 && (
+				<p className='green'>
+					{friend.name} ows you ${Math.abs(friend.balance)}.
+				</p>
+			)}
+
+			{friend.balance < 0 && (
+				<p className='red'>
+					You owe {friend.name} ${Math.abs(friend.balance)}.
+				</p>
+			)}
+
+			{friend.balance === 0 && (
+				<p className='gray'>You and {friend.name} are currently even.</p>
+			)}
+
+			<Button>Select</Button>
+		</li>
+	);
+}
+
+function Button(props) {
+	const { children } = props;
+
+	return <button className='button'>{children}</button>;
 }
