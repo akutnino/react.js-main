5ef063e784f927ec1a3680d183d3ba022a830d16
Nino Akut <akutnino@gmail.com>
2024-03-16 23:22:08 +0800

    created the static page of the steps-v2 project

--- 04-steps/starter/steps-v2/src/App.js
+++ 04-steps/starter/steps-v2/src/App.js
@@ -1,3 +1,32 @@
+const messages = [
+	'Learn React ⚛️',
+	'Apply for jobs 💼',
+	'Invest your new income 🤑'
+];
+
 export default function App(props) {
-	return <div>Hello React</div>;
+	const stepNumber = 1;
+
+	return (
+		<div className='steps'>
+			<div className='numbers'>
+				<div className={stepNumber >= 1 ? 'active' : ''}>1</div>
+				<div className={stepNumber >= 2 ? 'active' : ''}>2</div>
+				<div className={stepNumber >= 3 ? 'active' : ''}>3</div>
+			</div>
+
+			<p className='message'>
+				Step {stepNumber}: {messages[stepNumber - 1]}
+			</p>
+
+			<div className='buttons'>
+				<button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+					Previous
+				</button>
+				<button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 }
