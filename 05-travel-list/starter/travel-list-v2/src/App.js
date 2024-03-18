6bac0d4c2a33318a04f30d3b994f8efee62bfd7c
Nino Akut <akutnino@gmail.com>
2024-03-19 00:27:48 +0800

    built the static layout of the travel list v2 app

--- 05-travel-list/starter/travel-list-v2/src/App.js
+++ 05-travel-list/starter/travel-list-v2/src/App.js
@@ -1,3 +1,34 @@
 export default function App(props) {
-	return <div className='app'></div>;
+	return (
+		<div className='app'>
+			<Logo />
+			<Form />
+			<PackingList />
+			<Stats />
+		</div>
+	);
+}
+
+function Logo(props) {
+	return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form(props) {
+	return (
+		<div className='add-form'>
+			<h3>What do you need for your trip? 😍</h3>
+		</div>
+	);
+}
+
+function PackingList(props) {
+	return <div className='list'>List</div>;
+}
+
+function Stats(props) {
+	return (
+		<footer className='stats'>
+			<em>👜 You have X items on your list, and you already packed x (X%)</em>
+		</footer>
+	);
 }
