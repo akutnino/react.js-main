6d117a3889b8e5093fe60efe46270274302f29ce
Nino Akut <akutnino@gmail.com>
2024-03-19 00:44:47 +0800

    rendered the PackingItem list component

--- 05-travel-list/starter/travel-list-v2/src/App.js
+++ 05-travel-list/starter/travel-list-v2/src/App.js
@@ -1,3 +1,18 @@
+const initialItems = [
+	{
+		id: 1,
+		description: 'Passports',
+		quantity: 2,
+		packed: false
+	},
+	{
+		id: 2,
+		description: 'Socks',
+		quantity: 12,
+		packed: true
+	}
+];
+
 export default function App(props) {
 	return (
 		<div className='app'>
@@ -16,13 +31,36 @@ function Logo(props) {
 function Form(props) {
 	return (
 		<div className='add-form'>
-			<h3>What do you need for your trip? 😍</h3>
+			<h3>What do you need for your 😍 trip?</h3>
 		</div>
 	);
 }
 
 function PackingList(props) {
-	return <div className='list'>List</div>;
+	return (
+		<div className='list'>
+			<ul>
+				{initialItems.map((item) => (
+					<PackingItem itemObject={item} />
+				))}
+			</ul>
+		</div>
+	);
+}
+
+function PackingItem(props) {
+	const { itemObject } = props;
+	const { id, description, quantity, packed } = itemObject;
+	const packedStyle = { textDecoration: 'line-through' };
+
+	return (
+		<li>
+			<span style={packed ? packedStyle : {}}>
+				{quantity} {description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
 }
 
 function Stats(props) {
