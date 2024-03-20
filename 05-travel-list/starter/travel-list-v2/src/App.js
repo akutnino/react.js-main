751c7c6d40dd5be11b6afbd854083f6c8f41ad05
Nino Akut <akutnino@gmail.com>
2024-03-21 02:54:12 +0800

    calculated the statistics as derived state in the stats component

--- 05-travel-list/starter/travel-list-v2/src/App.js
+++ 05-travel-list/starter/travel-list-v2/src/App.js
@@ -47,7 +47,7 @@ export default function App(props) {
 				onDelete={handleDelete}
 				onPacked={handlePacked}
 			/>
-			<Stats />
+			<Stats itemsArray={itemsArray} />
 		</div>
 	);
 }
@@ -161,9 +161,27 @@ function PackingItem(props) {
 }
 
 function Stats(props) {
+	const { itemsArray } = props;
+	const totalLength = itemsArray.length;
+	const totalPackedItems = itemsArray.reduce(
+		(acc, curr) => (curr.packed ? (acc += 1) : (acc += 0)),
+		0
+	);
+	const packedItemsPercentage = Number(
+		((totalPackedItems / totalLength) * 100).toFixed()
+	);
+
+	// prettier-ignore
 	return (
 		<footer className='stats'>
-			<em>👜 You have X items on your list, and you already packed x (X%)</em>
+			<em>
+				{totalLength === 0 ? `Start Packin` : null}
+				{packedItemsPercentage === 100 ? `Let's Go!` : null}
+				{totalLength > 0 && packedItemsPercentage < 100
+					? `👜 You have ${totalLength} items on your list, and you already packed 
+						${totalPackedItems} (${packedItemsPercentage ? packedItemsPercentage : 0}%)`
+					: null}
+			</em>
 		</footer>
 	);
 }
