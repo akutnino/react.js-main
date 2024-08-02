8651034503333c81276e1f1d2b1ef464df8ce3e0
Nino Akut <akutnino@gmail.com>
2024-08-02 23:08:54 +0800

    created the PageNotFound component and Implemented the first routs

--- 11-worldwise/starter/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise/src/App.jsx
@@ -1,3 +1,31 @@
-export default function App(props) {
-	return <div>WorldWise</div>;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route
+					path='/'
+					element={<Homepage />}
+				/>
+				<Route
+					path='product'
+					element={<Product />}
+				/>
+				<Route
+					path='pricing'
+					element={<Pricing />}
+				/>
+				<Route
+					path='*'
+					element={<PageNotFound />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
