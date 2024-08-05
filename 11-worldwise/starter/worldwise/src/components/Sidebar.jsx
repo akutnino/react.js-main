326a0b0b332ce891ec0926ae0d004fa3dbf0f9d6
Nino Akut <akutnino@gmail.com>
2024-08-06 01:29:28 +0800

    created the Footer.jsx component and its styles

--- 11-worldwise/starter/worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/starter/worldwise/src/components/Sidebar.jsx
@@ -2,20 +2,15 @@ import { Outlet } from 'react-router-dom';
 import styles from '../styles/Sidebar.module.scss';
 import AppNav from './AppNav';
 import Logo from './Logo';
+import Footer from './Footer';
 
 export default function Sidebar() {
 	return (
 		<div className={styles.sidebar}>
 			<Logo />
 			<AppNav />
-
 			<Outlet />
-
-			<footer className={styles.footer}>
-				<p className={styles.copyright}>
-					&copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
-				</p>
-			</footer>
+			<Footer />
 		</div>
 	);
 }
