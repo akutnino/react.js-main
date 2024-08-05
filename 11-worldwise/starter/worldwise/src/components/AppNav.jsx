ba1d4018a8f5e0fd55b4fcd37fc82a1685195406
Nino Akut <akutnino@gmail.com>
2024-08-06 01:46:52 +0800

    implemented Nested Routes and Index Route in AppNav.jsx

--- 11-worldwise/starter/worldwise/src/components/AppNav.jsx
+++ 11-worldwise/starter/worldwise/src/components/AppNav.jsx
@@ -1,7 +1,17 @@
+import { NavLink } from 'react-router-dom';
+import styles from '../styles/AppNav.module.scss';
+
 export default function AppNav() {
 	return (
-		<div>
-			<h1>AppNav</h1>
-		</div>
+		<nav className={styles.nav}>
+			<ul>
+				<li>
+					<NavLink to={'cities'}>Cities</NavLink>
+				</li>
+				<li>
+					<NavLink to={'countries'}>Countries</NavLink>
+				</li>
+			</ul>
+		</nav>
 	);
 }
