e258c4fafb4f75b86796be72ed36fb3aaa2fab6d
Nino Akut <akutnino@gmail.com>
2024-08-15 04:09:04 +0800

    Displaying City Markers on Map

--- 11-worldwise/starter/worldwise/src/components/Map.jsx
+++ 11-worldwise/starter/worldwise/src/components/Map.jsx
@@ -1,10 +1,31 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useCities } from '../contexts/CitiesContext';
 import styles from '../styles/Map.module.scss';
+
+const unicodeToEmoji = (flagUnicode) => {
+	const FIRST_CHARACTER_UNICODE = 127462; // https://www.alt-codes.net/flags
+
+	const flagCodeString = [...flagUnicode]
+		.map((flagChar) => {
+			const unicodeDifference = flagChar.codePointAt() - FIRST_CHARACTER_UNICODE;
+
+			return String.fromCodePoint(unicodeDifference + 'A'.codePointAt());
+		})
+		.join('');
+
+	return (
+		<img
+			src={`https://flagsapi.com/${flagCodeString}/flat/24.png`}
+			alt='Flag Emoji'
+		/>
+	);
+};
 
 export default function Map() {
 	const navigate = useNavigate();
+	const { citiesArray } = useCities();
 	const [mapPosition, setMapPosition] = useState([51.505, -0.09]);
 
 	const handleMapClick = () => {
@@ -18,7 +39,7 @@ export default function Map() {
 		>
 			<MapContainer
 				center={mapPosition}
-				zoom={13}
+				zoom={4}
 				scrollWheelZoom={true}
 				className={styles.map}
 			>
@@ -26,11 +47,25 @@ export default function Map() {
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 				/>
-				<Marker position={mapPosition}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
+				{citiesArray.map((cityObject) => {
+					const {
+						cityName,
+						emoji,
+						id,
+						position: { lat, lng },
+					} = cityObject;
+
+					return (
+						<Marker
+							key={id}
+							position={[lat, lng]}
+						>
+							<Popup>
+								<span>{unicodeToEmoji(emoji)}</span> <span>{cityName}</span>
+							</Popup>
+						</Marker>
+					);
+				})}
 			</MapContainer>
 		</div>
 	);
