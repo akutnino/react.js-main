c1ecf068664fae5115dffe6a216477b016912548
Nino Akut <akutnino@gmail.com>
2024-08-25 01:33:28 +0800

    finished Adding Fake Authentication: Protecting the app Routes

--- 11-worldwise/starter/worldwise/src/pages/ProtectedRoute.jsx
+++ 11-worldwise/starter/worldwise/src/pages/ProtectedRoute.jsx
@@ -1,6 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/FakeAuthContext';
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+ProtectedRoute.propTypes = {
+	children: PropTypes.node,
+};
 
 export default function ProtectedRoute(props) {
 	const { children } = props;
@@ -13,5 +18,5 @@ export default function ProtectedRoute(props) {
 		return () => {};
 	}, [isAuthenticated, navigate]);
 
-	return children;
+	return isAuthenticated ? children : null;
 }
