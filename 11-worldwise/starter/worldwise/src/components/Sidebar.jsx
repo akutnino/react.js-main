5852e9c042b98f9565aca20eee9a0ab08cb0ba3f
Nino Akut <akutnino@gmail.com>
2024-08-30 19:36:33 +0800

    refactor Sidebar.jsx

--- 11-worldwise/starter/worldwise/src/components/Sidebar.jsx
+++ 11-worldwise/starter/worldwise/src/components/Sidebar.jsx
@@ -1,19 +1,15 @@
 import { Outlet } from 'react-router-dom';
-import { useCities } from '../contexts/CitiesContext';
 import AppNav from './AppNav';
 import Logo from './Logo';
 import Footer from './Footer';
-import Spinner from './Spinner';
 import styles from '../styles/Sidebar.module.scss';
 
 export default function Sidebar(props) {
-	const { isLoading } = useCities();
-
 	return (
 		<div className={styles.sidebar}>
 			<Logo />
 			<AppNav />
-			{isLoading ? <Spinner /> : <Outlet />}
+			<Outlet />
 			<Footer />
 		</div>
 	);
