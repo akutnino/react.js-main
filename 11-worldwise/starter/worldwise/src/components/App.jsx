cd305913f0aac5f4fb0dd2ae6f9ad342678bd03e
Nino Akut <akutnino@gmail.com>
2024-09-03 00:03:25 +0800

    Optimizing Bundle Size With Code Splitting and finished the WorldWise App

--- 11-worldwise/starter/worldwise/src/components/App.jsx
+++ 11-worldwise/starter/worldwise/src/components/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { CitiesProvider } from '../contexts/CitiesContext';
 import { AuthProvider } from '../contexts/FakeAuthContext';
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 
 import CityList from './CityList';
 import CountryList from './CountryList';
 import City from './City';
 import Form from './Form';
 import ProtectedRoute from '../pages/ProtectedRoute';
+import SpinnerFullPage from './SpinnerFullPage';
 
 const Homepage = lazy(() => import('../pages/Homepage'));
 const Product = lazy(() => import('../pages/Product'));
@@ -21,62 +22,64 @@ export default function App() {
 		<AuthProvider>
 			<CitiesProvider>
 				<BrowserRouter>
-					<Routes>
-						<Route
-							path='/'
-							element={<Homepage />}
-						/>
-						<Route
-							path='product'
-							element={<Product />}
-						/>
-						<Route
-							path='pricing'
-							element={<Pricing />}
-						/>
-						<Route
-							path='login'
-							element={<Login />}
-						/>
-						<Route
-							path='app'
-							element={
-								<ProtectedRoute>
-									<AppLayout />
-								</ProtectedRoute>
-							}
-						>
+					<Suspense fallback={<SpinnerFullPage />}>
+						<Routes>
 							<Route
-								index
+								path='/'
+								element={<Homepage />}
+							/>
+							<Route
+								path='product'
+								element={<Product />}
+							/>
+							<Route
+								path='pricing'
+								element={<Pricing />}
+							/>
+							<Route
+								path='login'
+								element={<Login />}
+							/>
+							<Route
+								path='app'
 								element={
-									<Navigate
-										replace
-										to='cities'
-									/>
+									<ProtectedRoute>
+										<AppLayout />
+									</ProtectedRoute>
 								}
-							/>
+							>
+								<Route
+									index
+									element={
+										<Navigate
+											replace
+											to='cities'
+										/>
+									}
+								/>
+								<Route
+									path='cities'
+									element={<CityList />}
+								/>
+								<Route
+									path='cities/:id'
+									element={<City />}
+								/>
+								<Route
+									path='countries'
+									element={<CountryList />}
+								/>
+								<Route
+									path='form'
+									element={<Form />}
+								/>
+							</Route>
 							<Route
-								path='cities'
-								element={<CityList />}
+								path='*'
+								element={<PageNotFound />}
 							/>
-							<Route
-								path='cities/:id'
-								element={<City />}
-							/>
-							<Route
-								path='countries'
-								element={<CountryList />}
-							/>
-							<Route
-								path='form'
-								element={<Form />}
-							/>
-						</Route>
-						<Route
-							path='*'
-							element={<PageNotFound />}
-						/>
-					</Routes>
+						</Routes>
+					</Suspense>
 				</BrowserRouter>
 			</CitiesProvider>
 		</AuthProvider>
