e13651d2b91a78de8c6fc6295a0056636cd8d496
Nino Akut <akutnino@gmail.com>
2024-09-07 03:43:06 +0800

    created redux store and tried each action type

--- 15-redux-intro/redux-intro/src/store.js
+++ 15-redux-intro/redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from 'redux';
+
 const INITIAL_STATE = {
 	balance: 0,
 	loanBalance: 0,
@@ -8,32 +10,46 @@ const reducerFunc = (currentState = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case 'account/deposit':
 			return {
-				balance: currentState.balance + action.payload,
 				...currentState,
+				balance: currentState.balance + action.payload,
 			};
 
 		case 'account/withdraw':
 			return {
-				balance: currentState.balance - action.payload,
 				...currentState,
+				balance: currentState.balance - action.payload,
 			};
 
 		case 'account/requestLoan':
 			if (currentState.loanBalance > 0) return currentState;
 			return {
-				loanBalance: action.payload,
 				...currentState,
+				balance: currentState.balance + action.payload.amount,
+				loanBalance: action.payload.amount,
+				loanPurpose: action.payload.purpose,
 			};
 
 		case 'account/payLoan':
 			return {
+				...currentState,
 				balance: currentState.balance - currentState.loanBalance,
 				loanBalance: 0,
 				loanPurpose: '',
-				...currentState,
 			};
 
 		default:
 			return currentState;
 	}
 };
+
+const store = createStore(reducerFunc);
+
+store.dispatch({ type: 'account/deposit', payload: 500 });
+store.dispatch({ type: 'account/withdraw', payload: 400 });
+store.dispatch({
+	type: 'account/requestLoan',
+	payload: { amount: 1000, purpose: 'buy a car' },
+});
+store.dispatch({ type: 'account/payLoan' });
+
+console.log(store.getState());
