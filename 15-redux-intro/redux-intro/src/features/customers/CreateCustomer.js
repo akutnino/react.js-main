896a443f61cca0bf5d95da2deaf4a442d416daa9
Nino Akut <akutnino@gmail.com>
2024-09-10 05:05:37 +0800

    used the useDispatch function to update the store state

--- 15-redux-intro/redux-intro/src/features/customers/CreateCustomer.js
+++ 15-redux-intro/redux-intro/src/features/customers/CreateCustomer.js
@@ -1,10 +1,24 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createCustomer } from './customerSlice';
 
 function Customer() {
 	const [fullName, setFullName] = useState('');
 	const [nationalId, setNationalId] = useState('');
+	const dispatch = useDispatch();
 
-	function handleClick() {}
+	const handleFullNameInput = (event) => {
+		setFullName(event.target.value);
+	};
+
+	const handleNationalIdInput = (event) => {
+		setNationalId(event.target.value);
+	};
+
+	const handleCreateCustomer = () => {
+		if (!fullName || !nationalId) return;
+		dispatch(createCustomer(fullName, nationalId));
+	};
 
 	return (
 		<div>
@@ -14,17 +28,17 @@ function Customer() {
 					<label>Customer full name</label>
 					<input
 						value={fullName}
-						onChange={(e) => setFullName(e.target.value)}
+						onChange={handleFullNameInput}
 					/>
 				</div>
 				<div>
 					<label>National ID</label>
 					<input
 						value={nationalId}
-						onChange={(e) => setNationalId(e.target.value)}
+						onChange={handleNationalIdInput}
 					/>
 				</div>
-				<button onClick={handleClick}>Create new customer</button>
+				<button onClick={handleCreateCustomer}>Create new customer</button>
 			</div>
 		</div>
 	);
