068f223d2d5287327232ce816b433d032215a843
Nino Akut <akutnino@gmail.com>
2024-09-14 05:34:00 +0800

    Creating the Account Slice

--- 15-redux-intro/redux-intro/src/features/accounts/AccountOperations.js
+++ 15-redux-intro/redux-intro/src/features/accounts/AccountOperations.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deposit, payload, requestLoan, withdraw } from './accountSlice';
+import { deposit, payLoan, requestLoan, withdraw } from './accountSlice';
 
 function AccountOperations() {
 	const [depositAmount, setDepositAmount] = useState('');
@@ -39,7 +39,8 @@ function AccountOperations() {
 	const handleDeposit = () => {
 		if (!depositAmount) return;
 
-		dispatch(deposit(depositAmount, currency));
+		// dispatch(deposit(depositAmount, currency));
+		dispatch(deposit(depositAmount));
 		setDepositAmount('');
 		setCurrency('USD');
 	};
@@ -62,7 +63,7 @@ function AccountOperations() {
 	const handlePayLoan = () => {
 		if (!currentLoanBalance) return;
 
-		dispatch(payload());
+		dispatch(payLoan());
 	};
 
 	return (
--- 15-redux-intro/redux-intro/src/features/accounts/accountSlice.js
+++ 15-redux-intro/redux-intro/src/features/accounts/accountSlice.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 const INITIAL_STATE_ACCOUNT = {
 	balance: 0,
 	loanBalance: 0,
@@ -5,88 +7,41 @@ const INITIAL_STATE_ACCOUNT = {
 	isLoading: false,
 };
 
-const accountReducer = (currentState = INITIAL_STATE_ACCOUNT, action) => {
-	switch (action.type) {
-		case 'account/deposit':
-			return {
-				...currentState,
-				balance: currentState.balance + action.payload,
-				isLoading: false,
-			};
+const accountSlice = createSlice({
+	name: 'account',
+	initialState: INITIAL_STATE_ACCOUNT,
+	reducers: {
+		deposit: (currentState, action) => {
+			currentState.balance = currentState.balance + action.payload;
+		},
+		withdraw: (currentState, action) => {
+			currentState.balance = currentState.balance - action.payload;
+		},
+		requestLoan: {
+			prepare: (loanAmount, loanPurpose) => {
+				return { payload: { loanAmount, loanPurpose } };
+			},
+			reducer: (currentState, action) => {
+				if (currentState.loanBalance > 0) return;
+				currentState.balance = currentState.balance + action.payload.loanAmount;
+				currentState.loanBalance = action.payload.loanAmount;
+				currentState.loanPurpose = action.payload.loanPurpose;
+			},
+		},
+		payLoan: (currentState) => {
+			currentState.balance = currentState.balance - currentState.loanBalance;
+			currentState.loanBalance = 0;
+			currentState.loanPurpose = '';
+		},
+		convertingCurrency: (currentState) => {
+			currentState.isLoading = true;
+		},
+	},
+});
 
-		case 'account/withdraw':
-			return {
-				...currentState,
-				balance: currentState.balance - action.payload,
-			};
+const {
+	reducer,
+	actions: { deposit, withdraw, requestLoan, payLoan },
+} = accountSlice;
 
-		case 'account/requestLoan':
-			if (currentState.loanBalance > 0) return currentState;
-			return {
-				...currentState,
-				balance: currentState.balance + action.payload.amount,
-				loanBalance: action.payload.amount,
-				loanPurpose: action.payload.purpose,
-			};
-
-		case 'account/payLoan':
-			return {
-				...currentState,
-				balance: currentState.balance - currentState.loanBalance,
-				loanBalance: 0,
-				loanPurpose: '',
-			};
-
-		case 'account/convertingCurrency':
-			return {
-				...currentState,
-				isLoading: true,
-			};
-
-		default:
-			return currentState;
-	}
-};
-
-export function deposit(amount, currency) {
-	if (currency === 'USD') return { type: 'account/deposit', payload: amount };
-	return async (dispatch, getState) => {
-		try {
-			dispatch({ type: 'account/convertingCurrency' });
-
-			const host = 'api.frankfurter.app';
-			const fetchURL = `https://${host}/latest?amount=${amount}&from=${currency}&to=USD`;
-			const fetchOptions = {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-					Accept: 'application/json',
-				},
-			};
-
-			const response = await fetch(fetchURL, fetchOptions);
-			if (!response.ok) throw new Error('FETCH RESPONSE FAILED');
-
-			const data = await response.json();
-			const convertedAmount = data.rates.USD;
-
-			dispatch({ type: 'account/deposit', payload: convertedAmount });
-		} catch (error) {
-			console.error({ error });
-		}
-	};
-}
-
-export function withdraw(amount) {
-	return { type: 'account/withdraw', payload: amount };
-}
-
-export function requestLoan(amount, purpose) {
-	return { type: 'account/requestLoan', payload: { amount, purpose } };
-}
-
-export function payload() {
-	return { type: 'account/payLoan' };
-}
-
-export default accountReducer;
+export { reducer as default, deposit, withdraw, requestLoan, payLoan };
