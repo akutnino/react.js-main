e292aead9d615b8e489914aef68bc196d4540112
Nino Akut <akutnino@gmail.com>
2024-09-27 14:09:16 +0800

    added null as default return in Username.jsx

--- 16-fast-react-pizza/fast-react-pizza/src/components/user/Username.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/user/Username.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 function Username() {
 	const { userName } = useSelector((store) => store.user);
 
-	if (!userName) return;
+	if (!userName) return null;
 	return <div className='hidden text-sm font-semibold md:block'>{userName}</div>;
 }
 
