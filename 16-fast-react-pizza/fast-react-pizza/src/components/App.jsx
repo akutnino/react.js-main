d05b583d41ffeb37b0c6211639c0ac883fd9d52e
Nino Akut <akutnino@gmail.com>
2024-10-09 01:36:42 +0800

    modified createBrowserRouter from an array to components with createRoutesFromElements

--- 16-fast-react-pizza/fast-react-pizza/src/components/App.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/App.jsx
@@ -1,4 +1,9 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
 import { action as updateOrderAction } from './order/UpdateOrder';
 import Menu, { loader as menuLoader } from './menu/Menu';
 import Order, { loader as orderLoader } from './order/Order';
@@ -8,43 +13,44 @@ import Cart from './cart/Cart';
 import AppLayout from '../interfaces/AppLayout';
 import Error from '../interfaces/Error';
 
-const router = createBrowserRouter([
-	{
-		element: <AppLayout />,
-		errorElement: <Error />,
-		children: [
-			{
-				path: '/',
-				element: <Home />,
-			},
-			{
-				path: '/menu',
-				element: <Menu />,
-				loader: menuLoader,
-				errorElement: <Error />,
-			},
-			{
-				path: '/cart',
-				element: <Cart />,
-			},
-			{
-				path: '/order/new',
-				element: <CreateOrder />,
-				action: createOrderAction,
-			},
-			{
-				path: '/order/:orderId',
-				element: <Order />,
-				loader: orderLoader,
-				action: updateOrderAction,
-				errorElement: <Error />,
-			},
-		],
-	},
-]);
+const myRouter = createBrowserRouter(
+	createRoutesFromElements(
+		<Route
+			element={<AppLayout />}
+			errorElement={<Error />}
+		>
+			<Route
+				path='/'
+				element={<Home />}
+			/>
+			<Route
+				path='/menu'
+				element={<Menu />}
+				loader={menuLoader}
+				errorElement={<Error />}
+			/>
+			<Route
+				path='/cart'
+				element={<Cart />}
+			/>
+			<Route
+				path='/order/new'
+				element={<CreateOrder />}
+				action={createOrderAction}
+			/>
+			<Route
+				path='/order/:orderId'
+				element={<Order />}
+				loader={orderLoader}
+				action={updateOrderAction}
+				errorElement={<Error />}
+			/>
+		</Route>,
+	),
+);
 
 function App() {
-	return <RouterProvider router={router} />;
+	return <RouterProvider router={myRouter} />;
 }
 
 export default App;
