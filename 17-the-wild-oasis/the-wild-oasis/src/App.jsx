f7d1b4309ac68e646d865250c5705bfeca2b8c91
Nino Akut <akutnino@gmail.com>
2024-10-14 16:27:08 +0800

    testing the as prop from styles components

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -14,8 +14,20 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<Heading>The Wild Oasis</Heading>
-				<Button onClick={() => alert('Hello')}>Check In</Button>
+				<Heading type='h1'>The Wild Oasis</Heading>
+				<Heading type='h2'>Check in and out</Heading>
+				<Button
+					type='button'
+					onClick={() => alert('Hello')}
+				>
+					Check In
+				</Button>
+				<Heading
+					as='p'
+					type='h3'
+				>
+					Check in and out
+				</Heading>
 				<Input
 					type='number'
 					placeholder='Number of guests'
--- 17-the-wild-oasis/the-wild-oasis/src/components/common/Heading.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/components/common/Heading.jsx
@@ -1,14 +1,29 @@
 import styled, { css } from 'styled-components';
 
-const test = css`
-	text-align: center;
-`;
-
 const Heading = styled.h1`
-	font-size: 60px;
-	font-weight: 600;
+	${(props) =>
+		props.type === 'h1' &&
+		css`
+			font-size: 3rem;
+			font-weight: 600;
+		`}
+
+	${(props) =>
+		props.type === 'h2' &&
+		css`
+			font-size: 2rem;
+			font-weight: 600;
+		`}
+
+  ${(props) =>
+		props.type === 'h3' &&
+		css`
+			font-size: 5rem;
+			font-weight: 500;
+		`}
+    
+	line-height: 1.4;
 	background-color: yellow;
-	${test}
 `;
 
 export default Heading;
