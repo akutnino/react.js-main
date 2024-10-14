748683966218c72ff23435c796ef85e24533c571
Nino Akut <akutnino@gmail.com>
2024-10-14 17:16:53 +0800

    Building More Reusable Styled Components

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -3,9 +3,9 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './components/common/Button';
 import Input from './components/common/Input';
 import Heading from './components/common/Heading';
+import Row from './components/common/Row';
 
 const StyledApp = styled.div`
-	background-color: orangered;
 	padding: 20px;
 `;
 
@@ -14,24 +14,40 @@ function App() {
 		<>
 			<GlobalStyles />
 			<StyledApp>
-				<Heading type='h1'>The Wild Oasis</Heading>
-				<Heading type='h2'>Check in and out</Heading>
-				<Button
-					type='button'
-					onClick={() => alert('Hello')}
-				>
-					Check In
-				</Button>
-				<Heading
-					as='p'
-					type='h3'
-				>
-					Check in and out
-				</Heading>
-				<Input
-					type='number'
-					placeholder='Number of guests'
-				></Input>
+				<Row type='horizontal'>
+					<Heading type='h1'>The Wild Oasis</Heading>
+
+					<div>
+						<Heading type='h2'>Check in and out</Heading>
+						<Button
+							type='button'
+							onClick={() => alert('Hello')}
+						>
+							Check In
+						</Button>
+						<Button
+							variation='secondary'
+							size='large'
+							type='button'
+							onClick={() => alert('Hello')}
+						>
+							Check Out
+						</Button>
+					</div>
+				</Row>
+
+				<Row>
+					<Heading
+						as='p'
+						type='h3'
+					>
+						Check in and out
+					</Heading>
+					<Input
+						type='number'
+						placeholder='Number of guests'
+					></Input>
+				</Row>
 			</StyledApp>
 		</>
 	);
