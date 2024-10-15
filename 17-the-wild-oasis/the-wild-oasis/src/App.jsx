2b09ffed97bf264fea174a6adf4178e94f62b9cb
Nino Akut <akutnino@gmail.com>
2024-10-15 23:25:58 +0800

    Building the App Layout

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -15,6 +15,7 @@ import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './components/common/AppLayout';
 
 const Router = createBrowserRouter(
 	createRoutesFromElements(
@@ -22,39 +23,42 @@ const Router = createBrowserRouter(
 			path='/'
 			element={<Root />}
 		>
-			<Route
-				index
-				element={
-					<Navigate
-						replace
-						to='dashboard'
-					/>
-				}
-			/>
-			<Route
-				path='dashboard'
-				element={<Dashboard />}
-			/>
-			<Route
-				path='bookings'
-				element={<Bookings />}
-			/>
-			<Route
-				path='cabins'
-				element={<Cabins />}
-			/>
-			<Route
-				path='users'
-				element={<NewUsers />}
-			/>
-			<Route
-				path='settings'
-				element={<Settings />}
-			/>
-			<Route
-				path='account'
-				element={<Account />}
-			/>
+			<Route element={<AppLayout />}>
+				<Route
+					index
+					element={
+						<Navigate
+							replace
+							to='dashboard'
+						/>
+					}
+				/>
+				<Route
+					path='dashboard'
+					element={<Dashboard />}
+				/>
+				<Route
+					path='bookings'
+					element={<Bookings />}
+				/>
+				<Route
+					path='cabins'
+					element={<Cabins />}
+				/>
+				<Route
+					path='users'
+					element={<NewUsers />}
+				/>
+				<Route
+					path='settings'
+					element={<Settings />}
+				/>
+				<Route
+					path='account'
+					element={<Account />}
+				/>
+			</Route>
+
 			<Route
 				path='login'
 				element={<Login />}
