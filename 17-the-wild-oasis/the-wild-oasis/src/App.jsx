8ca161fc77c141c07775bc5cf060fbacf743e8fb
Nino Akut <akutnino@gmail.com>
2024-10-17 01:11:20 +0800

    Setting Up React Query

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -5,6 +5,8 @@ import {
 	Route,
 	RouterProvider,
 } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Root from './pages/Root';
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
@@ -16,6 +18,14 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from './styles/GlobalStyles';
 import AppLayout from './components/common/AppLayout';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 60 * 1000,
+		},
+	},
+});
 
 const Router = createBrowserRouter(
 	createRoutesFromElements(
@@ -73,10 +83,11 @@ const Router = createBrowserRouter(
 
 function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
 			<GlobalStyles />
 			<RouterProvider router={Router} />
-		</>
+		</QueryClientProvider>
 	);
 }
 
