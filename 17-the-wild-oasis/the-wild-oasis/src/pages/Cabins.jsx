e132040aa1992651f87185621f204c391d907590
Nino Akut <akutnino@gmail.com>
2024-10-17 02:18:05 +0800

    Fetching Cabin Data

--- 17-the-wild-oasis/the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/pages/Cabins.jsx
@@ -1,30 +1,19 @@
-import { useEffect } from 'react';
-import { getCabins } from '../services/apiCabins';
 import Heading from '../components/common/Heading';
 import Row from '../components/common/Row';
+import CabinTable from '../components/features/cabins/CabinTable';
 
 function Cabins() {
-	useEffect(() => {
-		const fetchCabins = async () => {
-			try {
-				const data = await getCabins();
-				if (!data) throw new Error('Fetch Request Failed');
-
-				console.log(data);
-			} catch (error) {
-				console.log({ error });
-			}
-		};
-
-		fetchCabins();
-		return () => {};
-	}, []);
-
 	return (
-		<Row type='horizontal'>
-			<Heading as='h1'>All cabins</Heading>
-			<p>TEST</p>
-		</Row>
+		<>
+			<Row type='horizontal'>
+				<Heading as='h1'>All cabins</Heading>
+				<p>Filter / Sort</p>
+			</Row>
+
+			<Row>
+				<CabinTable />
+			</Row>
+		</>
 	);
 }
 
