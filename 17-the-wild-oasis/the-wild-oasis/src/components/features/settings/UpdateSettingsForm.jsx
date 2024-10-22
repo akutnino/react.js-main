5f0fed67dc7af45abd0009dfe265f9594c22ed89
Nino Akut <akutnino@gmail.com>
2024-10-23 04:52:38 +0800

    Fetching Applications Settings

--- 17-the-wild-oasis/the-wild-oasis/src/components/features/settings/UpdateSettingsForm.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/components/features/settings/UpdateSettingsForm.jsx
@@ -1,24 +1,56 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import { useSettings } from './useSettings';
+import Form from '../../common/Form';
+import FormRow from '../../common/FormRow';
+import Input from '../../common/Input';
+import Spinner from '../../common/Spinner';
 
 function UpdateSettingsForm() {
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
-      </FormRow>
-    </Form>
-  );
+	const {
+		isLoading,
+		settings: {
+			minBookingLength,
+			maxBookingLength,
+			maxGuestsPerBooking,
+			breakfastPrice,
+		} = {},
+	} = useSettings();
+
+	if (isLoading) return <Spinner />;
+	return (
+		<Form>
+			<FormRow formLabel='Minimum nights/booking'>
+				<Input
+					type='number'
+					id='min-nights'
+					defaultValue={minBookingLength}
+				/>
+			</FormRow>
+
+			<FormRow formLabel='Maximum nights/booking'>
+				<Input
+					type='number'
+					id='max-nights'
+					defaultValue={maxBookingLength}
+				/>
+			</FormRow>
+
+			<FormRow formLabel='Maximum guests/booking'>
+				<Input
+					type='number'
+					id='max-guests'
+					defaultValue={maxGuestsPerBooking}
+				/>
+			</FormRow>
+
+			<FormRow formLabel='Breakfast price'>
+				<Input
+					type='number'
+					id='breakfast-price'
+					defaultValue={breakfastPrice}
+				/>
+			</FormRow>
+		</Form>
+	);
 }
 
 export default UpdateSettingsForm;
--- 17-the-wild-oasis/the-wild-oasis/src/pages/Settings.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/pages/Settings.jsx
@@ -1,7 +1,14 @@
 import Heading from '../components/common/Heading';
+import Row from '../components/common/Row';
+import UpdateSettingsForm from '../components/features/settings/UpdateSettingsForm';
 
 function Settings() {
-	return <Heading as='h1'>Update hotel settings</Heading>;
+	return (
+		<Row>
+			<Heading as='h1'>Update hotel settings</Heading>
+			<UpdateSettingsForm />
+		</Row>
+	);
 }
 
 export default Settings;
