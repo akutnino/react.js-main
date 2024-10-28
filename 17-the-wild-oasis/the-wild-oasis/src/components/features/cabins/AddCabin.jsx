fb302e9701cbb596c4bcf111fb8f75270a71091c
Nino Akut <akutnino@gmail.com>
2024-10-28 23:56:13 +0800

    removed the table modal and window in the cabins tab

--- 17-the-wild-oasis/the-wild-oasis/src/components/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/components/features/cabins/AddCabin.jsx
@@ -1,27 +1,20 @@
 import Button from '../../common/Button';
 import CreateCabinForm from './CreateCabinForm';
 import Modal from '../../common/Modal';
-import CabinTable from './CabinTable';
 
 function AddCabin() {
 	return (
-		<Modal>
-			<Modal.Open opens='cabin-form'>
-				<Button>Add New Cabin</Button>
-			</Modal.Open>
+		<div>
+			<Modal>
+				<Modal.Open opens='cabin-form'>
+					<Button>Add New Cabin</Button>
+				</Modal.Open>
 
-			<Modal.Window name='cabin-form'>
-				<CreateCabinForm />
-			</Modal.Window>
-
-			<Modal.Open opens='table'>
-				<Button>Show Table</Button>
-			</Modal.Open>
-
-			<Modal.Window name='table'>
-				<CabinTable />
-			</Modal.Window>
-		</Modal>
+				<Modal.Window name='cabin-form'>
+					<CreateCabinForm />
+				</Modal.Window>
+			</Modal>
+		</div>
 	);
 }
 
