e234efc21366120c5a95eca250b23928bd5117a5
Nino Akut <akutnino@gmail.com>
2024-12-20 19:25:24 +0800

    configured vitest coverage config and installed configured package

---
+++ 03-pizza-menu/pizza-menu/src/tests/Menu.test.tsx
@@ -0,0 +1,9 @@
+import { describe, test, expect } from 'vitest';
+
+describe('Menu test suite', () => {
+	test('should', () => {
+		const systemUnderTest = 1;
+		const actualResult = 1;
+		expect(actualResult).toBe(systemUnderTest);
+	});
+});
--- 03-pizza-menu/pizza-menu/vitest.config.ts
+++ 03-pizza-menu/pizza-menu/vitest.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, ViteUserConfig } from 'vitest/config';
 
-const rootDir = '<rootDir>/src';
-const testDir = '<rootDir>/src/test';
-
-export default defineConfig({
+const config: ViteUserConfig = defineConfig({
 	test: {
 		coverage: {
 			enabled: true,
 			all: true,
-			include: [`${rootDir}/**/*.tsx`, `${rootDir}/**/*.ts`],
+			include: ['**/src/**', '**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+			provider: 'v8',
+			reporter: ['text', 'html', 'clover', 'json'],
+			reportOnFailure: true,
 		},
 		watch: false,
 		globals: true,
-		environment: 'jsdom',
-		dir: `${testDir}/**/*.ts`,
+		environment: 'node',
+		dir: './src/tests/',
+		reporters: ['verbose'],
 	},
 });
+
+export default config;
