1e57dc2473b6d8b5329c7f8ce2aef49092309a51
Nino Akut <akutnino@gmail.com>
2025-01-04 23:58:30 +0800

    updated App component test suite

--- 04-steps/steps/src/tests/components/App.test.tsx
+++ 04-steps/steps/src/tests/components/App.test.tsx
@@ -1,5 +1,6 @@
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, fireEvent, render, renderHook } from '@testing-library/react';
 import { afterEach, describe, expect, test } from 'vitest';
+import { useState } from 'react';
 import App from '../../components/App.tsx';
 
 describe('App component test suite', () => {
@@ -10,16 +11,48 @@ describe('App component test suite', () => {
 	test('should render the component correctly', () => {
 		const { getByTestId, container } = render(<App />);
 
-		expect(getByTestId('steps')).toBeInTheDocument();
-		expect(container).toBeTruthy();
+		expect(getByTestId('close')).toEqual(container.firstElementChild);
+		expect(getByTestId('steps')).toEqual(container.lastElementChild);
 	});
 
-	test('should render the correct class name', () => {
+	test('should render the steps element by default', () => {
 		const { getByTestId } = render(<App />);
 
-		expect(getByTestId('numbers')).toBeInTheDocument();
-		expect(getByTestId('numbers').children[0].getAttribute('class')).toBe('active');
-		expect(getByTestId('numbers').children[1].getAttribute('class')).toBe('active');
-		expect(getByTestId('numbers').children[2].getAttribute('class')).toBe('active');
+		expect(getByTestId('close')).toBeInTheDocument();
+		expect(getByTestId('steps')).toBeInTheDocument();
 	});
+
+	test('should update isOpen boolean state when close button is clicked', () => {
+		const { getByTestId, container } = render(<App />);
+		const closeButton = container.firstElementChild as Element;
+
+		const { result } = renderHook(() => {
+			const [isOpen, setIsOpen] = useState<boolean>(true);
+			return { isOpen, setIsOpen };
+		});
+
+		const handleOpenMock = () => {
+			result.current.setIsOpen(!result.current.isOpen);
+		};
+
+		closeButton.addEventListener('click', handleOpenMock);
+
+		fireEvent(
+			getByTestId('close'),
+			new MouseEvent('click', {
+				bubbles: true,
+				cancelable: true,
+			})
+		);
+
+		expect(closeButton).toBeInTheDocument();
+		expect(result.current.isOpen).toBe(false);
+		expect(getByTestId('close')).toEqual(container.lastElementChild);
+	});
+
+	test('should remove the steps component if isOpen boolean state is false', () => {});
+
+	test('should increase step count if next button is clicked', () => {});
+
+	test('should decrease step count if previous button is clicked', () => {});
 });
