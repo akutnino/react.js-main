61f06aea05edc1ef26391b34b669a89782f80557
Nino Akut <akutnino@gmail.com>
2025-01-08 02:30:41 +0800

    removed parts in App component unit test

--- 04-steps/steps/src/tests/components/App.test.tsx
+++ 04-steps/steps/src/tests/components/App.test.tsx
@@ -22,7 +22,7 @@ describe('App component test suite', () => {
 		expect(getByTestId('steps')).toBeInTheDocument();
 	});
 
-	test('should update isOpen boolean state when close button is clicked', () => {
+	test('should update isOpen boolean state and remove the steps component if close button is clicked', () => {
 		const { getByTestId, container } = render(<App />);
 		const closeButton = container.firstElementChild as Element;
 
@@ -48,113 +48,7 @@ describe('App component test suite', () => {
 		expect(closeButton).toBeInTheDocument();
 		expect(result.current.isOpen).toBe(false);
 		expect(closeButton).toEqual(container.lastElementChild);
-	});
-
-	test('should remove the steps component if isOpen boolean state is false', () => {
-		const { getByTestId, container } = render(<App />);
-		const closeButton = container.firstElementChild as HTMLElement;
-
-		const { result } = renderHook(() => {
-			const [isOpen, setIsOpen] = useState<boolean>(true);
-			return { isOpen, setIsOpen };
-		});
-
-		const handleOpenMock = () => {
-			result.current.setIsOpen(!result.current.isOpen);
-		};
-
-		closeButton.addEventListener('click', handleOpenMock);
-
-		fireEvent.click(
-			getByTestId('close'),
-			new MouseEvent('click', {
-				bubbles: true,
-				cancelable: true,
-			})
-		);
-
-		expect(closeButton).toBeInTheDocument();
-		expect(result.current.isOpen).toBe(false);
-		expect(closeButton).toEqual(container.lastElementChild);
 		expect(container.childElementCount).toBe(1);
-	});
-
-	test('should increase step count if next button is clicked', () => {
-		const { getByTestId } = render(<App />);
-		const nextButton = getByTestId('buttons').lastElementChild as HTMLElement;
-		let timesCalled: number = 0;
-
-		const { result } = renderHook(() => {
-			const [step, setStep] = useState<number>(1);
-			return { step, setStep };
-		});
-
-		const handleNextMock = () => {
-			result.current.setStep((currentStep) => (currentStep < 3 ? currentStep + 1 : 3));
-			timesCalled++;
-		};
-
-		nextButton.addEventListener('click', handleNextMock);
-
-		fireEvent.click(
-			nextButton,
-			new MouseEvent('click', {
-				bubbles: true,
-				cancelable: true,
-			})
-		);
-
-		fireEvent.click(
-			nextButton,
-			new MouseEvent('click', {
-				bubbles: true,
-				cancelable: true,
-			})
-		);
-
-		fireEvent.click(
-			nextButton,
-			new MouseEvent('click', {
-				bubbles: true,
-				cancelable: true,
-			})
-		);
-
-		expect(getByTestId('buttons')).toBeInTheDocument();
-		expect(nextButton).toBeInTheDocument();
-		expect(result.current.step).toBe(3);
-		expect(timesCalled).toBe(3);
-	});
-
-	test('should decrease step count if previous button is clicked', () => {
-		const { getByTestId } = render(<App />);
-		const previousButton = getByTestId('buttons').firstElementChild as HTMLElement;
-		let timesCalled: number = 0;
-
-		const { result } = renderHook(() => {
-			const [step, setStep] = useState<number>(3);
-			return { step, setStep };
-		});
-
-		const handlePreviousMock = () => {
-			result.current.setStep((currentStep) => (currentStep > 1 ? currentStep - 1 : 1));
-			timesCalled++;
-		};
-
-		previousButton.addEventListener('click', handlePreviousMock);
-
-		fireEvent.click(
-			previousButton,
-			new MouseEvent('click', {
-				bubbles: true,
-				cancelable: true,
-			})
-		);
-
-		expect(getByTestId('buttons')).toBeInTheDocument();
-		expect(previousButton).toBeInTheDocument();
-		expect(result.current.step).toBe(2);
-		expect(timesCalled).toBe(1);
 	});
 
 	test('should both increase and decrease step count if the buttons are clicked', () => {
