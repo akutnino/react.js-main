fea6c1b795f577cc7a634ebd72b1dd275dd41778
Nino Akut <akutnino@gmail.com>
2025-01-14 05:45:58 +0800

    updated App component added initialItems and Type

--- 05-travel-list/travel-list/src/components/App.tsx
+++ 05-travel-list/travel-list/src/components/App.tsx
@@ -3,12 +3,25 @@ import Logo from './Logo.tsx';
 import PackingList from './PackingList.tsx';
 import Stats from './Stats.tsx';
 
+export type ItemType = {
+	id: number;
+	description: string;
+	quantity: number;
+	packed: boolean;
+};
+
+const initialItems: ItemType[] = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+	{ id: 2, description: 'Socks', quantity: 12, packed: false },
+	{ id: 3, description: 'Charger', quantity: 1, packed: true },
+];
+
 function App() {
 	return (
-		<div>
+		<div className='app'>
 			<Logo />
 			<Form />
-			<PackingList />
+			<PackingList initialItems={initialItems} />
 			<Stats />
 		</div>
 	);
