f45f2123334871cc1ef34834da0db51f95124459
Nino Akut <akutnino@gmail.com>
2025-01-13 22:28:21 +0800

    created vitest.config.ts setup

---
+++ 05-travel-list/travel-list/vitest.config.ts
@@ -0,0 +1,27 @@
+import { configDefaults, defineConfig, ViteUserConfig } from 'vitest/config';
+
+const config: ViteUserConfig = defineConfig({
+	test: {
+		coverage: {
+			enabled: true,
+			all: true,
+			provider: 'v8',
+			reporter: ['text', 'html', 'clover', 'json'],
+			include: ['**/src/***', '**\\/*.{test,spec}.?(c|m)[jt]s?(x)'],
+			exclude: [...configDefaults.include, './src/components/Main.tsx'],
+			reportOnFailure: true,
+		},
+		globals: true,
+		watch: false,
+		environment: 'jsdom',
+		dir: './src/tests',
+		reporters: ['verbose'],
+		setupFiles: [
+			'@testing-library/jest-dom',
+			'@testing-library/react',
+			'@testing-library/user-event',
+		],
+	},
+});
+
+export default config;
