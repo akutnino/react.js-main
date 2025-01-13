191c9df6e241e0d5399664db7d629e60dd2f8064
Nino Akut <akutnino@gmail.com>
2025-01-14 06:31:55 +0800

    updated Form component completed the jsx

--- 05-travel-list/travel-list/src/components/Form.tsx
+++ 05-travel-list/travel-list/src/components/Form.tsx
@@ -1,8 +1,32 @@
+import { type FormEvent } from 'react';
+
 function Form() {
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+	};
+
 	return (
-		<div className='add-form'>
+		<form
+			className='add-form'
+			onSubmit={handleSubmit}
+		>
 			<h3>What do you need for your 😍 trip?</h3>
-		</div>
+			<select>
+				{Array.from(Array(20), (_, index) => index + 1).map((value) => (
+					<option
+						value={value}
+						key={value}
+					>
+						{value}
+					</option>
+				))}
+			</select>
+			<input
+				type='text'
+				placeholder='Item...'
+			/>
+			<button type='button'>Add</button>
+		</form>
 	);
 }
 
