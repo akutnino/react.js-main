d0091a1b7584938390b9e43e3c36fb6e22862e6a
Nino Akut <akutnino@gmail.com>
2025-01-14 05:48:41 +0800

    updated PackingList created list of items

--- 05-travel-list/travel-list/src/components/PackingList.tsx
+++ 05-travel-list/travel-list/src/components/PackingList.tsx
@@ -1,5 +1,19 @@
-function PackingList() {
-	return <div className='list'>LIST</div>;
+import { ItemType } from './App.tsx';
+import PackingItem from './PackingItem.tsx';
+
+function PackingList({ initialItems }: { initialItems: ItemType[] }) {
+	return (
+		<div className='list'>
+			<ul>
+				{initialItems.map((item: ItemType) => (
+					<PackingItem
+						{...item}
+						key={item.id}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default PackingList;
