9c10fa9a960f1245aace759ea973f1097063e4ed
Nino Akut <akutnino@gmail.com>
2025-01-17 23:39:50 +0800

    removed initalItems array in App component

--- 05-travel-list/travel-list/src/components/App.tsx
+++ 05-travel-list/travel-list/src/components/App.tsx
@@ -11,12 +11,6 @@ export type ItemType = {
 	packed: boolean;
 };
 
-// const initialItems: ItemType[] = [
-// 	{ id: 1, description: 'Passports', quantity: 2, packed: false },
-// 	{ id: 2, description: 'Socks', quantity: 12, packed: false },
-// 	{ id: 3, description: 'Charger', quantity: 1, packed: true },
-// ];
-
 function App() {
 	const [items, setItems] = useState<ItemType[]>([]);
 
