09c6acf7d8901d54f0d7cf8b233fef343285e9a7
Nino Akut <akutnino@gmail.com>
2025-01-23 02:18:45 +0800

    updated unit test of Logo component

--- 05-travel-list/travel-list/src/tests/components/Logo.test.tsx
+++ 05-travel-list/travel-list/src/tests/components/Logo.test.tsx
@@ -1,10 +1,15 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import Logo from '../../components/Logo.tsx';
 
 describe('App component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		const { getByTestId } = render(<Logo />);
+
+		expect(getByTestId('logo').innerHTML).toEqual('🌴 Far Away 👜');
+	});
 });
