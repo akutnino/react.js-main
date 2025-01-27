f0da788040ea8746a4110db1bc4c7f5f38ba878e
Nino Akut <akutnino@gmail.com>
2025-01-28 00:37:25 +0800

    updated unit test of Form component

--- 05-travel-list/travel-list/src/tests/components/Form.test.tsx
+++ 05-travel-list/travel-list/src/tests/components/Form.test.tsx
@@ -1,7 +1,7 @@
 import { cleanup, fireEvent, render, renderHook } from '@testing-library/react';
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { useState } from 'react';
 import { afterEach, describe, expect, test } from 'vitest';
-import { type ItemType } from '../../components/App.tsx';
+import App, { type ItemType } from '../../components/App.tsx';
 import Form from '../../components/Form.tsx';
 
 describe('App component test suite', () => {
@@ -74,14 +74,7 @@ describe('App component test suite', () => {
 	});
 
 	test('should exit the submit mock if the description state is falsy ', () => {
-		const { result } = renderHook(() => {
-			const [items, setItems] = useState<ItemType[]>([]);
-			return { items, setItems };
-		});
-
-		const { getByTestId, queryAllByTestId } = render(
-			<Form setItems={result.current.setItems} />
-		);
+		const { getByTestId, queryAllByTestId } = render(<App />);
 		const inputElement = getByTestId('form-input') as HTMLInputElement;
 		const buttonElement = getByTestId('form-submit') as HTMLButtonElement;
 
@@ -98,67 +91,23 @@ describe('App component test suite', () => {
 	});
 
 	test('should render correct changes in DOM when user clicks Add button ', () => {
-		const { result } = renderHook(() => {
-			const [items, setItems] = useState<ItemType[]>([]);
-			const [description, setDescription] = useState<string>('');
-			const [quantity, setQuantity] = useState<number>(1);
+		const { getByTestId, queryAllByTestId } = render(<App />);
+		const inputElement = getByTestId('form-input') as HTMLInputElement;
+		const buttonElement = getByTestId('form-submit') as HTMLButtonElement;
 
-			return {
-				items,
-				description,
-				quantity,
-				setItems,
-				setDescription,
-				setQuantity,
-			};
-		});
-
-		const { getByTestId } = render(<Form setItems={result.current.setItems} />);
-		let inputMockFnIsCalled: number = 0;
-		let submitMockFnIsCalled: number = 0;
-
-		const handleInputMock = (event: ChangeEvent<HTMLInputElement>) => {
-			result.current.setDescription(event.currentTarget.value);
-			inputMockFnIsCalled++;
-		};
-
-		const handleSubmitMock = (event: FormEvent<HTMLFormElement>) => {
-			event.preventDefault();
-
-			if (!result.current.description) return;
-
-			const newItem: ItemType = {
-				id: idNumber,
-				description: result.current.description,
-				quantity: result.current.quantity,
-				packed: false,
-			};
-
-			result.current.setItems((currentItem) => [...currentItem, newItem]);
-			result.current.setDescription('');
-			result.current.setQuantity(1);
-			submitMockFnIsCalled++;
-		};
-
-		// getByTestId('form-input').addEventListener('change', handleInputMock as () => void);
-		// getByTestId('form').addEventListener('submit', handleSubmitMock as () => void);
-
-		fireEvent.change(getByTestId('form-input'), {
+		fireEvent.change(inputElement, {
 			target: { value: 'test' },
 		});
 
 		fireEvent.click(
-			getByTestId('form-submit'),
+			buttonElement,
 			new MouseEvent('click', {
 				bubbles: true,
 				cancelable: true,
 			})
 		);
 
-		expect(result.current.items.length).toEqual(1);
-		expect(result.current.description).toEqual('');
-		expect(result.current.quantity).toEqual(1);
-		// expect(inputMockFnIsCalled).toBe(1);
-		// expect(submitMockFnIsCalled).toBe(1);
+		expect(inputElement.value).toEqual('');
+		expect(queryAllByTestId('packing-item')).toHaveLength(1);
 	});
 });
