1dc1fd4c09ab3012e114aa13136b32441ff33ecf
Nino Akut <akutnino@gmail.com>
2025-02-03 01:25:03 +0800

    added an onClick attribute to the Button component

--- 06-eat-n-split/eat-n-split/src/components/Button.tsx
+++ 06-eat-n-split/eat-n-split/src/components/Button.tsx
@@ -1,10 +1,11 @@
 import { type ReactNode } from 'react';
 
-function Button({ children }: { children: ReactNode }) {
+function Button({ onClick, children }: { onClick: () => void; children: ReactNode }) {
 	return (
 		<button
 			className='button'
 			type='button'
+			onClick={onClick}
 		>
 			{children}
 		</button>
