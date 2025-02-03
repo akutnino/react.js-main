884bd6821c49e50f8281b3cf68f00702d41dac48
Nino Akut <akutnino@gmail.com>
2025-02-03 17:48:49 +0800

    updated FriendsList component

--- 06-eat-n-split/eat-n-split/src/components/FriendsList.tsx
+++ 06-eat-n-split/eat-n-split/src/components/FriendsList.tsx
@@ -1,31 +1,10 @@
 import { type FriendObjectType } from '../types/components/types.ts';
 import FriendListItem from './FriendListItem.tsx';
 
-function FriendsList() {
-	const friendsrray: FriendObjectType[] = [
-		{
-			id: 118836,
-			name: 'Clark',
-			image: 'https://i.pravatar.cc/48?u=118836',
-			balance: -7,
-		},
-		{
-			id: 933372,
-			name: 'Sarah',
-			image: 'https://i.pravatar.cc/48?u=933372',
-			balance: 20,
-		},
-		{
-			id: 499476,
-			name: 'Anthony',
-			image: 'https://i.pravatar.cc/48?u=499476',
-			balance: 0,
-		},
-	];
-
+function FriendsList({ friendsArray }: { friendsArray: FriendObjectType[] }) {
 	return (
 		<ul>
-			{friendsrray.map((friend: FriendObjectType) => (
+			{friendsArray.map((friend: FriendObjectType) => (
 				<FriendListItem
 					{...friend}
 					key={friend.id}
