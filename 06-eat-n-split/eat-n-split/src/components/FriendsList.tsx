ea70a96ab9e04bf603e84d4424ba4defa9cacbea
Nino Akut <akutnino@gmail.com>
2025-02-04 07:59:40 +0800

    updated FriendsList component

--- 06-eat-n-split/eat-n-split/src/components/FriendsList.tsx
+++ 06-eat-n-split/eat-n-split/src/components/FriendsList.tsx
@@ -1,12 +1,26 @@
+import { type Dispatch } from 'react';
 import { type FriendObjectType } from '../types/components/types.ts';
 import FriendListItem from './FriendListItem.tsx';
 
-function FriendsList({ friendsArray }: { friendsArray: FriendObjectType[] }) {
+function FriendsList({
+	friendsArray,
+	selectedFriend,
+	setSelectedFriend,
+	setToggleFormAddFriend,
+}: {
+	friendsArray: FriendObjectType[];
+	selectedFriend: FriendObjectType | null;
+	setSelectedFriend: Dispatch<React.SetStateAction<FriendObjectType | null>>;
+	setToggleFormAddFriend: Dispatch<React.SetStateAction<boolean>>;
+}) {
 	return (
 		<ul>
 			{friendsArray.map((friend: FriendObjectType) => (
 				<FriendListItem
-					{...friend}
+					selectedFriend={selectedFriend}
+					setSelectedFriend={setSelectedFriend}
+					setToggleFormAddFriend={setToggleFormAddFriend}
+					friend={friend}
 					key={friend.id}
 				/>
 			))}
