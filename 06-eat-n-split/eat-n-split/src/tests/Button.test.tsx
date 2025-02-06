22021b0ba2c063a86c67b3a11814d763493cc6a9
Nino Akut <akutnino@gmail.com>
2025-02-07 02:16:35 +0800

    updated unit test of Button component

--- 06-eat-n-split/eat-n-split/src/tests/Button.test.tsx
+++ 06-eat-n-split/eat-n-split/src/tests/Button.test.tsx
@@ -1,10 +1,26 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import { afterEach, describe, expect, test, vi } from 'vitest';
+import Button from '../components/Button.tsx';
 
 describe('Button component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('', () => {});
+	test('should render the component correctly', () => {
+		const handleOnClick = vi.fn();
+		const { getByTestId } = render(<Button onClick={handleOnClick}>Sample</Button>);
+		const buttonElement = getByTestId('button') as HTMLButtonElement;
+
+		fireEvent.click(
+			buttonElement,
+			new MouseEvent('click', {
+				bubbles: true,
+				cancelable: true,
+			})
+		);
+
+		expect(buttonElement).toBeInTheDocument();
+		expect(handleOnClick).toHaveBeenCalledOnce();
+	});
 });
