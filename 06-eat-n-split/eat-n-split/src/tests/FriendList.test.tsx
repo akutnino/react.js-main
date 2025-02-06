fa2172161f0f53d4e9c7834e4186d27401889b57
Nino Akut <akutnino@gmail.com>
2025-02-07 02:46:39 +0800

    updated unit test of FriendsList component

--- 06-eat-n-split/eat-n-split/src/tests/FriendList.test.tsx
+++ 06-eat-n-split/eat-n-split/src/tests/FriendList.test.tsx
@@ -1,10 +1,47 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render, renderHook } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import { useState } from 'react';
+import { type FriendObjectType } from '../types/components/types.ts';
+import FriendsList from '../components/FriendsList.tsx';
 
 describe('FriendList component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('', () => {});
+	test('should render the component correctly', () => {
+		const { result } = renderHook(() => {
+			const [friendsArray, setFriendsArray] = useState<FriendObjectType[]>([
+				{
+					id: 118836,
+					name: 'Clark',
+					image: 'https://i.pravatar.cc/48?u=118836',
+					balance: -7,
+				},
+			]);
+			const [toggleFormAddFriend, setToggleFormAddFriend] = useState<boolean>(true);
+			const [selectedFriend, setSelectedFriend] = useState<FriendObjectType | null>(null);
+
+			return {
+				friendsArray,
+				toggleFormAddFriend,
+				selectedFriend,
+				setFriendsArray,
+				setToggleFormAddFriend,
+				setSelectedFriend,
+			};
+		});
+
+		const { getByTestId } = render(
+			<FriendsList
+				friendsArray={result.current.friendsArray}
+				selectedFriend={result.current.selectedFriend}
+				setSelectedFriend={result.current.setSelectedFriend}
+				setToggleFormAddFriend={result.current.setToggleFormAddFriend}
+			/>
+		);
+
+		expect(getByTestId('friendsList')).toBeInTheDocument();
+		expect(getByTestId('friendsList')).toContainElement(getByTestId('friendListItem'));
+	});
 });
