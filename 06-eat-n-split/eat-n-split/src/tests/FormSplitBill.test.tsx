c5321b94e3a4f383684f86f037ba12f79ae72f67
Nino Akut <akutnino@gmail.com>
2025-02-08 23:59:54 +0800

    updated unit test of FormSplitBill component

--- 06-eat-n-split/eat-n-split/src/tests/FormSplitBill.test.tsx
+++ 06-eat-n-split/eat-n-split/src/tests/FormSplitBill.test.tsx
@@ -1,10 +1,33 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, render, renderHook } from '@testing-library/react';
 import { afterEach, describe, test } from 'vitest';
+import { useState } from 'react';
+import { type FriendObjectType } from '../types/components/types.ts';
+import FormSplitBill from '../components/FormSplitBill.tsx';
 
 describe('FormSplitBill component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('', () => {});
+	test('should render the component correctly', () => {
+		const { result } = renderHook(() => {
+			const [friendsArray, setFriendsArray] = useState<FriendObjectType[]>([]);
+			const [selectedFriend, setSelectedFriend] = useState<FriendObjectType | null>(null);
+
+			return {
+				friendsArray,
+				selectedFriend,
+				setFriendsArray,
+				setSelectedFriend,
+			};
+		});
+
+		const { getByTestId } = render(
+			<FormSplitBill
+				selectedFriend={result.current.selectedFriend}
+				setFriendsArray={result.current.setFriendsArray}
+				setSelectedFriend={result.current.setSelectedFriend}
+			/>
+		);
+	});
 });
