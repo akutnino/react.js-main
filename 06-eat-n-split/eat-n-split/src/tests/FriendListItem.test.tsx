f15ece498faa6276069271e5facce5856f1db1e4
Nino Akut <akutnino@gmail.com>
2025-02-11 04:38:23 +0800

    updated unit test of FriendListItem component

--- 06-eat-n-split/eat-n-split/src/tests/FriendListItem.test.tsx
+++ 06-eat-n-split/eat-n-split/src/tests/FriendListItem.test.tsx
@@ -1,48 +1,14 @@
 import { cleanup, fireEvent, render, renderHook } from '@testing-library/react';
-import { afterEach, describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 import { useState } from 'react';
 import { type FriendObjectType } from '../types/components/types.ts';
 import FriendListItem from '../components/FriendListItem.tsx';
 
 describe('FriendListItem component test suite', () => {
-	afterEach(() => {
-		cleanup();
-	});
+	let friendListItemElement: HTMLLIElement;
+	let selectButtonElement: HTMLButtonElement;
 
-	test('should render the component correctly', () => {
-		const friendDummyObject = {
-			id: 118836,
-			name: 'Clark',
-			image: 'https://i.pravatar.cc/48?u=118836',
-			balance: -7,
-		};
-
-		const { result } = renderHook(() => {
-			const [toggleFormAddFriend, setToggleFormAddFriend] = useState<boolean>(true);
-			const [selectedFriend, setSelectedFriend] = useState<FriendObjectType | null>(null);
-
-			return {
-				toggleFormAddFriend,
-				selectedFriend,
-				setToggleFormAddFriend,
-				setSelectedFriend,
-			};
-		});
-
-		const { getByTestId, getByText } = render(
-			<FriendListItem
-				selectedFriend={result.current.selectedFriend}
-				setSelectedFriend={result.current.setSelectedFriend}
-				setToggleFormAddFriend={result.current.setToggleFormAddFriend}
-				friend={friendDummyObject}
-			/>
-		);
-
-		expect(getByTestId('friendListItem')).toBeInTheDocument();
-		expect(getByTestId('friendListItem')).toContainElement(getByText('Select'));
-	});
-
-	test('should rerender the Select button if it is clicked by the user', () => {
+	beforeEach(() => {
 		const friendDummyObject = {
 			id: 118836,
 			name: 'Clark',
@@ -76,16 +42,29 @@ describe('FriendListItem component test suite', () => {
 			/>
 		);
 
-		const selectButton = getByTestId('button') as HTMLButtonElement;
+		friendListItemElement = getByTestId('friendListItem') as HTMLLIElement;
+		selectButtonElement = getByTestId('button') as HTMLButtonElement;
+	});
 
+	afterEach(() => {
+		cleanup();
+	});
+
+	test('should render the component correctly', () => {
+		expect(friendListItemElement).toBeInTheDocument();
+		expect(friendListItemElement).toContainElement(selectButtonElement);
+		expect(selectButtonElement.innerHTML).toBe('Close');
+	});
+
+	test('should rerender the Select button if it is clicked by the user', () => {
 		fireEvent.click(
-			selectButton,
+			selectButtonElement,
 			new MouseEvent('click', {
 				bubbles: true,
 				cancelable: true,
 			})
 		);
 
-		expect(selectButton.innerHTML).toBe('Close');
+		expect(selectButtonElement.innerHTML).toBe('Close');
 	});
 });
