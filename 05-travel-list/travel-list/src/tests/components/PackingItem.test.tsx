bb8c1b54e01698f548588710395a3d7294d5d03a
Nino Akut <akutnino@gmail.com>
2025-02-12 03:33:46 +0800

    updated unit test of PackingItem component

--- 05-travel-list/travel-list/src/tests/components/PackingItem.test.tsx
+++ 05-travel-list/travel-list/src/tests/components/PackingItem.test.tsx
@@ -1,77 +1,29 @@
 import { cleanup, fireEvent, render, renderHook } from '@testing-library/react';
 import { useState } from 'react';
-import { afterEach, describe, expect, test } from 'vitest';
-import App, { type ItemType } from '../../components/App.tsx';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import { type ItemType } from '../../components/App.tsx';
 import PackingItem from '../../components/PackingItem.tsx';
 
 describe('PackingItem component test suite', () => {
-	afterEach(() => {
-		cleanup();
-	});
+	let packingItemElement: Element | null;
+	let listItemElement: HTMLLIElement;
+	let checkboxInputElement: HTMLInputElement;
+	let itemNameSpanElement: HTMLSpanElement;
+	let deleteButtonElement: HTMLButtonElement;
+	let sutResult: {
+		current: {
+			items: ItemType[];
+			setItems: React.Dispatch<React.SetStateAction<ItemType[]>>;
+		};
+	};
 
-	test('should render the component correctly', () => {
-		const dummyItem = [
+	beforeEach(() => {
+		const dummyItemsArray: ItemType[] = [
 			{
 				id: 1,
 				description: 'Passports',
 				quantity: 2,
 				packed: false,
-			},
-		];
-
-		const { result } = renderHook(() => {
-			const [items, setItems] = useState<ItemType[]>(dummyItem);
-			return { items, setItems };
-		});
-
-		const { getByTestId } = render(
-			<PackingItem
-				item={result.current.items[0]}
-				setItems={result.current.setItems}
-			/>
-		);
-
-		expect(getByTestId('packing-item')).toBeInTheDocument();
-		expect(getByTestId('checkbox-input')).toBeInTheDocument();
-		expect(getByTestId('item-name')).toBeInTheDocument();
-		expect(getByTestId('delete-btn')).toBeInTheDocument();
-	});
-
-	test('should render the component elements in order', () => {
-		const dummyItem = [
-			{
-				id: 1,
-				description: 'Passports',
-				quantity: 2,
-				packed: false,
-			},
-		];
-
-		const { result } = renderHook(() => {
-			const [items, setItems] = useState<ItemType[]>(dummyItem);
-			return { items, setItems };
-		});
-
-		const { getByTestId, container } = render(
-			<PackingItem
-				item={result.current.items[0]}
-				setItems={result.current.setItems}
-			/>
-		);
-
-		const packingItemElement = container.firstElementChild;
-
-		expect(getByTestId('checkbox-input')).toEqual(packingItemElement?.firstElementChild);
-		expect(getByTestId('delete-btn')).toEqual(packingItemElement?.lastElementChild);
-	});
-
-	test('should update list item check state if user clicked the checkbox', () => {
-		const dummyItem = [
-			{
-				id: 1,
-				description: 'Passports',
-				quantity: 2,
-				packed: true,
 			},
 			{
 				id: 2,
@@ -82,62 +34,68 @@ describe('PackingItem component test suite', () => {
 		];
 
 		const { result } = renderHook(() => {
-			const [items, setItems] = useState<ItemType[]>(dummyItem);
+			const [items, setItems] = useState<ItemType[]>(dummyItemsArray);
 			return { items, setItems };
 		});
 
-		const { getByTestId } = render(
+		const { getByTestId, container } = render(
 			<PackingItem
 				item={result.current.items[0]}
 				setItems={result.current.setItems}
 			/>
 		);
-		const checkboxElement = getByTestId('checkbox-input') as HTMLInputElement;
 
+		sutResult = result;
+		packingItemElement = container.firstElementChild;
+		listItemElement = getByTestId('packing-item') as HTMLLIElement;
+		checkboxInputElement = getByTestId('checkbox-input') as HTMLInputElement;
+		itemNameSpanElement = getByTestId('item-name') as HTMLSpanElement;
+		deleteButtonElement = getByTestId('delete-btn') as HTMLButtonElement;
+	});
+
+	afterEach(() => {
+		cleanup();
+	});
+
+	test('should render the component correctly', () => {
+		expect(listItemElement).toBeInTheDocument();
+		expect(checkboxInputElement).toBeInTheDocument();
+		expect(itemNameSpanElement).toBeInTheDocument();
+		expect(deleteButtonElement).toBeInTheDocument();
+	});
+
+	test('should render the component elements in order', () => {
+		expect(checkboxInputElement).toEqual(packingItemElement?.firstElementChild);
+		expect(deleteButtonElement).toEqual(packingItemElement?.lastElementChild);
+	});
+
+	test('should update list item check state if user clicked the checkbox', () => {
 		fireEvent.click(
-			checkboxElement,
+			checkboxInputElement,
 			new MouseEvent('click', {
 				bubbles: true,
 				cancelable: true,
 			})
 		);
 
-		fireEvent.change(checkboxElement, {
+		fireEvent.change(checkboxInputElement, {
 			target: {
 				checked: true,
 			},
 		});
 
-		expect(checkboxElement.checked).toBe(true);
+		expect(checkboxInputElement.checked).toBe(true);
 	});
 
-	test('should delete list item if user clicked the checkbox', () => {
-		const { getByTestId, queryAllByTestId } = render(<App />);
-		const inputElement = getByTestId('form-input') as HTMLInputElement;
-		const submitButton = getByTestId('form-submit') as HTMLButtonElement;
-
-		fireEvent.change(inputElement, {
-			target: { value: 'test' },
-		});
-
+	test('should delete list item if user clicked the delete button', () => {
 		fireEvent.click(
-			submitButton,
+			deleteButtonElement,
 			new MouseEvent('click', {
 				bubbles: true,
 				cancelable: true,
 			})
 		);
 
-		const deleteButton = getByTestId('delete-btn') as HTMLButtonElement;
-
-		fireEvent.click(
-			deleteButton,
-			new MouseEvent('click', {
-				bubbles: true,
-				cancelable: true,
-			})
-		);
-
-		expect(queryAllByTestId('packing-item')).toHaveLength(0);
+		expect(sutResult.current.items).toHaveLength(1);
 	});
 });
