6f60cb9649cb32f9e33b8ee174fbca88e703e02d
Nino Akut <akutnino@gmail.com>
2025-02-13 18:59:39 +0800

    updated unit test of Menu component

--- 03-pizza-menu/pizza-menu/src/tests/components/Menu.test.tsx
+++ 03-pizza-menu/pizza-menu/src/tests/components/Menu.test.tsx
@@ -1,53 +1,57 @@
 import { cleanup, render } from '@testing-library/react';
-import { afterEach, assert, describe, expect, expectTypeOf, test } from 'vitest';
-import Menu, { PizzaDataType } from '../../components/Menu.tsx';
+import {
+	afterEach,
+	assert,
+	beforeEach,
+	describe,
+	expect,
+	expectTypeOf,
+	test,
+} from 'vitest';
+import Menu, { type PizzaDataType } from '../../components/Menu.tsx';
 
 describe('Menu component test suite', () => {
+	let menuElement: HTMLElement;
+	let paragraphElement: HTMLParagraphElement;
+	const dummyPizzaArray: PizzaDataType[] = [
+		{
+			name: 'Pizza Name',
+			ingredients: 'Pizza Ingredients',
+			price: 10,
+			photoName: '/Pizza.jpg',
+			soldOut: true,
+		},
+	];
+
+	beforeEach(() => {
+		const { getByTestId } = render(<Menu />);
+
+		menuElement = getByTestId('menu') as HTMLElement;
+		paragraphElement = getByTestId('pizza-menu') as HTMLParagraphElement;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
 	test('should render Menu component correctly', () => {
-		const dummyPizzaArray: PizzaDataType[] = [
-			{
-				name: 'Pizza Name',
-				ingredients: 'Pizza Ingredients',
-				price: 10,
-				photoName: '/Pizza.jpg',
-				soldOut: true,
-			},
-		];
-		const { getByTestId } = render(<Menu />);
-
-		expect(getByTestId('menu')).toBeInTheDocument();
+		expect(menuElement).toBeInTheDocument();
 		expectTypeOf(dummyPizzaArray).toBeArray();
 	});
 
 	test('should render Menu component fragment if pizzaData.length is not zero', () => {
-		const dummyPizzaArray: PizzaDataType[] = [
-			{
-				name: 'Pizza Name',
-				ingredients: 'Pizza Ingredients',
-				price: 10,
-				photoName: '/Pizza.jpg',
-				soldOut: true,
-			},
-		];
-		const { getByTestId } = render(<Menu />);
-
-		expect(getByTestId('menu')).toBeInTheDocument();
+		expect(menuElement).toBeInTheDocument();
 		expectTypeOf(dummyPizzaArray).toBeArray();
 		expect(dummyPizzaArray.length).toBeGreaterThan(0);
-		expect(getByTestId('pizza-menu')).toBeInTheDocument();
+		expect(paragraphElement).toBeInTheDocument();
 	});
 
 	test('should render Menu component menu message if pizzaData.length is zero', () => {
-		const dummyPizzaArray: PizzaDataType[] = [];
-		const { getByTestId } = render(<Menu {...{ pizzaArray: dummyPizzaArray }} />);
+		const dummyEmptyPizzaArray: PizzaDataType[] = [];
+		const { getByTestId } = render(<Menu {...{ pizzaArray: dummyEmptyPizzaArray }} />);
 
-		expect(getByTestId('menu')).toBeInTheDocument();
-		expectTypeOf(dummyPizzaArray).toBeArray();
-		assert.equal(dummyPizzaArray.length, 0); //learn diffrence between assert and expect.
+		expectTypeOf(dummyEmptyPizzaArray).toBeArray();
+		assert.equal(dummyEmptyPizzaArray.length, 0); //learn diffrence between assert and expect.
 		expect(getByTestId('menu-message')).toBeInTheDocument();
 	});
 });
