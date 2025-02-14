1ace62d70a9d6598710361a3c8c0af38d8c64c04
Nino Akut <akutnino@gmail.com>
2025-02-15 04:12:33 +0800

    created unit test files for components

---
+++ 07-usepopcorn/usepopcorn/src/tests/Main.test.tsx
@@ -0,0 +1,10 @@
+import { cleanup } from '@testing-library/react';
+import { afterEach, describe, test } from 'vitest';
+
+describe('Main component test suite', () => {
+	afterEach(() => {
+		cleanup();
+	});
+
+	test.todo('should render the component without crashing', () => {});
+});
