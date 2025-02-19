bcfe8a364d6c319c2c15e6fafadbcd04366c2694
Nino Akut <akutnino@gmail.com>
2025-02-20 03:34:43 +0800

    updated MainSection component for component composition

--- 07-usepopcorn/usepopcorn/src/components/MainSection.tsx
+++ 07-usepopcorn/usepopcorn/src/components/MainSection.tsx
@@ -1,13 +1,7 @@
 import { type ReactNode } from 'react';
-import WatchedBox from './WatchedBox.tsx';
 
 function MainSection({ children }: { children: ReactNode }) {
-	return (
-		<main className='main'>
-			{children}
-			<WatchedBox />
-		</main>
-	);
+	return <main className='main'>{children}</main>;
 }
 
 export default MainSection;
