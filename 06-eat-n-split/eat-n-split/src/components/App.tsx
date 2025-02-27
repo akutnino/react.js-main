907b0468361e9a748c947147a83dce675c1da515
Nino Akut <akutnino@gmail.com>
2025-02-27 23:50:25 +0800

    updated App component in eat-n-split project by adding key prop to FormSplitBill

--- 06-eat-n-split/eat-n-split/src/components/App.tsx
+++ 06-eat-n-split/eat-n-split/src/components/App.tsx
@@ -68,6 +68,7 @@ function App() {
 					selectedFriend={selectedFriend}
 					setFriendsArray={setFriendsArray}
 					setSelectedFriend={setSelectedFriend}
+					key={selectedFriend?.id}
 				/>
 			)}
 		</div>
