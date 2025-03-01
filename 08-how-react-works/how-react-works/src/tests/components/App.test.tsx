9ce1cf2991c1a014846541c1be855da0c60f4c08
Nino Akut <akutnino@gmail.com>
2025-03-02 04:06:02 +0800

    updated unit test of App component

--- 08-how-react-works/how-react-works/src/tests/components/App.test.tsx
+++ 08-how-react-works/how-react-works/src/tests/components/App.test.tsx
@@ -1,10 +1,30 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import App from '../../components/App.tsx';
 
 describe('App component test suite', () => {
+	let appContainer: Element | null;
+	let appDivElement: HTMLDivElement;
+	let tabbedDivElement: HTMLDivElement;
+
+	beforeEach(() => {
+		const { getByTestId, container } = render(<App />);
+
+		appContainer = container.firstElementChild;
+		appDivElement = getByTestId('container') as HTMLDivElement;
+		tabbedDivElement = getByTestId('tabbed') as HTMLDivElement;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should show all components in the DOM', () => {});
+	test('should show all components in the DOM', () => {
+		expect(appDivElement).toBeInTheDocument();
+		expect(tabbedDivElement).toBeInTheDocument();
+	});
+
+	test('should render the components in order', () => {
+		expect(tabbedDivElement).toEqual(appContainer?.firstElementChild);
+	});
 });
