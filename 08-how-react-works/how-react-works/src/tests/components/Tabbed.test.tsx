d138539d7c13c9ff06dd75613ac381dd4dcc4219
Nino Akut <akutnino@gmail.com>
2025-03-03 23:58:18 +0800

    updated unit test of Tabbed component

--- 08-how-react-works/how-react-works/src/tests/components/Tabbed.test.tsx
+++ 08-how-react-works/how-react-works/src/tests/components/Tabbed.test.tsx
@@ -1,10 +1,13 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import { afterEach, describe, test } from 'vitest';
+import Tabbed from '../../components/Tabbed.tsx';
 
 describe('Tabbed component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		const { getByTestId } = render(<Tabbed />);
+	});
 });
