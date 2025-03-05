e4435ea02e08866fadf7ad66ba89d0a4b4b0adcd
Nino Akut <akutnino@gmail.com>
2025-03-05 23:55:57 +0800

    updated unit test of Tabbed component

--- 08-how-react-works/how-react-works/src/tests/components/Tabbed.test.tsx
+++ 08-how-react-works/how-react-works/src/tests/components/Tabbed.test.tsx
@@ -1,15 +1,29 @@
 import { cleanup, render } from '@testing-library/react';
-import { afterEach, describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
 import Tabbed from '../../components/Tabbed.tsx';
+import { ContentType } from '../../types/components/types.ts';
 
 describe('Tabbed component test suite', () => {
+	let tabbedElement: HTMLDivElement;
+	const dummyContent: ContentType = [
+		{
+			summary: 'React is a library for building UIs',
+			details:
+				'Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+		},
+	];
+
+	beforeEach(() => {
+		const { getByTestId } = render(<Tabbed content={dummyContent} />);
+
+		tabbedElement = getByTestId('tabbed') as HTMLDivElement;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
 	test('should render the component correctly', () => {
-		const { getByTestId } = render(<Tabbed />);
-
-		expect();
+		expect(tabbedElement).toBeInTheDocument();
 	});
 });
