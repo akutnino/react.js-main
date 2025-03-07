a99406f2304f878c81c318b4a37709a35b4a40a3
Nino Akut <akutnino@gmail.com>
2025-03-07 23:51:27 +0800

    updated unit test of Tab component

--- 08-how-react-works/how-react-works/src/tests/components/Tab.test.tsx
+++ 08-how-react-works/how-react-works/src/tests/components/Tab.test.tsx
@@ -1,10 +1,28 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render, renderHook } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import { useState } from 'react';
+import Tab from '../../components/Tab.tsx';
 
 describe('Tab component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		const { result } = renderHook(() => {
+			const [activeTab, setActiveTab] = useState<number>(0);
+			return { activeTab, setActiveTab };
+		});
+
+		const { getByTestId } = render(
+			<Tab
+				num={0}
+				activeTab={result.current.activeTab}
+				setActiveTab={result.current.setActiveTab}
+			/>
+		);
+
+		expect(getByTestId('tab')).toBeInTheDocument();
+		expect(getByTestId('tab').innerHTML).toBe('Tab 1');
+	});
 });
