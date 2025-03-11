972b30b3fe6793ccb19d3eb315270c7988852223
Nino Akut <akutnino@gmail.com>
2025-03-11 23:54:43 +0800

    updated Main component

--- 07-usepopcorn/usepopcorn/src/components/Main.tsx
+++ 07-usepopcorn/usepopcorn/src/components/Main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import '../styles/index.css';
-// import App from './App.tsx';
-import StarRating from './StarRating.tsx';
+import '../styles/index.css';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
 	<StrictMode>
-		{/* <App /> */}
-		<StarRating maxRating={5} />
+		<App />
 	</StrictMode>
 );
