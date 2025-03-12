5eab963f95b9df7acf61bc70261f4330093209e7
Nino Akut <akutnino@gmail.com>
2025-03-13 05:26:00 +0800

    updated NavBar component

--- 07-usepopcorn/usepopcorn/src/components/NavBar.tsx
+++ 07-usepopcorn/usepopcorn/src/components/NavBar.tsx
@@ -1,12 +1,10 @@
 import { type ReactNode } from 'react';
 import Logo from './Logo.tsx';
-import Search from './Search.tsx';
 
 function NavBar({ children }: { children: ReactNode }) {
 	return (
 		<nav className='nav-bar'>
 			<Logo />
-			<Search />
 			{children}
 		</nav>
 	);
