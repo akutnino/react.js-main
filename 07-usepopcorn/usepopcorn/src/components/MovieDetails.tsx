a4fb88d0a3fb57b9641197c4c1c28691a2f59fce
Nino Akut <akutnino@gmail.com>
2025-03-29 01:33:28 +0800

    updated MovieDetails component by adding useEffect to fetch movie details

--- 07-usepopcorn/usepopcorn/src/components/MovieDetails.tsx
+++ 07-usepopcorn/usepopcorn/src/components/MovieDetails.tsx
@@ -1,4 +1,13 @@
-import { type Dispatch } from 'react';
+import { useEffect, useState, type Dispatch } from 'react';
+import {
+	SuccessFetchMoviesDetailsResponseType,
+	type FetchMovieDetailsResponseType,
+} from '../types/components/types.ts';
+import StarRating from './StarRating.tsx';
+import Loader from './Loader.tsx';
+import ErrorMessage from './ErrorMessage.tsx';
+
+const KEY = '3494c38';
 
 function MovieDetails({
 	selectedMovieID,
@@ -7,19 +16,95 @@ function MovieDetails({
 	selectedMovieID: string | null;
 	setSelectedMovieID: Dispatch<React.SetStateAction<string | null>>;
 }) {
+	const [movie, setMovie] = useState<SuccessFetchMoviesDetailsResponseType | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [fetchErrorMessage, setFetchErrorMessage] = useState<string>('');
+
 	const handleCloseDetails = () => {
 		setSelectedMovieID(null);
 	};
 
+	useEffect(() => {
+		const fetchMovieDetails = async () => {
+			try {
+				setFetchErrorMessage('');
+				setIsLoading(true);
+
+				const fetchURL: RequestInfo = `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedMovieID}`;
+				const fetchOptions: RequestInit = {
+					method: 'GET',
+				};
+
+				const response: Response = await fetch(fetchURL, fetchOptions);
+				if (!response.ok) throw new Error('Fetch Response Failed');
+
+				const data: FetchMovieDetailsResponseType = await response.json();
+				if (data.Response === 'False') throw new Error(data.Error);
+
+				setMovie(data);
+			} catch (error) {
+				if (error instanceof Error) {
+					setFetchErrorMessage(error.message);
+				}
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		if (selectedMovieID === null) return;
+		fetchMovieDetails();
+
+		return () => setMovie(null);
+	}, [selectedMovieID]);
+
 	return (
 		<div className='details'>
-			<button
-				className='btn-back'
-				onClick={handleCloseDetails}
-			>
-				&larr;
-			</button>
-			{selectedMovieID}
+			{isLoading && <Loader />}
+
+			{fetchErrorMessage && <ErrorMessage message={fetchErrorMessage} />}
+
+			{!fetchErrorMessage && !isLoading && (
+				<>
+					<header>
+						<button
+							className='btn-back'
+							onClick={handleCloseDetails}
+						>
+							&larr;
+						</button>
+						<img
+							src={movie?.Poster}
+							alt={`Poster of: ${movie?.Title}`}
+							srcSet={movie?.Poster}
+						/>
+						<div className='details-overview'>
+							<h2>{movie?.Title}</h2>
+							<p>
+								{movie?.Released} &bull; {movie?.Runtime}
+							</p>
+							<p>{movie?.Genre}</p>
+							<p>
+								<span>⭐</span> {movie?.imdbRating} IMDB Rating
+							</p>
+						</div>
+					</header>
+
+					<section>
+						<div className='rating'>
+							<StarRating
+								maxRating={10}
+								size={24}
+							/>
+						</div>
+
+						<p>
+							<em>{movie?.Plot}</em>
+						</p>
+						<p>Cast: {movie?.Actors}</p>
+						<p>Directed by: {movie?.Director}</p>
+					</section>
+				</>
+			)}
 		</div>
 	);
 }
