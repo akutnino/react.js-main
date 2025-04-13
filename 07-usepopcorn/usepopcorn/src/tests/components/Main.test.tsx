e20614ebe046100d7e6005081d88d56dff28a5b8
Nino Akut <akutnino@gmail.com>
2025-04-13 23:04:03 +0800

    updated unit test of Main component

--- 07-usepopcorn/usepopcorn/src/tests/components/Main.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/Main.test.tsx
@@ -1,10 +1,18 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import App from '../../components/App.tsx';
 
 describe('Main component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component without crashing', () => {});
+	test('should render the component without crashing', () => {
+		const { getByTestId, container } = render(<App />);
+
+		expect(getByTestId('navbar')).toBeInTheDocument();
+		expect(getByTestId('mainSection')).toBeInTheDocument();
+		expect(container).toContainElement(getByTestId('navbar'));
+		expect(container).toContainElement(getByTestId('mainSection'));
+	});
 });
