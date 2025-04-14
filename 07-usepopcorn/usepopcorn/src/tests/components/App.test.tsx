1231d9a017f423eb44985b5429a9366a2ddc660b
Nino Akut <akutnino@gmail.com>
2025-04-14 23:43:05 +0800

    updated unit test of App component

--- 07-usepopcorn/usepopcorn/src/tests/components/App.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/App.test.tsx
@@ -1,10 +1,25 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import App from '../../components/App.tsx';
 
 describe('App component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should show all components in the DOM', () => {});
+	test('should show all components in the DOM', () => {
+		const { getByTestId, queryAllByTestId } = render(<App />);
+		const boxElement: HTMLElement[] = queryAllByTestId('box');
+
+		expect(getByTestId('logo')).toBeInTheDocument();
+		expect(getByTestId('search')).toBeInTheDocument();
+		expect(getByTestId('numResults')).toBeInTheDocument();
+		expect(getByTestId('mainSection').children[0]).toBe(boxElement[0]);
+		expect(getByTestId('mainSection').children[1]).toBe(boxElement[1]);
+		expect(getByTestId('movieList')).toBeInTheDocument();
+		expect(getByTestId('watchedSummary')).toBeInTheDocument();
+		expect(getByTestId('watchedMoviesList')).toBeInTheDocument();
+	});
+
+	test('should render the user input correctly if user search for movie', () => {});
 });
