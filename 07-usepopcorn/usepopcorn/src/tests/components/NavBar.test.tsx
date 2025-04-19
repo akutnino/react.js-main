91441217f61ebff43357ef51382544accf14e238
Nino Akut <akutnino@gmail.com>
2025-04-19 23:58:52 +0800

    updated unit test of NavBar component

--- 07-usepopcorn/usepopcorn/src/tests/components/NavBar.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/NavBar.test.tsx
@@ -1,10 +1,30 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import NavBar from '../../components/NavBar.tsx';
 
 describe('NavBar component test suite', () => {
+	let navBarElement: HTMLElement;
+	let logoElement: HTMLDivElement;
+
+	beforeEach(() => {
+		const { getByTestId } = render(
+			<NavBar>
+				<div data-testid='children'>children</div>
+			</NavBar>
+		);
+
+		navBarElement = getByTestId('navbar') as HTMLElement;
+		logoElement = getByTestId('logo') as HTMLDivElement;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		expect(navBarElement).toBeInTheDocument();
+		expect(logoElement).toBeInTheDocument();
+	});
+
+	test.todo('should render the components in order', () => {});
 });
