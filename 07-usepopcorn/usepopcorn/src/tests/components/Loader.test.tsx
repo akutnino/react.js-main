391487c8c35d3d186ba384b36f83f6bbefe5a4fe
Nino Akut <akutnino@gmail.com>
2025-04-23 05:00:43 +0800

    updated unit test of Loader component

--- 07-usepopcorn/usepopcorn/src/tests/components/Loader.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/Loader.test.tsx
@@ -1,5 +1,17 @@
-import { describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import Loader from '../../components/Loader.tsx';
 
 describe('Loader component test suite', () => {
-	test.todo('should render the component correctly', () => {});
+	afterEach(() => {
+		cleanup();
+	});
+
+	test('should render the component correctly', () => {
+		const { getByTestId } = render(<Loader />);
+		const loaderElement = getByTestId('loader') as HTMLParagraphElement;
+
+		expect(loaderElement).toBeInTheDocument();
+		expect(loaderElement.textContent).toBe('Loading...');
+	});
 });
