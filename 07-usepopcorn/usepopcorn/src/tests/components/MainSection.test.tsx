6bed189c8c40d539741d0d1f044860783684a8d4
Nino Akut <akutnino@gmail.com>
2025-04-23 03:02:28 +0800

    updated unit test of MainSection component

--- 07-usepopcorn/usepopcorn/src/tests/components/MainSection.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/MainSection.test.tsx
@@ -1,10 +1,33 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import MainSection from '../../components/MainSection.tsx';
 
 describe('MainSection component test suite', () => {
+	let mainSectionElement: HTMLElement;
+	let childrenElement: HTMLDivElement;
+
+	beforeEach(() => {
+		const { getByTestId } = render(
+			<MainSection>
+				<div data-testid='children'>children</div>
+			</MainSection>
+		);
+
+		mainSectionElement = getByTestId('mainSection') as HTMLElement;
+		childrenElement = getByTestId('children') as HTMLDivElement;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		expect(mainSectionElement).toBeInTheDocument();
+		expect(childrenElement).toBeInTheDocument();
+	});
+
+	test('should check that the children is mounted by default', () => {
+		expect(mainSectionElement.firstElementChild).toBe(childrenElement);
+		expect(childrenElement.parentElement).toBe(mainSectionElement);
+	});
 });
