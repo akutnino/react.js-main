f856f532f122b473a474bea2c378c9658f06a7f1
Nino Akut <akutnino@gmail.com>
2025-04-24 23:44:49 +0800

    updated unit test of Search component

--- 07-usepopcorn/usepopcorn/src/tests/components/Search.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/Search.test.tsx
@@ -1,12 +1,59 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, fireEvent, render, renderHook } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import { useState } from 'react';
+import Search from '../../components/Search.tsx';
 
 describe('Search component test suite', () => {
+	let searchElement: HTMLInputElement;
+
+	beforeEach(() => {
+		const { result } = renderHook(() => {
+			const [query, setQuery] = useState<string>('');
+			return { query, setQuery };
+		});
+
+		const { getByTestId } = render(
+			<Search
+				query={result.current.query}
+				setQuery={result.current.setQuery}
+			/>
+		);
+
+		searchElement = getByTestId('search') as HTMLInputElement;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		expect(searchElement).toBeInTheDocument();
+	});
 
-	test.todo('should render the correct value if the user enters an input', () => {});
+	test('should render the correct value if the user enters an input', () => {
+		// Simulating the user event to the searchElement.\
+		fireEvent.click(
+			searchElement,
+			new MouseEvent('click', {
+				bubbles: true,
+				cancelable: true,
+			})
+		);
+
+		fireEvent.change(searchElement, {
+			target: { value: 'testing' },
+		});
+
+		expect(searchElement.value).toEqual('testing');
+	});
+
+	test('should check if the component is focused if the user clicks the Enter key', () => {
+		// Simulating the user event to the searchElement.\
+		fireEvent.keyDown(searchElement, {
+			key: 'Enter',
+			code: 'Enter',
+		});
+
+		expect(document.activeElement === searchElement).toBe(true);
+	});
 });
