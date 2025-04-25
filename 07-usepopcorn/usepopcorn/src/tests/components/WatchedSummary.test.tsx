034d68316f0ac3d0bcfe144d9601ebeb8a300f88
Nino Akut <akutnino@gmail.com>
2025-04-25 23:53:22 +0800

    updated unit test of WatchedSummary component

--- 07-usepopcorn/usepopcorn/src/tests/components/WatchedSummary.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/WatchedSummary.test.tsx
@@ -1,10 +1,34 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, beforeEach, describe, test } from 'vitest';
+import WatchedSummary from '../../components/WatchedSummary.tsx';
 
 describe('WatchedSummary component test suite', () => {
+	let watchedSummaryElement: HTMLDivElement;
+	let totalWatchedElement: HTMLSpanElement;
+	let avgImdbRatingElement: HTMLSpanElement;
+	let avgRuntimeElement: HTMLSpanElement;
+
+	beforeEach(() => {
+		const { getByTestId } = render(<WatchedSummary watched={[]} />);
+
+		watchedSummaryElement = getByTestId('watchedSummary') as HTMLDivElement;
+		totalWatchedElement = getByTestId('totalWatched') as HTMLSpanElement;
+		avgImdbRatingElement = getByTestId('avgImdbRating') as HTMLSpanElement;
+		avgRuntimeElement = getByTestId('avgRuntime') as HTMLSpanElement;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
 	test.todo('should render the component correctly', () => {});
+
+	test.todo('should render watched summary stats as zero by default', () => {
+		// totalWatched, avgImdbRating, avgUserRating, and avgRuntime
+	});
+
+	// prettier-ignore
+	test.todo('should render the correct watched summary stats if watchedMoviesList is not empty', () => {
+		// totalWatched, avgImdbRating, avgUserRating, and avgRuntime
+	});
 });
