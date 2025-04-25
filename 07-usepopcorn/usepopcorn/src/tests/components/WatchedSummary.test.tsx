66f6e86d430c7fee59e7881277f23b2bc96730dd
Nino Akut <akutnino@gmail.com>
2025-04-26 02:28:24 +0800

    updated unit test of WatchedSummary component

--- 07-usepopcorn/usepopcorn/src/tests/components/WatchedSummary.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/WatchedSummary.test.tsx
@@ -1,19 +1,29 @@
 import { cleanup, render } from '@testing-library/react';
-import { afterEach, beforeEach, describe, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import { type WatchedMovieDataType } from '../../types/components/types.ts';
 import WatchedSummary from '../../components/WatchedSummary.tsx';
 
 describe('WatchedSummary component test suite', () => {
 	let watchedSummaryElement: HTMLDivElement;
 	let totalWatchedElement: HTMLSpanElement;
 	let avgImdbRatingElement: HTMLSpanElement;
+	let avgUserRatingElement: HTMLSpanElement;
 	let avgRuntimeElement: HTMLSpanElement;
 
 	beforeEach(() => {
+		const currentTestName = expect.getState().currentTestName as string;
+		const exemptedTestNames: string[] = [
+			'WatchedSummary component test suite > should render the correct watched summary stats if watchedMoviesList is not empty',
+		];
+
+		if (exemptedTestNames.includes(currentTestName)) return;
+
 		const { getByTestId } = render(<WatchedSummary watched={[]} />);
 
 		watchedSummaryElement = getByTestId('watchedSummary') as HTMLDivElement;
 		totalWatchedElement = getByTestId('totalWatched') as HTMLSpanElement;
 		avgImdbRatingElement = getByTestId('avgImdbRating') as HTMLSpanElement;
+		avgUserRatingElement = getByTestId('avgUserRating') as HTMLSpanElement;
 		avgRuntimeElement = getByTestId('avgRuntime') as HTMLSpanElement;
 	});
 
@@ -21,14 +31,44 @@ describe('WatchedSummary component test suite', () => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
-
-	test.todo('should render watched summary stats as zero by default', () => {
-		// totalWatched, avgImdbRating, avgUserRating, and avgRuntime
+	test('should render the component correctly', () => {
+		expect(watchedSummaryElement).toBeInTheDocument();
+		expect(totalWatchedElement).toBeInTheDocument();
+		expect(avgImdbRatingElement).toBeInTheDocument();
+		expect(avgUserRatingElement).toBeInTheDocument();
+		expect(avgRuntimeElement).toBeInTheDocument();
 	});
 
-	// prettier-ignore
-	test.todo('should render the correct watched summary stats if watchedMoviesList is not empty', () => {
-		// totalWatched, avgImdbRating, avgUserRating, and avgRuntime
+	test('should render watched summary stats as zero by default', () => {
+		expect(totalWatchedElement.innerHTML.split(' ').at(0)).toBe('0');
+		expect(avgImdbRatingElement.innerHTML).toBe('0.00');
+		expect(avgUserRatingElement.innerHTML).toBe('0.00');
+		expect(avgRuntimeElement.innerHTML.split(' ').at(0)).toBe('0');
+	});
+
+	test('should render the correct watched summary stats if watchedMoviesList is not empty', () => {
+		const dummyWatchedArray: WatchedMovieDataType[] = [
+			{
+				Poster: 'test_Poster',
+				Title: 'test_Title',
+				Year: 'test_Year',
+				countRatingDecisions: 1,
+				imdbID: 'test_imdbID',
+				imdbRating: 5,
+				runtime: 100,
+				userRating: 10,
+			},
+		];
+		const { getByTestId } = render(<WatchedSummary watched={dummyWatchedArray} />);
+
+		const totalWatchedElement = getByTestId('totalWatched') as HTMLSpanElement;
+		const avgImdbRatingElement = getByTestId('avgImdbRating') as HTMLSpanElement;
+		const avgUserRatingElement = getByTestId('avgUserRating') as HTMLSpanElement;
+		const avgRuntimeElement = getByTestId('avgRuntime') as HTMLSpanElement;
+
+		expect(totalWatchedElement.innerHTML.split(' ').at(0)).toBe('1');
+		expect(avgImdbRatingElement.innerHTML).toBe('5.00');
+		expect(avgUserRatingElement.innerHTML).toBe('10.00');
+		expect(avgRuntimeElement.innerHTML.split(' ').at(0)).toBe('100');
 	});
 });
