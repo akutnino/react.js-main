c48b6feb16b57e8737dea9ac171fa60ff0796c72
Nino Akut <akutnino@gmail.com>
2025-04-26 04:41:50 +0800

    updated unit test of MovieList component

--- 07-usepopcorn/usepopcorn/src/tests/components/MovieList.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/MovieList.test.tsx
@@ -1,10 +1,76 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render, renderHook } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import { type MovieDataType } from '../../types/components/types.ts';
+import { useMovies } from '../../hooks/useMovies.ts';
+import MovieList from '../../components/MovieList.tsx';
 
 describe('MovieList component test suite', () => {
+	const EMPTY_MOVIES_ARRAY: MovieDataType[] = [];
+	let movieListElement: HTMLUListElement;
+	let childrenElementArray: HTMLElement[];
+
+	beforeEach(() => {
+		const currentTestName = expect.getState().currentTestName as string;
+		const exemptedTestName: string[] = [
+			'MovieList component test suite > should render the MovieListItem if the movies array is not empty',
+		];
+
+		if (exemptedTestName.includes(currentTestName)) return;
+
+		const { result } = renderHook(() => {
+			const { setSelectedMovieID } = useMovies('');
+			return { setSelectedMovieID };
+		});
+
+		const { getByTestId, queryAllByTestId } = render(
+			<MovieList
+				movies={EMPTY_MOVIES_ARRAY}
+				setSelectedMovieID={result.current.setSelectedMovieID}
+			/>
+		);
+
+		movieListElement = getByTestId('movieList') as HTMLUListElement;
+		childrenElementArray = queryAllByTestId('movieListItem') as HTMLElement[];
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		expect(movieListElement).toBeInTheDocument();
+	});
+
+	test('should check that MovieList is empty after initial render', () => {
+		expect(childrenElementArray).toHaveLength(0);
+	});
+
+	test('should render the MovieListItem if the movies array is not empty', () => {
+		const dummyMoviesArray: MovieDataType[] = [
+			{
+				imdbID: 'testID',
+				Poster: 'testPoster',
+				Title: 'testTitle',
+				Year: 'testYear',
+			},
+		];
+
+		const { result } = renderHook(() => {
+			const { setSelectedMovieID } = useMovies('');
+			return { setSelectedMovieID };
+		});
+
+		const { getByTestId, queryAllByTestId } = render(
+			<MovieList
+				movies={dummyMoviesArray}
+				setSelectedMovieID={result.current.setSelectedMovieID}
+			/>
+		);
+
+		const childrenElementArray = queryAllByTestId('movieListItem') as HTMLElement[];
+		const movieListItemElement = getByTestId('movieListItem') as HTMLLIElement;
+
+		expect(movieListItemElement).toBeInTheDocument();
+		expect(childrenElementArray).toHaveLength(1);
+	});
 });
