c3f562b8c7374eb15d73f95ce441045d0eff7903
Nino Akut <akutnino@gmail.com>
2025-04-28 23:48:00 +0800

    updated unit test of MovieListItem component

--- 07-usepopcorn/usepopcorn/src/tests/components/MovieListItem.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/MovieListItem.test.tsx
@@ -1,10 +1,39 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render, renderHook } from '@testing-library/react';
+import { afterEach, beforeEach, describe, test } from 'vitest';
+import { type MovieDataType } from '../../types/components/types.ts';
+import { useMovies } from '../../hooks/useMovies.ts';
+import MovieListItem from '../../components/MovieListItem.tsx';
 
 describe('MovieListItem component test suite', () => {
+	beforeEach(() => {
+		const dummyMovieObject: MovieDataType = {
+			Poster: 'test_Poster',
+			Title: 'test_Title',
+			Type: 'movie',
+			Year: 'test_Year',
+			imdbID: 'test_imdbID',
+		};
+
+		const { result } = renderHook(() => {
+			const { setSelectedMovieID } = useMovies('');
+			return { setSelectedMovieID };
+		});
+
+		const { getByTestId } = render(
+			<MovieListItem
+				movie={dummyMovieObject}
+				setSelectedMovieID={result.current.setSelectedMovieID}
+			/>
+		);
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
 	test.todo('should render the component correctly', () => {});
+
+	test.todo('should render the correct movie detials', () => {});
+
+	test.todo('should set SelectedMovieID if the user clicks on MovieListItem', () => {});
 });
