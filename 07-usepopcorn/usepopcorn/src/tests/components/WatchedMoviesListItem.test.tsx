ceea5c1f0d99231dcce5fcd3892dcae7058ca2be
Nino Akut <akutnino@gmail.com>
2025-04-30 23:49:35 +0800

    updated unit test of WatchedMoviesListItem component

--- 07-usepopcorn/usepopcorn/src/tests/components/WatchedMoviesListItem.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/WatchedMoviesListItem.test.tsx
@@ -1,10 +1,16 @@
 import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { afterEach, beforeEach, describe, test } from 'vitest';
 
 describe('WatchedMoviesListItem component test suite', () => {
+	beforeEach(() => {});
+
 	afterEach(() => {
 		cleanup();
 	});
 
 	test.todo('should render the component correctly', () => {});
+
+	test.todo('should render the correct movie detials', () => {});
+
+	test.todo('should delete WatchedMoviesListItem if the user clicked the delete button');
 });
