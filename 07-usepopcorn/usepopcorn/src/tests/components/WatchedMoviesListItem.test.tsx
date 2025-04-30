2fd91bef9076de335286cc90551b98d8fd9f5b5b
Nino Akut <akutnino@gmail.com>
2025-05-01 04:58:57 +0800

    updated unit test of WatchedMoviesListItem component

--- 07-usepopcorn/usepopcorn/src/tests/components/WatchedMoviesListItem.test.tsx
+++ 07-usepopcorn/usepopcorn/src/tests/components/WatchedMoviesListItem.test.tsx
@@ -1,16 +1,62 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, beforeEach, describe, test } from 'vitest';
+import { cleanup, render, renderHook } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import { useLocalStorageState } from '../../hooks/useLocalStorageState.ts';
+import { type WatchedMovieDataType } from '../../types/components/types.ts';
+import WatchedMoviesListItem from '../../components/WatchedMoviesListItem.tsx';
 
 describe('WatchedMoviesListItem component test suite', () => {
-	beforeEach(() => {});
+	let watchedMoviesListItemElement: HTMLLIElement;
+	let imdbRatingElement: HTMLSpanElement;
+	let userRatingElement: HTMLSpanElement;
+	let runtimeElement: HTMLSpanElement;
+	let deleteButtonElement: HTMLButtonElement;
+
+	beforeEach(() => {
+		const dummyWatchedObject: WatchedMovieDataType = {
+			Poster: 'test_Poster',
+			Title: 'test_Title',
+			Year: 'test_Year',
+			countRatingDecisions: 1,
+			imdbID: 'test_imdbID',
+			imdbRating: 5,
+			runtime: 100,
+			userRating: 10,
+		};
+
+		const { result } = renderHook(() => {
+			const { setWatched } = useLocalStorageState([], '');
+			return { setWatched };
+		});
+
+		const { getByTestId, getByText } = render(
+			<WatchedMoviesListItem
+				movie={dummyWatchedObject}
+				setWatched={result.current.setWatched}
+			/>
+		);
+
+		watchedMoviesListItemElement = getByTestId('watchedMoviesListItem') as HTMLLIElement;
+		imdbRatingElement = getByTestId('imdbRating') as HTMLSpanElement;
+		userRatingElement = getByTestId('userRating') as HTMLSpanElement;
+		runtimeElement = getByTestId('runtime') as HTMLSpanElement;
+		deleteButtonElement = getByText('X') as HTMLButtonElement;
+	});
 
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		expect(watchedMoviesListItemElement).toBeInTheDocument();
+		expect(imdbRatingElement).toBeInTheDocument();
+		expect(userRatingElement).toBeInTheDocument();
+		expect(runtimeElement).toBeInTheDocument();
+		expect(deleteButtonElement).toBeInTheDocument();
+	});
 
-	test.todo('should render the correct movie detials', () => {});
-
-	test.todo('should delete WatchedMoviesListItem if the user clicked the delete button');
+	test('should render the correct movie detials', () => {
+		expect(imdbRatingElement.innerHTML).toBe('5');
+		expect(userRatingElement.innerHTML).toBe('10');
+		expect(runtimeElement.innerHTML).toBe('100 min');
+	});
 });
