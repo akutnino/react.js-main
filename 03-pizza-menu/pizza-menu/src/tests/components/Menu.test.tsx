7fad88f217590fcef82372fc4a9bf3e9b6dcc7f8
Nino Akut <akutnino@gmail.com>
2025-05-02 04:28:22 +0800

    updated unit test of Menu component

--- 03-pizza-menu/pizza-menu/src/tests/components/Menu.test.tsx
+++ 03-pizza-menu/pizza-menu/src/tests/components/Menu.test.tsx
@@ -11,9 +11,7 @@ import {
 import Menu, { type PizzaDataType } from '../../components/Menu.tsx';
 
 describe('Menu component test suite', () => {
-	let menuElement: HTMLElement;
-	let paragraphElement: HTMLParagraphElement;
-	const dummyPizzaArray: PizzaDataType[] = [
+	const DUMMY_PIZZA_ARRAY: PizzaDataType[] = [
 		{
 			name: 'Pizza Name',
 			ingredients: 'Pizza Ingredients',
@@ -22,6 +20,9 @@ describe('Menu component test suite', () => {
 			soldOut: true,
 		},
 	];
+
+	let menuElement: HTMLElement;
+	let paragraphElement: HTMLParagraphElement;
 
 	beforeEach(() => {
 		const { getByTestId } = render(<Menu />);
@@ -36,22 +37,22 @@ describe('Menu component test suite', () => {
 
 	test('should render Menu component correctly', () => {
 		expect(menuElement).toBeInTheDocument();
-		expectTypeOf(dummyPizzaArray).toBeArray();
+		expectTypeOf(DUMMY_PIZZA_ARRAY).toBeArray();
 	});
 
 	test('should render Menu component fragment if pizzaData.length is not zero', () => {
 		expect(menuElement).toBeInTheDocument();
-		expectTypeOf(dummyPizzaArray).toBeArray();
-		expect(dummyPizzaArray.length).toBeGreaterThan(0);
+		expectTypeOf(DUMMY_PIZZA_ARRAY).toBeArray();
+		expect(DUMMY_PIZZA_ARRAY.length).toBeGreaterThan(0);
 		expect(paragraphElement).toBeInTheDocument();
 	});
 
 	test('should render Menu component menu message if pizzaData.length is zero', () => {
-		const dummyEmptyPizzaArray: PizzaDataType[] = [];
-		const { getByTestId } = render(<Menu {...{ pizzaArray: dummyEmptyPizzaArray }} />);
+		const DUMMY_EMPTY_PIZZA_ARRAY: PizzaDataType[] = [];
+		const { getByTestId } = render(<Menu {...{ pizzaArray: DUMMY_EMPTY_PIZZA_ARRAY }} />);
 
-		expectTypeOf(dummyEmptyPizzaArray).toBeArray();
-		assert.equal(dummyEmptyPizzaArray.length, 0); //learn diffrence between assert and expect.
+		expectTypeOf(DUMMY_EMPTY_PIZZA_ARRAY).toBeArray();
+		assert.equal(DUMMY_EMPTY_PIZZA_ARRAY.length, 0); //learn diffrence between assert and expect.
 		expect(getByTestId('menu-message')).toBeInTheDocument();
 	});
 });
