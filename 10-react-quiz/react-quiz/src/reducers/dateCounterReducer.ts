538f2fa50c15b301696fcceb4f99b799e282e255
Nino Akut <akutnino@gmail.com>
2025-05-31 04:11:02 +0800

    created dateCounterReducer.ts for DateCounter component

---
+++ 10-react-quiz/react-quiz/src/reducers/dateCounterReducer.ts
@@ -0,0 +1,51 @@
+import type {
+	ActionType,
+	InitalDateCounterStateType,
+} from '../types/components/types.ts';
+
+export const INITIAL_DATE_COUNTER_STATE: InitalDateCounterStateType = {
+	count: 0,
+	step: 1,
+};
+
+function dateCounterReducer(
+	currentState: InitalDateCounterStateType,
+	action: ActionType
+): InitalDateCounterStateType {
+	console.log(currentState, action);
+
+	switch (action.type) {
+		case 'setCount': {
+			return {
+				...currentState,
+				count: action.payload,
+			};
+		}
+		case 'setStep': {
+			return {
+				...currentState,
+				step: action.payload,
+			};
+		}
+		case 'decreasing': {
+			return {
+				...currentState,
+				count: currentState.count - currentState.step,
+			};
+		}
+		case 'increasing': {
+			return {
+				...currentState,
+				count: currentState.count + currentState.step,
+			};
+		}
+		case 'reset': {
+			return INITIAL_DATE_COUNTER_STATE;
+		}
+		default: {
+			return currentState;
+		}
+	}
+}
+
+export { dateCounterReducer };
