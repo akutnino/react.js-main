ba83da121b6e11ea2881123ae4b9a07dfb48f57b
Nino Akut <akutnino@gmail.com>
2025-06-02 04:16:34 +0800

    updated App component

--- 10-react-quiz/react-quiz/src/components/App.tsx
+++ 10-react-quiz/react-quiz/src/components/App.tsx
@@ -1,7 +1,47 @@
+import { useEffect, useReducer } from 'react';
+import {
+	INITIAL_REACT_QUIZ_STATE,
+	reactQuizReducer,
+} from '../reducers/reactQuizReducer.ts';
+import type { QuestionsArrayType } from '../types/components/types.ts';
 import Header from './Header.tsx';
 import MainContent from './MainContent.tsx';
 
 function App() {
+	const [state, dispatch] = useReducer(reactQuizReducer, INITIAL_REACT_QUIZ_STATE);
+
+	useEffect(() => {
+		const fetchQuestions = async () => {
+			try {
+				const fetchURL: string = `http://localhost:8000/questions`;
+				const fetchOptions: RequestInit = {
+					method: 'GET',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json',
+					},
+				};
+
+				const response: Response = await fetch(fetchURL, fetchOptions);
+				if (!response.ok) throw new Error('Failed Fetch Request');
+
+				const data: QuestionsArrayType = await response.json();
+				dispatch({
+					type: 'dataReceived',
+					payload: data,
+				});
+			} catch (error) {
+				if (error instanceof Error) {
+					dispatch({
+						type: 'dataFailed',
+						payload: error.message,
+					});
+				}
+			}
+		};
+		fetchQuestions();
+	}, []);
+
 	return (
 		<div className='app'>
 			<Header />
