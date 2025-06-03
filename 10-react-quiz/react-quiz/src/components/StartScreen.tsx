6bda9733901876dc7b4c89f58fec1e4823ecf406
Nino Akut <akutnino@gmail.com>
2025-06-03 23:51:40 +0800

    created StartScreen component

---
+++ 10-react-quiz/react-quiz/src/components/StartScreen.tsx
@@ -0,0 +1,16 @@
+function StartScreen({ totalQuestions }: { totalQuestions: number }) {
+	return (
+		<div className='start'>
+			<h2>Welcome to The React Quiz!</h2>
+			<h3>{totalQuestions} questions to test your React mastery</h3>
+			<button
+				type='button'
+				className='btn btn-ui'
+			>
+				Let's Start
+			</button>
+		</div>
+	);
+}
+
+export default StartScreen;
