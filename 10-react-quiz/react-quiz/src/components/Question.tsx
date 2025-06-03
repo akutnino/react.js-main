4f7cdec67ae34a0b4d305cb3c29e33f417e6b84d
Nino Akut <akutnino@gmail.com>
2025-06-04 04:39:38 +0800

    updated Question component by passing down props

--- 10-react-quiz/react-quiz/src/components/Question.tsx
+++ 10-react-quiz/react-quiz/src/components/Question.tsx
@@ -1,13 +1,31 @@
-import type { QuestionType } from '../types/components/types.ts';
+import type { ActionDispatch } from 'react';
+import type {
+	QuestionType,
+	ReactQuizActionType,
+	UserAnswerIndexType,
+} from '../types/components/types.ts';
 import Options from './Options.tsx';
 
-function Question({ question }: { question: QuestionType }) {
-	const { question: quizQuestion, options }: QuestionType = question;
+function Question({
+	question,
+	userAnswerIndex,
+	dispatch,
+}: {
+	question: QuestionType;
+	userAnswerIndex: UserAnswerIndexType;
+	dispatch: ActionDispatch<[action: ReactQuizActionType]>;
+}) {
+	const { question: quizQuestion, options, correctOption }: QuestionType = question;
 
 	return (
 		<div>
 			<h4>{quizQuestion}</h4>
-			<Options options={options} />
+			<Options
+				options={options}
+				correctOption={correctOption}
+				userAnswerIndex={userAnswerIndex}
+				dispatch={dispatch}
+			/>
 		</div>
 	);
 }
