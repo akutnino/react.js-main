195c0d58c635e9251bacbe9f1948e5efbe059ea7
Nino Akut <akutnino@gmail.com>
2025-06-04 01:54:06 +0800

    updated Question component by adding Options component

--- 10-react-quiz/react-quiz/src/components/Question.tsx
+++ 10-react-quiz/react-quiz/src/components/Question.tsx
@@ -1,5 +1,15 @@
-function Question() {
-	return <div>Question</div>;
+import type { QuestionType } from '../types/components/types.ts';
+import Options from './Options.tsx';
+
+function Question({ question }: { question: QuestionType }) {
+	const { question: quizQuestion, options }: QuestionType = question;
+
+	return (
+		<div>
+			<h4>{quizQuestion}</h4>
+			<Options options={options} />
+		</div>
+	);
 }
 
 export default Question;
