de6714baa17a4b5037cfeca6027084f65418bd1d
Nino Akut <akutnino@gmail.com>
2025-06-04 04:38:19 +0800

    updated Options component by adding an event handler

--- 10-react-quiz/react-quiz/src/components/Options.tsx
+++ 10-react-quiz/react-quiz/src/components/Options.tsx
@@ -1,11 +1,40 @@
-function Options({ options }: { options: string[] }) {
+import type { ActionDispatch } from 'react';
+import type {
+	ReactQuizActionType,
+	UserAnswerIndexType,
+} from '../types/components/types.ts';
+
+function Options({
+	options,
+	correctOption,
+	userAnswerIndex,
+	dispatch,
+}: {
+	options: string[];
+	correctOption: number;
+	userAnswerIndex: UserAnswerIndexType;
+	dispatch: ActionDispatch<[action: ReactQuizActionType]>;
+}) {
+	const handleAnswer = (answerIndex: number) => {
+		return () => {
+			dispatch({
+				type: 'userAnswer',
+				payload: answerIndex,
+			});
+		};
+	};
+
 	return (
 		<div className='options'>
 			{options.map((option: string, index: number) => {
 				return (
 					<button
 						type='button'
-						className='btn btn-option'
+						className={`btn btn-option ${index === userAnswerIndex ? 'answer' : ''} 
+            ${index === correctOption && userAnswerIndex && 'correct'} 
+            ${index !== correctOption && userAnswerIndex && 'wrong'}`}
+						disabled={userAnswerIndex !== null}
+						onClick={handleAnswer(index)}
 						key={index}
 					>
 						{option}
