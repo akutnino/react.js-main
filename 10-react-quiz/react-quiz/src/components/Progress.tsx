82fa754173a24f9cd597d293c0f99eb537dd1a1a
Nino Akut <akutnino@gmail.com>
2025-06-06 08:14:36 +0800

    created Progress component

---
+++ 10-react-quiz/react-quiz/src/components/Progress.tsx
@@ -0,0 +1,37 @@
+import type { UserAnswerIndexType } from '../types/components/types.ts';
+
+function Progress({
+	questionIndex,
+	totalQuestions,
+	userTotalPoints,
+	maxPossiblePoints,
+	userAnswerIndex,
+}: {
+	questionIndex: number;
+	totalQuestions: number;
+	userTotalPoints: number;
+	maxPossiblePoints: number;
+	userAnswerIndex: UserAnswerIndexType;
+}) {
+	const progressValue: number =
+		userAnswerIndex !== null ? questionIndex + 1 : questionIndex;
+
+	return (
+		<header className='progress'>
+			<progress
+				max={totalQuestions}
+				value={progressValue}
+			/>
+
+			<p>
+				Question <strong>{questionIndex + 1}</strong> / {totalQuestions}
+			</p>
+
+			<p>
+				<strong>{userTotalPoints}</strong> / {maxPossiblePoints}
+			</p>
+		</header>
+	);
+}
+
+export default Progress;
