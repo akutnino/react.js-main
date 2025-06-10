f66f8c7fbf37d5ab23ee8a4a9248962472f4250c
Nino Akut <akutnino@gmail.com>
2025-06-11 04:02:14 +0800

    updated App component

--- 10-react-quiz/react-quiz/src/components/App.tsx
+++ 10-react-quiz/react-quiz/src/components/App.tsx
@@ -123,7 +123,6 @@ function App() {
 						userTotalPoints={userTotalPoints}
 						maxPossiblePoints={maxPossiblePoints}
 						userHighscore={userHighscore}
-						questions={questions}
 						dispatch={dispatch}
 					/>
 				)}
