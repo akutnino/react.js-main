60531850b4be1bbe6dee7aea6f72cc925b6858d0
Nino Akut <akutnino@gmail.com>
2025-06-16 23:54:48 +0800

    created Timer component

---
+++ 10-react-quiz/react-quiz/src/components/Timer.tsx
@@ -0,0 +1,47 @@
+import { useEffect, type ActionDispatch } from 'react';
+import type {
+	ReactQuizActionType,
+	UserAnswerIndexType,
+} from '../types/components/types.ts';
+
+function Timer({
+	quizTimeRemaining,
+	dispatch,
+}: {
+	quizTimeRemaining: UserAnswerIndexType;
+	dispatch: ActionDispatch<[action: ReactQuizActionType]>;
+}) {
+	const MINUTES: number = Math.floor(quizTimeRemaining! / 60);
+	const SECONDS: number = quizTimeRemaining! % 60;
+
+	useEffect(() => {
+		const TIMER_ID = setInterval(() => {
+			if (quizTimeRemaining === 0) {
+				clearInterval(TIMER_ID);
+				dispatch({
+					type: 'finishQuiz',
+				});
+			} else {
+				dispatch({
+					type: 'updateQuizTime',
+				});
+			}
+		}, 1000);
+
+		return () => {
+			clearInterval(TIMER_ID);
+		};
+	}, [quizTimeRemaining, dispatch]);
+
+	// prettier-ignore
+	return (
+		<div className='timer'>
+			{MINUTES < 10 ? '0' : ''}
+			{MINUTES}:
+      {SECONDS < 10 ? '0' : ''}
+			{SECONDS}
+		</div>
+	);
+}
+
+export default Timer;
