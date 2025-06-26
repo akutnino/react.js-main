54fe8890a49cee2b219b54e173fac79e3cae8d00
Nino Akut <akutnino@gmail.com>
2025-06-27 02:51:21 +0800

    updated ErrorMessage component by adding data-testid attribute

--- 10-react-quiz/react-quiz/src/components/ErrorMessage.tsx
+++ 10-react-quiz/react-quiz/src/components/ErrorMessage.tsx
@@ -1,7 +1,10 @@
 // renamed Error to ErrorMessage to avoid name clash with ErrorConstructor instance.
 function ErrorMessage() {
 	return (
-		<p className='error'>
+		<p
+			className='error'
+			data-testid='error'
+		>
 			<span>💥</span> There was an error fecthing questions.
 		</p>
 	);
