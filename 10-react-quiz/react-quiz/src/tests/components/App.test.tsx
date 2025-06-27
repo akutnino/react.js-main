98b30bd5fab1e2a73df657d68760e28b4049c4c2
Nino Akut <akutnino@gmail.com>
2025-06-28 03:07:14 +0800

    updated unit test of App component

--- 10-react-quiz/react-quiz/src/tests/components/App.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/App.test.tsx
@@ -7,4 +7,16 @@ describe('App component test suite', () => {
 	});
 
 	test.todo('should show all components in the DOM', () => {});
+
+	test.todo('should render the Loader component if the status is loading', () => {});
+	test.todo('should render the StartScreen component if the status is ready', () => {});
+	test.todo(
+		'should render the Progress, Question, and Footer components if the status is active',
+		() => {}
+	);
+	test.todo(
+		'should render the FinishScreen component if the status is finished',
+		() => {}
+	);
+	test.todo('should render the ErrorMessage component if the status is error', () => {});
 });
