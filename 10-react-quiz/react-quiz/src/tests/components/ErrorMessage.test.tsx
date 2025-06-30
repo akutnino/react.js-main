fca6f8f0e17ecc35bd65ac424c33dae90acb2086
Nino Akut <akutnino@gmail.com>
2025-06-30 23:42:07 +0800

    updated unit test of ErrorMessage component

--- 10-react-quiz/react-quiz/src/tests/components/ErrorMessage.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/ErrorMessage.test.tsx
@@ -1,10 +1,18 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import ErrorMessage from '../../components/ErrorMessage.tsx';
 
 describe('ErrorMessage component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		const { getByTestId } = render(<ErrorMessage />);
+
+		expect(getByTestId('error')).toBeInTheDocument();
+		expect(getByTestId('error').textContent).toBe(
+			'💥 There was an error fecthing questions.'
+		);
+	});
 });
