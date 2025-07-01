0ba9d9b81dd9f0948f476a0d8b6d6878172c8ca4
Nino Akut <akutnino@gmail.com>
2025-07-02 04:20:00 +0800

    updated MainContent component by adding data-testid attribute

--- 10-react-quiz/react-quiz/src/components/MainContent.tsx
+++ 10-react-quiz/react-quiz/src/components/MainContent.tsx
@@ -1,7 +1,14 @@
 import type { ReactNode } from 'react';
 
 function MainContent({ children }: { children: ReactNode }) {
-	return <main className='main'>{children}</main>;
+	return (
+		<main
+			className='main'
+			data-testid='main'
+		>
+			{children}
+		</main>
+	);
 }
 
 export default MainContent;
