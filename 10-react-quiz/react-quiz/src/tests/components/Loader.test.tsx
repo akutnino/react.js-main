53e3f277a724589d752ac8580880db1d01b3b9b5
Nino Akut <akutnino@gmail.com>
2025-07-02 03:25:29 +0800

    updated unit test of Loader component

--- 10-react-quiz/react-quiz/src/tests/components/Loader.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/Loader.test.tsx
@@ -1,10 +1,17 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import Loader from '../../components/Loader.tsx';
 
 describe('Loader component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		const { getByTestId } = render(<Loader />);
+
+		expect(getByTestId('loader')).toBeInTheDocument();
+		expect(getByTestId('loader').firstElementChild).toHaveAttribute('class', 'loader');
+		expect(getByTestId('loader').children[1].textContent).toBe('Loading questions...');
+	});
 });
