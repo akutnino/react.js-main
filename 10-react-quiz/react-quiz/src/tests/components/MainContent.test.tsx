91cf410d4c70b2e13c1e9c6352958d5560770063
Nino Akut <akutnino@gmail.com>
2025-07-02 04:21:41 +0800

    updated unit test of MainContent component

--- 10-react-quiz/react-quiz/src/tests/components/MainContent.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/MainContent.test.tsx
@@ -1,12 +1,43 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import {
+	cleanup,
+	render,
+	type Matcher,
+	type MatcherOptions,
+} from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import MainContent from '../../components/MainContent.tsx';
+
+type RenderGetByTestIdType = (
+	id: Matcher,
+	options?: MatcherOptions | undefined
+) => HTMLElement;
 
 describe('MainContent component test suite', () => {
+	let renderGetByTestId: RenderGetByTestIdType;
+
+	beforeEach(() => {
+		const { getByTestId } = render(
+			<MainContent>
+				<div>Test</div>
+			</MainContent>
+		);
+
+		renderGetByTestId = getByTestId as RenderGetByTestIdType;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		expect(renderGetByTestId('main')).toBeInTheDocument();
+		expect(renderGetByTestId('main').children).toHaveLength(1);
+	});
 
-	test.todo('should check if the children is mounted by defualt', () => {});
+	test('should check if the children is mounted by defualt', () => {
+		const testElement = renderGetByTestId('main').firstElementChild as HTMLDivElement;
+
+		expect(renderGetByTestId('main')).toContainElement(testElement);
+		expect(testElement).toHaveTextContent('Test');
+	});
 });
