fca04843b6e4a0f1d20a7a512e81faac078d78e0
Nino Akut <akutnino@gmail.com>
2025-07-02 01:49:41 +0800

    updated unit test of Header component

--- 10-react-quiz/react-quiz/src/tests/components/Header.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/Header.test.tsx
@@ -1,10 +1,18 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach, describe, expect, test } from 'vitest';
+import Header from '../../components/Header.tsx';
 
 describe('Header component test suite', () => {
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		const { getByTestId } = render(<Header />);
+
+		expect(getByTestId('header')).toBeInTheDocument();
+		expect(getByTestId('header').firstElementChild).toHaveAttribute('src', 'logo512.png');
+		expect(getByTestId('header').firstElementChild).toHaveAttribute('alt', 'React logo');
+		expect(getByTestId('header').children[1].textContent).toBe('The React Quiz');
+	});
 });
