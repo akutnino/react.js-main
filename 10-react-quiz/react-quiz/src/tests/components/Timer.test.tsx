711b896f311a9b65146cf93c3cc45598fdf96106
Nino Akut <akutnino@gmail.com>
2025-07-02 00:44:46 +0800

    updated unit test of Timer component

--- 10-react-quiz/react-quiz/src/tests/components/Timer.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/Timer.test.tsx
@@ -7,4 +7,12 @@ describe('Timer component test suite', () => {
 	});
 
 	test.todo('should render the component correctly', () => {});
+
+	test.todo('should check if the component updates the time correctly', () => {});
+
+	test.todo('should render the FinishScreen if the time is 0', () => {});
+
+	test.todo('should render correctly if the minutes is less that 10', () => {});
+
+	test.todo('should render correctly if the seconds is less that 10', () => {});
 });
