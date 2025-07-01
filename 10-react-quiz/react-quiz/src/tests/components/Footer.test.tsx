341d0be7c3b990eec3ee0b4437ec7eb60c9afb7f
Nino Akut <akutnino@gmail.com>
2025-07-02 04:56:17 +0800

    updated unit test of Footer component

--- 10-react-quiz/react-quiz/src/tests/components/Footer.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/Footer.test.tsx
@@ -1,12 +1,44 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import {
+	cleanup,
+	render,
+	type Matcher,
+	type MatcherOptions,
+} from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test } from 'vitest';
+import Footer from '../../components/Footer.tsx';
+
+type RenderGetByTestIdType = (
+	id: Matcher,
+	options?: MatcherOptions | undefined
+) => HTMLElement;
 
 describe('Footer component test suite', () => {
+	let renderGetByTestId: RenderGetByTestIdType;
+
+	beforeEach(() => {
+		const { getByTestId } = render(
+			<Footer>
+				<div>Test</div>
+			</Footer>
+		);
+
+		renderGetByTestId = getByTestId as RenderGetByTestIdType;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		expect(renderGetByTestId('footer')).toBeInTheDocument();
+		expect(renderGetByTestId('footer').children).toHaveLength(1);
+	});
 
-	test.todo('should check if the children is mounted by defualt', () => {});
+	test('should check if the children is mounted by defualt', () => {
+		const testElement = renderGetByTestId('footer').firstElementChild as HTMLDivElement;
+
+		expect(testElement).toBeInTheDocument();
+		expect(testElement).toHaveTextContent('Test');
+		expect(renderGetByTestId('footer')).toContainElement(testElement);
+	});
 });
