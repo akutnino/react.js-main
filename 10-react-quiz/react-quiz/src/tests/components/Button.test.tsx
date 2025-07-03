dd19b9b8db47c4f659f95ac78cab3c7b457128dc
Nino Akut <akutnino@gmail.com>
2025-07-03 23:52:26 +0800

    updated unit test of Button component

--- 10-react-quiz/react-quiz/src/tests/components/Button.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/Button.test.tsx
@@ -1,17 +1,50 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import {
+	cleanup,
+	fireEvent,
+	render,
+	type Matcher,
+	type MatcherOptions,
+} from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import Button from '../../components/Button.tsx';
+
+type RenderGetByTestIdType = (
+	id: Matcher,
+	options?: MatcherOptions | undefined
+) => HTMLElement;
 
 describe('Button component test suite', () => {
+	let renderGetByTestId: RenderGetByTestIdType;
+	const mockOnClick = vi.fn();
+
+	beforeEach(() => {
+		const { getByTestId } = render(<Button onClick={mockOnClick}>Click</Button>);
+		renderGetByTestId = getByTestId as RenderGetByTestIdType;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should render the component correctly', () => {});
+	test('should render the component correctly', () => {
+		expect(renderGetByTestId('button')).toBeInTheDocument();
+		expect(renderGetByTestId('button')).toHaveTextContent('Click');
+	});
 
-	test.todo('should check if the children is mounted by defualt', () => {});
+	test('should check if the children is mounted by defualt', () => {
+		expect(renderGetByTestId('button').innerHTML).toBeTruthy();
+		expect(renderGetByTestId('button').innerHTML).toBe('Click');
+	});
 
-	test.todo(
-		'should check if the callback is called when the button was clicked',
-		() => {}
-	);
+	test('should check if the callback is called when the button was clicked', () => {
+		fireEvent.click(
+			renderGetByTestId('button'),
+			new MouseEvent('click', {
+				cancelable: true,
+				bubbles: true,
+			})
+		);
+
+		expect(mockOnClick).toHaveBeenCalledOnce();
+	});
 });
