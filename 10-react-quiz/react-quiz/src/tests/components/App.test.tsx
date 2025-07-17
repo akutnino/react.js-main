2e6f67aeb33156ea267e8d0e5dc0fa9c156da0eb
Nino Akut <akutnino@gmail.com>
2025-07-18 03:50:50 +0800

    updated unit test of App component

--- 10-react-quiz/react-quiz/src/tests/components/App.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/App.test.tsx
@@ -1,8 +1,10 @@
 import {
 	cleanup,
+	fireEvent,
 	render,
 	type Matcher,
 	type MatcherOptions,
+	type SelectorMatcherOptions,
 	type waitForOptions,
 } from '@testing-library/react';
 import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
@@ -19,15 +21,22 @@ type RenderFindByTestIdType = (
 	waitForElementOptions?: waitForOptions | undefined
 ) => Promise<HTMLElement>;
 
+type RenderGetByTextType = (
+	id: Matcher,
+	options?: SelectorMatcherOptions | undefined
+) => HTMLElement;
+
 describe('App component test suite', () => {
 	let renderGetByTestId: RenderGetByTestIdType;
 	let renderFindByTestId: RenderFindByTestIdType;
+	let renderGetByText: RenderGetByTextType;
 
 	beforeEach(() => {
-		const { getByTestId, findByTestId } = render(<App />);
+		const { getByTestId, getByText, findByTestId } = render(<App />);
 
 		renderGetByTestId = getByTestId as RenderGetByTestIdType;
 		renderFindByTestId = findByTestId as RenderFindByTestIdType;
+		renderGetByText = getByText as RenderGetByTextType;
 	});
 
 	afterEach(() => {
@@ -44,24 +53,43 @@ describe('App component test suite', () => {
 	test('should render the Loader component if the status is loading', async () => {
 		await vi.waitFor(() => {
 			expect(renderGetByTestId('loader')).toBeInTheDocument();
+			expect(renderGetByTestId('loader')).toHaveTextContent('Loading questions...');
 		});
 
-		// expect(await renderFindByTestId('', {}));
+		expect(await renderFindByTestId('loader', undefined, undefined)).toBeInTheDocument();
 	});
 
 	test('should render the StartScreen component if the status is ready', async () => {
-		await vi.waitFor(() => {
-			expect(renderGetByTestId('start')).toBeInTheDocument();
+		await vi.waitFor(async () => {
+			expect(await renderFindByTestId('start')).toBeInTheDocument();
+			expect((await renderFindByTestId('start')).firstElementChild).toHaveTextContent(
+				'Welcome to The React Quiz!'
+			);
 		});
+
+		expect(await renderFindByTestId('start', undefined, undefined)).toBeInTheDocument();
 	});
 
-	test.todo(
-		'should render the Progress, Question, and Footer components if the status is active',
-		() => {}
-	);
+	test('should render the Progress, Question, and Footer components if the status is active', async () => {
+		expect(await renderFindByTestId('start', undefined, undefined)).toBeInTheDocument();
+
+		fireEvent.click(
+			renderGetByText(`Let's Start`),
+			new MouseEvent('click', {
+				bubbles: true,
+				cancelable: true,
+			})
+		);
+
+		expect(renderGetByTestId('progress')).toBeInTheDocument();
+		expect(renderGetByTestId('question')).toBeInTheDocument();
+		expect(renderGetByTestId('footer')).toBeInTheDocument();
+	});
+
 	test.todo(
 		'should render the FinishScreen component if the status is finished',
 		() => {}
 	);
+
 	test.todo('should render the ErrorMessage component if the status is error', () => {});
 });
