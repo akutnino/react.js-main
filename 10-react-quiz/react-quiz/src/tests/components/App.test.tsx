6068783fc66588323893ecabc011cccc6232fc5f
Nino Akut <akutnino@gmail.com>
2025-07-17 23:55:33 +0800

    updated unit test of App component

--- 10-react-quiz/react-quiz/src/tests/components/App.test.tsx
+++ 10-react-quiz/react-quiz/src/tests/components/App.test.tsx
@@ -1,15 +1,60 @@
-import { cleanup } from '@testing-library/react';
-import { afterEach, describe, test } from 'vitest';
+import {
+	cleanup,
+	render,
+	type Matcher,
+	type MatcherOptions,
+	type waitForOptions,
+} from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import App from '../../components/App.tsx';
+
+type RenderGetByTestIdType = (
+	id: Matcher,
+	options?: MatcherOptions | undefined
+) => HTMLElement;
+
+type RenderFindByTestIdType = (
+	id: Matcher,
+	options?: MatcherOptions | undefined,
+	waitForElementOptions?: waitForOptions | undefined
+) => Promise<HTMLElement>;
 
 describe('App component test suite', () => {
+	let renderGetByTestId: RenderGetByTestIdType;
+	let renderFindByTestId: RenderFindByTestIdType;
+
+	beforeEach(() => {
+		const { getByTestId, findByTestId } = render(<App />);
+
+		renderGetByTestId = getByTestId as RenderGetByTestIdType;
+		renderFindByTestId = findByTestId as RenderFindByTestIdType;
+	});
+
 	afterEach(() => {
 		cleanup();
 	});
 
-	test.todo('should show all components in the DOM', () => {});
+	test('should show all components in the DOM', () => {
+		expect(renderGetByTestId('container')).toBeInTheDocument();
+		expect(renderGetByTestId('container').firstElementChild).toHaveTextContent(
+			'The React Quiz'
+		);
+	});
 
-	test.todo('should render the Loader component if the status is loading', () => {});
-	test.todo('should render the StartScreen component if the status is ready', () => {});
+	test('should render the Loader component if the status is loading', async () => {
+		await vi.waitFor(() => {
+			expect(renderGetByTestId('loader')).toBeInTheDocument();
+		});
+
+		// expect(await renderFindByTestId('', {}));
+	});
+
+	test('should render the StartScreen component if the status is ready', async () => {
+		await vi.waitFor(() => {
+			expect(renderGetByTestId('start')).toBeInTheDocument();
+		});
+	});
+
 	test.todo(
 		'should render the Progress, Question, and Footer components if the status is active',
 		() => {}
