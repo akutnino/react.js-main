674b44b0368a770167b3bddc192bcecad9269c68
Nino Akut <akutnino@gmail.com>
2025-07-28 23:57:35 +0800

    created unit test for reactQuizReducer reducer

---
+++ 10-react-quiz/react-quiz/src/tests/reducers/reactQuizReducer.test.ts
@@ -0,0 +1,9 @@
+import { afterEach, beforeEach, describe, test } from 'vitest';
+
+describe('reactQuizReducer test suite', () => {
+	beforeEach(() => {});
+
+	afterEach(() => {});
+
+	test('', () => {});
+});
