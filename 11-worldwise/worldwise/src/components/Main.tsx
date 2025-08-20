c93fb0e1c5771a637bc9c4e102acc7edf082a602
Nino Akut <akutnino@gmail.com>
2025-08-21 04:32:21 +0800

    updated Main component - scss file import

--- 11-worldwise/worldwise/src/components/Main.tsx
+++ 11-worldwise/worldwise/src/components/Main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '../styles/index.css';
+import '../styles/Index.scss';
 
 import App from '../components/App.tsx';
 
