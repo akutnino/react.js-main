5bdedeb54a8b8367d2054b954ae5c071ed7d9b34
Nino Akut <akutnino@gmail.com>
2025-08-21 04:37:35 +0800

    updated Pricing component - added starter file syntax

--- 11-worldwise/worldwise/src/pages/Pricing.tsx
+++ 11-worldwise/worldwise/src/pages/Pricing.tsx
@@ -1,13 +1,28 @@
-import PageNav from '../components/PageNav.tsx';
+// Uses the same styles as Product
+import styles from '../styles/pages/Product.module.scss';
 
-function Pricing() {
+function Product() {
 	return (
-		<div>
-			<PageNav />
-
-			<h1>Pricing</h1>
-		</div>
+		<main className={styles.product}>
+			<section>
+				<div>
+					<h2>
+						Simple pricing.
+						<br />
+						Just $9/month.
+					</h2>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel labore
+						mollitia iusto. Recusandae quos provident, laboriosam fugit voluptatem iste.
+					</p>
+				</div>
+				<img
+					src='img-2.jpg'
+					alt='overview of a large city with skyscrapers'
+				/>
+			</section>
+		</main>
 	);
 }
 
-export default Pricing;
+export default Product;
