54a040cf94b18cc3fa39f9a8da08451c6bf9b86a
Nino Akut <akutnino@gmail.com>
2025-08-21 04:35:50 +0800

    updated Homepage component - added starter file syntax

--- 11-worldwise/worldwise/src/pages/Homepage.tsx
+++ 11-worldwise/worldwise/src/pages/Homepage.tsx
@@ -1,15 +1,21 @@
-import { Link } from 'react-router';
-import PageNav from '../components/PageNav.tsx';
+import styles from '../styles/pages//Homepage.module.scss';
 
 function Homepage() {
 	return (
-		<div>
-			<PageNav />
-
-			<h1>Homepage</h1>
-
-			<Link to={'/app'}>go to app</Link>
-		</div>
+		<main className={styles.homepage}>
+			<section>
+				<h1>
+					You travel the world.
+					<br />
+					WorldWise keeps track of your adventures.
+				</h1>
+				<h2>
+					A world map that tracks your footsteps into every city you can think of. Never
+					forget your wonderful experiences, and show your friends how you have wandered
+					the world.
+				</h2>
+			</section>
+		</main>
 	);
 }
 
