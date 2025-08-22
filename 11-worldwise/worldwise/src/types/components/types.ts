794004b76f6ebbc4ef29f61309d79778aa5d6c65
Nino Akut <akutnino@gmail.com>
2025-08-22 08:28:11 +0800

    created types.ts file for components

---
+++ 11-worldwise/worldwise/src/types/components/types.ts
@@ -0,0 +1,13 @@
+export type UserType = {
+	name: string;
+	email: string;
+	password: string;
+	avatar: string;
+};
+
+export type CurrentCityType = {
+	cityName: string;
+	emoji: string;
+	date: string;
+	notes: string;
+};
