246374a8dc9265472fb7aeca09287e7aa9519cbc
Nino Akut <akutnino@gmail.com>
2025-08-26 00:15:39 +0800

    updated PageNav component - added li element, Logo component

--- 11-worldwise/worldwise/src/components/PageNav.tsx
+++ 11-worldwise/worldwise/src/components/PageNav.tsx
@@ -1,20 +1,29 @@
 import { NavLink } from 'react-router';
 import styles from '../styles/components/PageNav.module.scss';
 
+import Logo from './Logo.tsx';
+
 function PageNav() {
 	return (
 		<nav className={styles.nav}>
-			<ul>
-				<li>
-					<NavLink to={'/'}>Home</NavLink>
-				</li>
+			<Logo />
 
+			<ul>
 				<li>
 					<NavLink to={'/product'}>Product</NavLink>
 				</li>
 
 				<li>
 					<NavLink to={'/pricing'}>Pricing</NavLink>
+				</li>
+
+				<li>
+					<NavLink
+						to={'/login'}
+						className={styles.ctaLink}
+					>
+						Login
+					</NavLink>
 				</li>
 			</ul>
 		</nav>
