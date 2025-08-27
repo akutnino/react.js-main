e11c0fd62664f79870d30f2f7c19b0a4804bee27
Nino Akut <akutnino@gmail.com>
2025-08-28 04:12:06 +0800

    updated Sidebar component - added Logo, AppNav, Footer component

--- 11-worldwise/worldwise/src/components/Sidebar.tsx
+++ 11-worldwise/worldwise/src/components/Sidebar.tsx
@@ -1,5 +1,20 @@
+import styles from '../styles/components/Sidebar.module.scss';
+
+import AppNav from './AppNav.tsx';
+import Footer from './Footer.tsx';
+import Logo from './Logo.tsx';
+
 function Sidebar() {
-	return;
+	return (
+		<div className={styles.sidebar}>
+			<Logo />
+			<AppNav />
+
+			<p>List of Cities</p>
+
+			<Footer />
+		</div>
+	);
 }
 
 export default Sidebar;
