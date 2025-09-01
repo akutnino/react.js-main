ed17587b5fa55ab94c7518933b6252cbb2adf27e
Nino Akut <akutnino@gmail.com>
2025-09-02 02:17:09 +0800

    updated CityList component - added Spinner, Message, CityItem component

--- 11-worldwise/worldwise/src/components/CityList.tsx
+++ 11-worldwise/worldwise/src/components/CityList.tsx
@@ -1,7 +1,29 @@
 import styles from '../styles/components/CityList.module.scss';
+import type { CityDataType } from '../types/components/types.ts';
 
-function CityList() {
-	return <ul className={styles.cityList}>LIST</ul>;
+import CityItem from './CityItem.tsx';
+import Message from './Message.tsx';
+import Spinner from './Spinner.tsx';
+
+function CityList({ cities, isLoading }: { cities: CityDataType[]; isLoading: boolean }) {
+	return (
+		<>
+			{isLoading && <Spinner />}
+
+			{!isLoading && !cities.length ? (
+				<Message message='Add a City by Clicking on the Map.' />
+			) : (
+				<ul className={styles.cityList}>
+					{cities.map((cityObject: CityDataType) => (
+						<CityItem
+							cityObject={cityObject}
+							key={cityObject.id}
+						/>
+					))}
+				</ul>
+			)}
+		</>
+	);
 }
 
 export default CityList;
