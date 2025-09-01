dbc1c9f7495ec7b4e8eed46ce907704c7f6f6fc6
Nino Akut <akutnino@gmail.com>
2025-09-02 01:58:53 +0800

    updated Message component

--- 11-worldwise/worldwise/src/components/Message.tsx
+++ 11-worldwise/worldwise/src/components/Message.tsx
@@ -1,6 +1,6 @@
 import styles from '../styles/components/Message.module.scss';
 
-function Message({ message }) {
+function Message({ message }: { message: string }) {
 	return (
 		<p className={styles.message}>
 			<span role='img'>👋</span> {message}
