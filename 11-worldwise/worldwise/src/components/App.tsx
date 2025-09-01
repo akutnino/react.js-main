30c6a974055ecd2f45bca61d1413653bb94a0cd5
Nino Akut <akutnino@gmail.com>
2025-09-02 02:19:47 +0800

    updated App component - fetch cities data, added CityList component

--- 11-worldwise/worldwise/src/components/App.tsx
+++ 11-worldwise/worldwise/src/components/App.tsx
@@ -1,13 +1,51 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { useEffect, useState } from 'react';
+import type { CityDataType } from '../types/components/types.ts';
 
 import Homepage from '../pages/Homepage.tsx';
 import Product from '../pages/Product.tsx';
 import Pricing from '../pages/Pricing.tsx';
 import Login from '../pages/Login.tsx';
 import AppLayout from '../pages/AppLayout.tsx';
+import CityList from './CityList.tsx';
 import PageNotFound from '../pages/PageNotFound.tsx';
 
 function App() {
+	const [cities, setCities] = useState<CityDataType[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		const fetchCitiesData = async () => {
+			try {
+				setIsLoading(true);
+
+				const fetchURL: string = 'http://localhost:8000/cities';
+				const fetchOptions: RequestInit = {
+					method: 'GET',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json',
+					},
+				};
+
+				const response: Response = await fetch(fetchURL, fetchOptions);
+				if (!response.ok) throw new Error('Failed Fetch Request');
+
+				const data: CityDataType[] = await response.json();
+				setCities(data);
+			} catch (error) {
+				if (error instanceof Error) {
+					console.log(error.message);
+				}
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchCitiesData();
+		return () => {};
+	}, []);
+
 	return (
 		<BrowserRouter basename='/'>
 			<Routes>
@@ -33,11 +71,21 @@ function App() {
 				>
 					<Route
 						index={true}
-						element={<p>List of Cities</p>}
+						element={
+							<CityList
+								cities={cities}
+								isLoading={isLoading}
+							/>
+						}
 					/>
 					<Route
 						path='cities'
-						element={<p>List of Cities</p>}
+						element={
+							<CityList
+								cities={cities}
+								isLoading={isLoading}
+							/>
+						}
 					/>
 					<Route
 						path='countries'
