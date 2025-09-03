50db68bd39e3e98407506fc00c7c0de4530e7033
Nino Akut <akutnino@gmail.com>
2025-09-04 00:33:21 +0800

    updated CityItem component - added img element, imoji icon

--- 11-worldwise/worldwise/src/components/CityItem.tsx
+++ 11-worldwise/worldwise/src/components/CityItem.tsx
@@ -1,13 +1,16 @@
 import styles from '../styles/components/CityItem.module.scss';
 import { formatDate } from '../functions/formatDate.ts';
 import type { CityDataType } from '../types/components/types.ts';
+import { unicodeToEmoji } from '../functions/unicodeToEmoji.ts';
 
 function CityItem({ cityObject }: { cityObject: CityDataType }) {
 	const { cityName, emoji, date }: CityDataType = cityObject;
 
 	return (
 		<li className={styles.cityItem}>
-			<span className={styles.emoji}>{emoji}</span>
+			<span className={styles.emoji}>
+				<img src={`https://flagsapi.com/${unicodeToEmoji(emoji)}/flat/32.png`} />
+			</span>
 			<h3 className={styles.name}>{cityName}</h3>
 			<time className={styles.date}>({formatDate(date)})</time>
 			<button
