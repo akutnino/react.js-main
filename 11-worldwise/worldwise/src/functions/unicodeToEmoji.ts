f01ea58bc52989652cda0e19532db16d31a6018c
Nino Akut <akutnino@gmail.com>
2025-09-03 23:50:42 +0800

    created unicodeToEmoji.ts function file

--- 11-worldwise/worldwise/src/functions/unicodeToEmoji.ts
+++ 11-worldwise/worldwise/src/functions/unicodeToEmoji.ts
@@ -1,7 +1,13 @@
-export const unicodeToEmoji = (countryCode: string) => {
-	const codePoints = countryCode
-		.toUpperCase()
-		.split('')
-		.map((char: string, index: number) => 127397 + char.charCodeAt(index));
-	return String.fromCodePoint(...codePoints);
+export const unicodeToEmoji = (countryCode: string): string => {
+	const countryCodeChar = [...countryCode].map((char: string) => {
+		const CHARCODE_LAST_CHARACTER: number = 'Z'.charCodeAt(0);
+		const UNITCODE_LAST_CHARACTER: number = 127487; // www.alt-codes.net/flags
+
+		const countryCodePoint: number = char.codePointAt(0)!;
+		const countryCharCode: number = UNITCODE_LAST_CHARACTER - countryCodePoint;
+
+		return String.fromCodePoint(CHARCODE_LAST_CHARACTER - countryCharCode);
+	});
+
+	return countryCodeChar.join('');
 };
