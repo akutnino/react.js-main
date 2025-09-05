eb434f12d417f5ad75a1a6fec609f1903ec553f7
Nino Akut <akutnino@gmail.com>
2025-09-06 00:45:09 +0800

    updated CountryItem component - added CountryIcon component

--- 11-worldwise/worldwise/src/components/CountryItem.tsx
+++ 11-worldwise/worldwise/src/components/CountryItem.tsx
@@ -1,14 +1,13 @@
-import { unicodeToEmoji } from '../functions/unicodeToEmoji.ts';
 import type { CityDataType } from '../types/components/types.ts';
 import styles from '../styles/components/CountryItem.module.scss';
+
+import CountryIcon from './CountryIcon.tsx';
 
 function CountryItem({ cityObject }: { cityObject: CityDataType }) {
 	return (
 		<li className={styles.countryItem}>
 			<span>
-				<img
-					src={`https://flagsapi.com/${unicodeToEmoji(cityObject.emoji)}/flat/32.png`}
-				/>
+				<CountryIcon countryCode={cityObject.emoji} />
 			</span>
 			<span>{cityObject.country}</span>
 		</li>
