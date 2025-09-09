c7bc319e7fcf4f8a8e6dd3192eef68f10e02c086
Nino Akut <akutnino@gmail.com>
2025-09-10 01:07:49 +0800

    updated Map component - used useNavigate hook

--- 11-worldwise/worldwise/src/components/Map.tsx
+++ 11-worldwise/worldwise/src/components/Map.tsx
@@ -1,18 +1,26 @@
-import { useSearchParams } from 'react-router';
+import { useNavigate, useSearchParams, type NavigateFunction } from 'react-router';
 import type { UseSearchParamsType } from '../types/components/types.ts';
 import styles from '../styles/components/Map.module.scss';
 
 function Map() {
+	const navigate: NavigateFunction = useNavigate();
 	const [searchParams, setSearchParams]: UseSearchParamsType = useSearchParams();
 	const lat: string | null = searchParams.get('lat');
 	const lng: string | null = searchParams.get('lng');
+
+	const handleClick = () => {
+		navigate('form');
+	};
 
 	const handleChangePosition = () => {
 		setSearchParams({ lat: 'nino', lng: 'brown' });
 	};
 
 	return (
-		<div className={styles.mapContainer}>
+		<div
+			className={styles.mapContainer}
+			onClick={handleClick}
+		>
 			<h1>Map</h1>
 			<h1>
 				Position: {lat}, {lng}
