54f218a739d4523de051ff8b6b48b929175ad3dd
Nino Akut <akutnino@gmail.com>
2025-09-09 17:46:09 +0800

    updated Map component - used useSearchParams hook

--- 11-worldwise/worldwise/src/components/Map.tsx
+++ 11-worldwise/worldwise/src/components/Map.tsx
@@ -1,7 +1,30 @@
+import { useSearchParams } from 'react-router';
+import type { UseSearchParamsType } from '../types/components/types.ts';
 import styles from '../styles/components/Map.module.scss';
 
 function Map() {
-	return <div className={styles.mapContainer}>Map</div>;
+	const [searchParams, setSearchParams]: UseSearchParamsType = useSearchParams();
+	const lat: string | null = searchParams.get('lat');
+	const lng: string | null = searchParams.get('lng');
+
+	const handleChangePosition = () => {
+		setSearchParams({ lat: 'nino', lng: 'brown' });
+	};
+
+	return (
+		<div className={styles.mapContainer}>
+			<h1>Map</h1>
+			<h1>
+				Position: {lat}, {lng}
+			</h1>
+			<button
+				type='button'
+				onClick={handleChangePosition}
+			>
+				Change Position
+			</button>
+		</div>
+	);
 }
 
 export default Map;
