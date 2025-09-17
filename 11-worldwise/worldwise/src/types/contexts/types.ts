2e814992cd6af76922ff6fb6422d70a4a1dfeaac
Nino Akut <akutnino@gmail.com>
2025-09-17 10:50:41 +0800

    updated types.ts file of contexts folder

--- 11-worldwise/worldwise/src/types/contexts/types.ts
+++ 11-worldwise/worldwise/src/types/contexts/types.ts
@@ -1,6 +1,10 @@
+import type { Dispatch } from 'react';
 import type { CityDataType } from '../components/types.ts';
 
 export type CitiesContextValue = {
 	cities: CityDataType[];
 	isLoading: boolean;
+	setIsLoading: Dispatch<React.SetStateAction<boolean>>;
+	currentCity: CityDataType | null;
+	setCurrentCity: Dispatch<React.SetStateAction<CityDataType | null>>;
 };
