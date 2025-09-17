c4f2f377dfcc9de23dc109c399e111f1b9d3a83f
Nino Akut <akutnino@gmail.com>
2025-09-17 10:52:21 +0800

    created fetchData.ts function file

---
+++ 11-worldwise/worldwise/src/functions/fetchData.ts
@@ -0,0 +1,36 @@
+import type {
+	SetIsLoadingType,
+	SetResponseDataType,
+	ResponseDataType,
+} from '../types/functions/types.ts';
+
+export const fetchData = async (
+	urlPath: string,
+	setIsLoading: SetIsLoadingType,
+	setResponseData: SetResponseDataType
+) => {
+	try {
+		setIsLoading(true);
+
+		const fetchURL: string = `http://localhost:8000/${urlPath}`;
+		const fetchOptions: RequestInit = {
+			method: 'GET',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		};
+
+		const response: Response = await fetch(fetchURL, fetchOptions);
+		if (!response.ok) throw new Error('Failed Fetch Request');
+
+		const responseData: ResponseDataType = await response.json();
+		setResponseData(responseData);
+	} catch (error) {
+		if (error instanceof Error) {
+			console.log(error.message);
+		}
+	} finally {
+		setIsLoading(false);
+	}
+};
