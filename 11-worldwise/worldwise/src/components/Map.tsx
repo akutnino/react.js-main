eac0dfa9972ce045eadde59b282a024b4109ceaf
Nino Akut <akutnino@gmail.com>
2025-09-19 11:17:54 +0800

    updated Map component - added react-leaflet components

--- 11-worldwise/worldwise/src/components/Map.tsx
+++ 11-worldwise/worldwise/src/components/Map.tsx
@@ -1,11 +1,14 @@
 import { useNavigate, useSearchParams, type NavigateFunction } from 'react-router';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useState, type MouseEvent } from 'react';
 import type { UseSearchParamsType } from '../types/components/types.ts';
-import type { MouseEvent } from 'react';
+import type { LatLngExpression } from 'leaflet';
 import styles from '../styles/components/Map.module.scss';
 
 function Map() {
 	const navigate: NavigateFunction = useNavigate();
 	const [searchParams, setSearchParams]: UseSearchParamsType = useSearchParams();
+	const [mapPosition, setMapPosition] = useState<LatLngExpression>([40, 0]);
 	const lat: string | null = searchParams.get('lat');
 	const lng: string | null = searchParams.get('lng');
 
@@ -15,7 +18,8 @@ function Map() {
 
 	const handleChangePosition = (event: MouseEvent<HTMLButtonElement>) => {
 		event.stopPropagation();
-		setSearchParams({ lat: 'nino', lng: 'brown' });
+		// setSearchParams({ lat: 'nino', lng: 'brown' });
+		console.log({ lat, lng });
 	};
 
 	return (
@@ -23,16 +27,22 @@ function Map() {
 			className={styles.mapContainer}
 			onClick={handleClick}
 		>
-			<h1>Map</h1>
-			<h1>
-				Position: {lat}, {lng}
-			</h1>
-			<button
-				type='button'
-				onClick={handleChangePosition}
+			<MapContainer
+				className={styles.map}
+				center={mapPosition}
+				zoom={13}
+				scrollWheelZoom={true}
 			>
-				Change Position
-			</button>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+				/>
+				<Marker position={mapPosition}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
 		</div>
 	);
 }
