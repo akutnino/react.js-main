0d2a95d6161a67c6f68b35b3a763455bf220275e
Nino Akut <akutnino@gmail.com>
2025-09-19 12:17:26 +0800

    updated Map component - used useCities hook to mark cities

--- 11-worldwise/worldwise/src/components/Map.tsx
+++ 11-worldwise/worldwise/src/components/Map.tsx
@@ -1,26 +1,28 @@
 import { useNavigate, useSearchParams, type NavigateFunction } from 'react-router';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { useState, type MouseEvent } from 'react';
-import type { UseSearchParamsType } from '../types/components/types.ts';
+import { useCities } from '../contexts/CitiesContext.tsx';
+import type { CitiesContextValue } from '../types/contexts/types.ts';
+import type { CityDataType, UseSearchParamsType } from '../types/components/types.ts';
 import type { LatLngExpression } from 'leaflet';
 import styles from '../styles/components/Map.module.scss';
 
 function Map() {
 	const navigate: NavigateFunction = useNavigate();
-	const [searchParams, setSearchParams]: UseSearchParamsType = useSearchParams();
+	const { cities }: CitiesContextValue = useCities();
 	const [mapPosition, setMapPosition] = useState<LatLngExpression>([40, 0]);
-	const lat: string | null = searchParams.get('lat');
-	const lng: string | null = searchParams.get('lng');
+	// const [searchParams, setSearchParams]: UseSearchParamsType = useSearchParams();
+	// const lat: string | null = searchParams.get('lat');
+	// const lng: string | null = searchParams.get('lng');
 
 	const handleClick = () => {
 		navigate('form');
 	};
 
-	const handleChangePosition = (event: MouseEvent<HTMLButtonElement>) => {
-		event.stopPropagation();
-		// setSearchParams({ lat: 'nino', lng: 'brown' });
-		console.log({ lat, lng });
-	};
+	// const handleChangePosition = (event: MouseEvent<HTMLButtonElement>) => {
+	// 	event.stopPropagation();
+	// 	setSearchParams({ lat: 'nino', lng: 'brown' });
+	// };
 
 	return (
 		<div
@@ -30,18 +32,24 @@ function Map() {
 			<MapContainer
 				className={styles.map}
 				center={mapPosition}
-				zoom={13}
+				zoom={4}
 				scrollWheelZoom={true}
 			>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
 				/>
-				<Marker position={mapPosition}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
+
+				{cities.map((cityObj: CityDataType) => (
+					<Marker
+						position={Object.values(cityObj.position) as LatLngExpression}
+						key={cityObj.id}
+					>
+						<Popup>
+							A pretty CSS3 popup. <br /> Easily customizable.
+						</Popup>
+					</Marker>
+				))}
 			</MapContainer>
 		</div>
 	);
