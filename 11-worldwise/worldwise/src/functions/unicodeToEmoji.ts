a3f90960674ed45fe84b1bf42a2bda2d23202800
Nino Akut <akutnino@gmail.com>
2025-09-25 13:31:40 +0800

    updated unicodeToEmoji.ts function file

--- 11-worldwise/worldwise/src/functions/unicodeToEmoji.ts
+++ 11-worldwise/worldwise/src/functions/unicodeToEmoji.ts
@@ -1,4 +1,10 @@
 export const unicodeToEmoji = (countryCode: string): string => {
+	const ENGLISH_ALPHABET: string[] = Array(26)
+		.fill(null)
+		.map((_, index) => String.fromCharCode(Number('A'.codePointAt(0)) + index));
+
+	if (ENGLISH_ALPHABET.includes(countryCode.at(0)!)) return countryCode.toUpperCase();
+
 	const countryCodeChar = [...countryCode].map((char: string) => {
 		const CHARCODE_LAST_CHARACTER: number = 'Z'.charCodeAt(0);
 		const UNITCODE_LAST_CHARACTER: number = 127487; // www.alt-codes.net/flags
