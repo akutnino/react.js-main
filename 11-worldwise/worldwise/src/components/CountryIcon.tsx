6ad4196b197ec36f963af7581e8d41fb11a49989
Nino Akut <akutnino@gmail.com>
2025-09-26 00:48:40 +0800

    updated CountryIcon component

--- 11-worldwise/worldwise/src/components/CountryIcon.tsx
+++ 11-worldwise/worldwise/src/components/CountryIcon.tsx
@@ -1,6 +1,7 @@
 import { unicodeToEmoji } from '../functions/unicodeToEmoji.ts';
 
 function CountryIcon({ countryCode }: { countryCode: string }) {
+	if (!countryCode) return null;
 	return <img src={`https://flagsapi.com/${unicodeToEmoji(countryCode)}/flat/32.png`} />;
 }
 
