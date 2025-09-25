d4a4f5f87327f8137d645ce65884570a1ebef8a3
Nino Akut <akutnino@gmail.com>
2025-09-25 13:34:05 +0800

    updated Form component - used fetchCityData function, useUrlPosition custom hook

--- 11-worldwise/worldwise/src/components/Form.tsx
+++ 11-worldwise/worldwise/src/components/Form.tsx
@@ -1,17 +1,26 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
-import { useState, type ChangeEvent, type MouseEvent } from 'react';
+import { useEffect, useState, type ChangeEvent, type MouseEvent } from 'react';
 import { useNavigate, type NavigateFunction } from 'react-router';
+import { useUrlPosition } from '../hooks/useUrlPosition.ts';
+import { fetchCityData } from '../functions/fetchCityData.ts';
 import styles from '../styles/components/Form.module.scss';
 
 import Button from './Button.tsx';
+import CountryIcon from './CountryIcon.tsx';
+import Message from './Message.tsx';
+import Spinner from './Spinner.tsx';
 
 function Form() {
 	const navigate: NavigateFunction = useNavigate();
+	const [mapLatitude, mapLongitude] = useUrlPosition();
 	const [cityName, setCityName] = useState<string>('');
-	// const [country, setCountry] = useState<string>('');
+	const [country, setCountry] = useState<string>('');
+	const [countryCode, setCountryCode] = useState<string>('');
 	const [date, setDate] = useState<Date | string>(new Date());
 	const [notes, setNotes] = useState<string>('');
+	const [isLoadingGeolocation, setIsLoadingGeolocation] = useState<boolean>(false);
+	const [fetchError, setFetchError] = useState<string>('');
 
 	const handleCityNameInput = (event: ChangeEvent<HTMLInputElement>) => {
 		setCityName(event.target.value);
@@ -30,46 +39,69 @@ function Form() {
 		navigate('/app/cities', { replace: true });
 	};
 
+	useEffect(() => {
+		fetchCityData(
+			mapLatitude,
+			mapLongitude,
+			setIsLoadingGeolocation,
+			setFetchError,
+			setCityName,
+			setCountry,
+			setCountryCode
+		);
+		return () => {};
+	}, [mapLatitude, mapLongitude]);
+
 	return (
-		<form className={styles.form}>
-			<div className={styles.row}>
-				<label htmlFor='cityName'>City name</label>
-				<input
-					id='cityName'
-					onChange={handleCityNameInput}
-					value={cityName}
-				/>
-				{/* <span className={styles.flag}>{emoji}</span> */}
-			</div>
+		<>
+			{isLoadingGeolocation && <Spinner />}
 
-			<div className={styles.row}>
-				<label htmlFor='date'>When did you go to {cityName}?</label>
-				<input
-					id='date'
-					onChange={handleDateInput}
-					value={date as string}
-				/>
-			</div>
+			{!isLoadingGeolocation && fetchError && <Message message={fetchError} />}
 
-			<div className={styles.row}>
-				<label htmlFor='notes'>Notes about your trip to {cityName}</label>
-				<textarea
-					id='notes'
-					onChange={handleNotesInput}
-					value={notes}
-				/>
-			</div>
+			{!isLoadingGeolocation && !fetchError && (
+				<form className={styles.form}>
+					<div className={styles.row}>
+						<label htmlFor='cityName'>City name</label>
+						<input
+							id='cityName'
+							onChange={handleCityNameInput}
+							value={cityName}
+						/>
+						<span className={styles.flag}>
+							<CountryIcon countryCode={countryCode} />
+						</span>
+					</div>
 
-			<div className={styles.buttons}>
-				<Button type='primary'>Add</Button>
-				<Button
-					type='back'
-					onClick={handleBack}
-				>
-					&larr; Back
-				</Button>
-			</div>
-		</form>
+					<div className={styles.row}>
+						<label htmlFor='date'>When did you go to {cityName}?</label>
+						<input
+							id='date'
+							onChange={handleDateInput}
+							value={date as string}
+						/>
+					</div>
+
+					<div className={styles.row}>
+						<label htmlFor='notes'>Notes about your trip to {cityName}</label>
+						<textarea
+							id='notes'
+							onChange={handleNotesInput}
+							value={notes}
+						/>
+					</div>
+
+					<div className={styles.buttons}>
+						<Button type='primary'>Add</Button>
+						<Button
+							type='back'
+							onClick={handleBack}
+						>
+							&larr; Back
+						</Button>
+					</div>
+				</form>
+			)}
+		</>
 	);
 }
 
