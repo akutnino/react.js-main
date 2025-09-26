9deeb5ea23791b862d84bae7e352815f0d095c3f
Nino Akut <akutnino@gmail.com>
2025-09-26 14:22:10 +0800

    updated Button component - added submit type

--- 11-worldwise/worldwise/src/components/Button.tsx
+++ 11-worldwise/worldwise/src/components/Button.tsx
@@ -6,14 +6,14 @@ function Button({
 	onClick,
 	children,
 }: {
-	type: 'primary' | 'back' | 'position';
+	type: 'primary' | 'back' | 'position' | 'submit';
 	onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 	children: ReactNode;
 }) {
 	return (
 		<button
-			className={`${styles.btn} ${styles[type]}`}
-			type='button'
+			className={`${styles.btn} ${type === 'submit' ? styles['primary'] : styles[type]}`}
+			type={type === 'submit' ? 'submit' : 'button'}
 			onClick={onClick}
 		>
 			{children}
