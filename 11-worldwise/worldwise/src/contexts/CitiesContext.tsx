9a650ee4258136b98ac32842a6a96415355678cd
Nino Akut <akutnino@gmail.com>
2025-09-26 14:55:50 +0800

    updated CitiesContext component - added getCityData, getNewCityData to context

--- 11-worldwise/worldwise/src/contexts/CitiesContext.tsx
+++ 11-worldwise/worldwise/src/contexts/CitiesContext.tsx
@@ -1,7 +1,12 @@
 import { createContext, useContext, useEffect, useState, type ReactNode } from 'react';
 import { getCityData } from '../functions/getCityData.ts';
+import { getNewCityData } from '../functions/getNewCityData.ts';
 import type { CityDataType } from '../types/components/types.ts';
 import type { CitiesContextValue } from '../types/contexts/types.ts';
+import type {
+	GetCityDataFuncType,
+	GetNewCityDataFuncType,
+} from '../types/functions/types.ts';
 
 const CitiesContext = createContext<CitiesContextValue | null>(null);
 
@@ -9,11 +14,13 @@ function CitiesProvider({ children }: { children: ReactNode }) {
 	const [cities, setCities] = useState<CityDataType[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [currentCity, setCurrentCity] = useState<CityDataType | null>(null);
+	const getCityDataFunc: GetCityDataFuncType = getCityData;
+	const getNewCityDataFunc: GetNewCityDataFuncType = getNewCityData;
 
 	useEffect(() => {
-		getCityData('cities', setIsLoading, setCities);
+		getCityDataFunc('cities', setIsLoading, setCities);
 		return () => {};
-	}, []);
+	}, [getCityDataFunc]);
 
 	return (
 		<CitiesContext.Provider
@@ -23,6 +30,8 @@ function CitiesProvider({ children }: { children: ReactNode }) {
 				setIsLoading,
 				currentCity,
 				setCurrentCity,
+				getCityDataFunc,
+				getNewCityDataFunc,
 			}}
 		>
 			{children}
