e3e442f5a782b77501a406997ea6efceb8596faf
Nino Akut <akutnino@gmail.com>
2025-09-27 13:01:34 +0800

    updated types.ts file for contexts - added PostNewCityDataType

--- 11-worldwise/worldwise/src/types/contexts/types.ts
+++ 11-worldwise/worldwise/src/types/contexts/types.ts
@@ -1,13 +1,21 @@
 import type { Dispatch } from 'react';
 import type { CityDataType } from '../components/types.ts';
-import type { GetCityDataType, GetNewCityDataType } from '../functions/types.ts';
+import type {
+	GetCityDataType,
+	GetNewCityDataType,
+	PostNewCityDataType,
+} from '../functions/types.ts';
+
+export type SetCitiesType = Dispatch<React.SetStateAction<CityDataType[]>>;
 
 export type CitiesContextValue = {
 	cities: CityDataType[];
+	setCities: SetCitiesType;
 	isLoading: boolean;
 	setIsLoading: Dispatch<React.SetStateAction<boolean>>;
 	currentCity: CityDataType | null;
 	setCurrentCity: Dispatch<React.SetStateAction<CityDataType | null>>;
 	getCityData: GetCityDataType;
 	getNewCityData: GetNewCityDataType;
+	postNewCityData: PostNewCityDataType;
 };
