e970cd0f9608a7c452fc60e6518f8c918e6ec4e8
Nino Akut <akutnino@gmail.com>
2025-10-01 11:24:24 +0800

    updated types.ts file for contexts - added all contents of types.ts file for functions

--- 11-worldwise/worldwise/src/types/contexts/types.ts
+++ 11-worldwise/worldwise/src/types/contexts/types.ts
@@ -1,11 +1,63 @@
 import type { Dispatch } from 'react';
 import type { CityDataType } from '../components/types.ts';
-import type {
-	DeleteCityDataType,
-	GetCityDataType,
-	GetNewCityDataType,
-	PostNewCityDataType,
-} from '../functions/types.ts';
+
+export type SetResponseDataType =
+	| Dispatch<React.SetStateAction<CityDataType | null>>
+	| Dispatch<React.SetStateAction<CityDataType[]>>;
+
+export type ResponseDataType = CityDataType[] & CityDataType;
+
+export type GetCityDataType = (
+	urlPath: string,
+	setResponseData: SetResponseDataType
+) => Promise<void>;
+
+export type GetNewCityDataType = (
+	mapLatitude: string | null,
+	mapLongitude: string | null,
+	setIsLoadingGeolocation: SetIsLoadingGeolocationType,
+	setFetchError: SetFetchErrorType,
+	setCityName: SetCityNameType,
+	setCountry: SetCountryType,
+	setCountryCode: SetCountryCodeType
+) => Promise<void>;
+
+export type PostNewCityDataType = (newCityData: CityDataType) => Promise<void>;
+
+export type DeleteCityDataType = (cityID: string) => Promise<void>;
+
+export type SetIsLoadingGeolocationType = Dispatch<React.SetStateAction<boolean>>;
+
+export type SetFetchErrorType = Dispatch<React.SetStateAction<string>>;
+
+export type SetCityNameType = Dispatch<React.SetStateAction<string>>;
+
+export type SetCountryType = Dispatch<React.SetStateAction<string>>;
+
+export type SetCountryCodeType = Dispatch<React.SetStateAction<string>>;
+
+type LocalityInfoType = {
+	administrative: object[];
+	informative: object[];
+};
+
+export type FormFetchCityDataType = {
+	latitude: number;
+	lookupSource: string;
+	longitude: number;
+	localityLanguageRequested: string;
+	continent: string;
+	continentCode: string;
+	countryName: string;
+	countryCode: string;
+	principalSubdivision: string;
+	principalSubdivisionCode: string;
+	city: string;
+	locality: string;
+	postcode: string;
+	plusCode: string;
+	localityInfo: LocalityInfoType;
+};
 
 export type SetCitiesType = Dispatch<React.SetStateAction<CityDataType[]>>;
 
