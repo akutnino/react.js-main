e1e9e95a51c38c49abd375aee2f4de27a5861cfb
Nino Akut <akutnino@gmail.com>
2025-10-01 12:00:48 +0800

    updated CitiesContext components - added getCityData, getNewCityData, postNewCityData, deleteCityData

--- 11-worldwise/worldwise/src/contexts/CitiesContext.tsx
+++ 11-worldwise/worldwise/src/contexts/CitiesContext.tsx
@@ -1,16 +1,16 @@
 import { createContext, useContext, useEffect, useState, type ReactNode } from 'react';
-import { getCityData as getCityDataFunc } from '../functions/getCityData.ts';
-import { getNewCityData as getNewCityDataFunc } from '../functions/getNewCityData.ts';
-import { postNewCityData as postNewCityDataFunc } from '../functions/postNewCityData.ts';
-import { deleteCityData as deleteCityDataFunc } from '../functions/deleteCityData.ts';
 import type { CityDataType } from '../types/components/types.ts';
-import type { CitiesContextValue } from '../types/contexts/types.ts';
 import type {
-	DeleteCityDataType,
-	GetCityDataType,
-	GetNewCityDataType,
-	PostNewCityDataType,
-} from '../types/functions/types.ts';
+	CitiesContextValue,
+	FormFetchCityDataType,
+	ResponseDataType,
+	SetCityNameType,
+	SetCountryCodeType,
+	SetCountryType,
+	SetFetchErrorType,
+	SetIsLoadingGeolocationType,
+	SetResponseDataType,
+} from '../types/contexts/types.ts';
 
 const CitiesContext = createContext<CitiesContextValue | null>(null);
 
@@ -18,15 +18,133 @@ function CitiesProvider({ children }: { children: ReactNode }) {
 	const [cities, setCities] = useState<CityDataType[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [currentCity, setCurrentCity] = useState<CityDataType | null>(null);
-	const getCityData: GetCityDataType = getCityDataFunc;
-	const getNewCityData: GetNewCityDataType = getNewCityDataFunc;
-	const postNewCityData: PostNewCityDataType = postNewCityDataFunc;
-	const deleteCityData: DeleteCityDataType = deleteCityDataFunc;
+
+	const getCityData = async (urlPath: string, setResponseData: SetResponseDataType) => {
+		try {
+			setIsLoading(true);
+
+			const fetchURL: string = `http://localhost:8000/${urlPath}`;
+			const fetchOptions: RequestInit = {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			};
+
+			const response: Response = await fetch(fetchURL, fetchOptions);
+			if (!response.ok) throw new Error('Failed Fetch Request');
+
+			const responseData: ResponseDataType = await response.json();
+			setResponseData(responseData);
+		} catch (error) {
+			if (error instanceof Error) {
+				console.log(error.message);
+			}
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const getNewCityData = async (
+		mapLatitude: string | null,
+		mapLongitude: string | null,
+		setIsLoadingGeolocation: SetIsLoadingGeolocationType,
+		setFetchError: SetFetchErrorType,
+		setCityName: SetCityNameType,
+		setCountry: SetCountryType,
+		setCountryCode: SetCountryCodeType
+	) => {
+		try {
+			setIsLoadingGeolocation(true);
+			setFetchError('');
+
+			const fetchURL:
+				| RequestInfo
+				| URL = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${mapLatitude}&longitude=${mapLongitude}`;
+
+			const fetchOptions: RequestInit = {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			};
+
+			const response: Response = await fetch(fetchURL, fetchOptions);
+			if (!response.ok) throw new Error('Failed Fetch Request');
+
+			const data: FormFetchCityDataType = await response.json();
+			if (!data.countryCode) throw new Error('Not a Country.Pls Click Somewhere else.');
+
+			setCityName(data.city || data.locality || '');
+			setCountry(data.countryName);
+			setCountryCode(data.countryCode);
+		} catch (error) {
+			if (error instanceof Error) {
+				setFetchError(error.message);
+			}
+		} finally {
+			setIsLoadingGeolocation(false);
+		}
+	};
+
+	const postNewCityData = async (newCityData: CityDataType) => {
+		try {
+			setIsLoading(true);
+
+			const fetchURL: RequestInfo | URL = `http://localhost:8000/cities`;
+			const fetchOptions: RequestInit = {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(newCityData),
+			};
+
+			const response: Response = await fetch(fetchURL, fetchOptions);
+			if (!response.ok) throw new Error('Failed Fetch Request');
+
+			const data: CityDataType = await response.json();
+			if (!data.id) throw new Error('API Post Request Failed');
+
+			setCities((currentState) => [...currentState, data]);
+		} catch (error) {
+			if (error instanceof Error) {
+				console.log(error.message);
+			}
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const deleteCityData = async (cityID: string) => {
+		try {
+			setIsLoading(true);
+
+			const fetchURL: RequestInfo | URL = `http://localhost:8000/cities/${cityID}`;
+			const fetchOptions: RequestInit = {
+				method: 'DELETE',
+			};
+
+			const response: Response = await fetch(fetchURL, fetchOptions);
+			if (!response.ok) throw new Error('Failed Fetch Request');
+
+			setCities((currentCities) => currentCities.filter((city) => city.id !== cityID));
+		} catch (error) {
+			if (error instanceof Error) {
+				console.log(error.message);
+			}
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
 	useEffect(() => {
-		getCityData('cities', setIsLoading, setCities);
+		getCityData('cities', setCities);
 		return () => {};
-	}, [getCityData]);
+	}, []);
 
 	return (
 		<CitiesContext.Provider
