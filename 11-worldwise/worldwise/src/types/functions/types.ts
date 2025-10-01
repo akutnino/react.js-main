2b3700b2b14ad6334a9911d4a3949574269eb9e6
Nino Akut <akutnino@gmail.com>
2025-10-01 11:22:12 +0800

    updated types.ts file for functions - commented out all its contents

--- 11-worldwise/worldwise/src/types/functions/types.ts
+++ 11-worldwise/worldwise/src/types/functions/types.ts
@@ -1,72 +1,72 @@
-import type { Dispatch } from 'react';
-import type { CityDataType } from '../components/types.ts';
-import type { SetCitiesType } from '../contexts/types.ts';
+// import type { Dispatch } from 'react';
+// import type { CityDataType } from '../components/types.ts';
+// import type { SetCitiesType } from '../contexts/types.ts';
 
-export type SetResponseDataType =
-	| Dispatch<React.SetStateAction<CityDataType | null>>
-	| Dispatch<React.SetStateAction<CityDataType[]>>;
+// export type SetResponseDataType =
+// 	| Dispatch<React.SetStateAction<CityDataType | null>>
+// 	| Dispatch<React.SetStateAction<CityDataType[]>>;
 
-export type SetIsLoadingType = Dispatch<React.SetStateAction<boolean>>;
+// export type SetIsLoadingType = Dispatch<React.SetStateAction<boolean>>;
 
-export type ResponseDataType = CityDataType[] & CityDataType;
+// export type ResponseDataType = CityDataType[] & CityDataType;
 
-export type SetIsLoadingGeolocationType = Dispatch<React.SetStateAction<boolean>>;
+// export type SetIsLoadingGeolocationType = Dispatch<React.SetStateAction<boolean>>;
 
-export type SetFetchErrorType = Dispatch<React.SetStateAction<string>>;
+// export type SetFetchErrorType = Dispatch<React.SetStateAction<string>>;
 
-export type SetCityNameType = Dispatch<React.SetStateAction<string>>;
+// export type SetCityNameType = Dispatch<React.SetStateAction<string>>;
 
-export type SetCountryType = Dispatch<React.SetStateAction<string>>;
+// export type SetCountryType = Dispatch<React.SetStateAction<string>>;
 
-export type SetCountryCodeType = Dispatch<React.SetStateAction<string>>;
+// export type SetCountryCodeType = Dispatch<React.SetStateAction<string>>;
 
-export type GetCityDataType = (
-	urlPath: string,
-	setIsLoading: SetIsLoadingType,
-	setResponseData: SetResponseDataType
-) => Promise<void>;
+// export type GetCityDataType = (
+// 	urlPath: string,
+// 	setIsLoading: SetIsLoadingType,
+// 	setResponseData: SetResponseDataType
+// ) => Promise<void>;
 
-export type GetNewCityDataType = (
-	mapLatitude: string | null,
-	mapLongitude: string | null,
-	setIsLoadingGeolocation: SetIsLoadingGeolocationType,
-	setFetchError: SetFetchErrorType,
-	setCityName: SetCityNameType,
-	setCountry: SetCountryType,
-	setCountryCode: SetCountryCodeType
-) => Promise<void>;
+// export type GetNewCityDataType = (
+// 	mapLatitude: string | null,
+// 	mapLongitude: string | null,
+// 	setIsLoadingGeolocation: SetIsLoadingGeolocationType,
+// 	setFetchError: SetFetchErrorType,
+// 	setCityName: SetCityNameType,
+// 	setCountry: SetCountryType,
+// 	setCountryCode: SetCountryCodeType
+// ) => Promise<void>;
 
-export type PostNewCityDataType = (
-	newCityData: CityDataType,
-	setIsLoading: SetIsLoadingType,
-	setCities: SetCitiesType
-) => Promise<void>;
+// export type PostNewCityDataType = (
+// 	newCityData: CityDataType,
+// 	setIsLoading: SetIsLoadingType,
+// 	setCities: SetCitiesType
+// ) => Promise<void>;
 
-export type DeleteCityDataType = (
-	cityID: string,
-	setIsLoading: SetIsLoadingType,
-	setCities: SetCitiesType
-) => Promise<void>;
+// export type DeleteCityDataType = (
+// 	cityID: string,
+// 	setIsLoading: SetIsLoadingType,
+// 	setCities: SetCitiesType
+// ) => Promise<void>;
 
-type LocalityInfoType = {
-	administrative: object[];
-	informative: object[];
-};
+// type LocalityInfoType = {
+// 	administrative: object[];
+// 	informative: object[];
+// };
 
-export type FormFetchCityDataType = {
-	latitude: number;
-	lookupSource: string;
-	longitude: number;
-	localityLanguageRequested: string;
-	continent: string;
-	continentCode: string;
-	countryName: string;
-	countryCode: string;
-	principalSubdivision: string;
-	principalSubdivisionCode: string;
-	city: string;
-	locality: string;
-	postcode: string;
-	plusCode: string;
-	localityInfo: LocalityInfoType;
-};
+// export type FormFetchCityDataType = {
+// 	latitude: number;
+// 	lookupSource: string;
+// 	longitude: number;
+// 	localityLanguageRequested: string;
+// 	continent: string;
+// 	continentCode: string;
+// 	countryName: string;
+// 	countryCode: string;
+// 	principalSubdivision: string;
+// 	principalSubdivisionCode: string;
+// 	city: string;
+// 	locality: string;
+// 	postcode: string;
+// 	plusCode: string;
+// 	localityInfo: LocalityInfoType;
+// };
