a119d4d700db09465fcd8edba36a60de619be56b
Nino Akut <akutnino@gmail.com>
2025-10-08 10:58:19 +0800

    updated AppLayout component - added useAuth hook and useEffect

--- 11-worldwise/worldwise/src/pages/AppLayout.tsx
+++ 11-worldwise/worldwise/src/pages/AppLayout.tsx
@@ -1,14 +1,33 @@
+import type { AuthContextValueType } from '../types/contexts/types.ts';
+import { useAuth } from '../contexts/FakeAuthContext.tsx';
+import { useNavigate, type NavigateFunction } from 'react-router';
+import { useEffect } from 'react';
 import styles from '../styles/pages/AppLayout.module.scss';
 
 import Sidebar from '../components/Sidebar.tsx';
 import Map from '../components/Map.tsx';
+import User from '../components/User.tsx';
 
 function AppLayout() {
+	const { isAuthenticated, user }: AuthContextValueType = useAuth();
+	const navigate: NavigateFunction = useNavigate();
+
+	useEffect(() => {
+		if (!isAuthenticated || !user) navigate('/', { replace: true });
+
+		return () => {};
+	}, [isAuthenticated, user, navigate]);
+
 	return (
-		<div className={styles.app}>
-			<Sidebar />
-			<Map />
-		</div>
+		<>
+			{isAuthenticated && (
+				<div className={styles.app}>
+					<Sidebar />
+					<User />
+					<Map />
+				</div>
+			)}
+		</>
 	);
 }
 
