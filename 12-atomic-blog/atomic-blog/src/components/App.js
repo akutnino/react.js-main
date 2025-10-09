e97de42be3c06bfeeedb67b6aaaa189baf1446a0
Nino Akut <akutnino@gmail.com>
2025-10-09 21:04:57 +0800

    updated atomic-blog App component

--- 12-atomic-blog/atomic-blog/src/components/App.js
+++ 12-atomic-blog/atomic-blog/src/components/App.js
@@ -14,12 +14,9 @@ function App() {
 	};
 
 	// Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
-	useEffect(
-		function () {
-			document.documentElement.classList.toggle('fake-dark-mode');
-		},
-		[isFakeDark]
-	);
+	useEffect(() => {
+		document.documentElement.classList.toggle('fake-dark-mode');
+	}, [isFakeDark]);
 
 	return (
 		<section>
