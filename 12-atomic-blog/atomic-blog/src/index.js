97777077fcd630a348fea4b82acc6dedc20925a2
Nino Akut <akutnino@gmail.com>
2025-10-17 14:23:28 +0800

    updated atomic-blog index.js file - import App component

--- 12-atomic-blog/atomic-blog/src/index.js
+++ 12-atomic-blog/atomic-blog/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/style.css';
-import App from './components/App-memo.js';
+import App from './components/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
