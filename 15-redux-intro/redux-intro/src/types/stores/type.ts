ecf28c9543223ee6edda6071156102dd8056caec
Nino Akut <akutnino@gmail.com>
2025-11-06 00:10:53 +0800

    updated type.ts file for stores - added ReducerActionType

--- 15-redux-intro/redux-intro/src/types/stores/type.ts
+++ 15-redux-intro/redux-intro/src/types/stores/type.ts
@@ -3,3 +3,31 @@ export type ReduxBankInitialStateType = {
 	loan: number;
 	loanPurpose: string;
 };
+
+type AccountDepositActionType = {
+	type: 'account/deposit';
+	payload: number;
+};
+
+type AccountWithdrawActionType = {
+	type: 'account/withdraw';
+	payload: number;
+};
+
+type AccountRequestLoanActionType = {
+	type: 'account/requestLoan';
+	payload: {
+		amount: number;
+		purpose: string;
+	};
+};
+
+type AccountPayLoanActionType = {
+	type: 'account/payLoan';
+};
+
+export type ReducerActionType =
+	| AccountDepositActionType
+	| AccountWithdrawActionType
+	| AccountRequestLoanActionType
+	| AccountPayLoanActionType;
