4458d55265543584cba3b9ad41f1548d7540ca36
Nino Akut <akutnino@gmail.com>
2025-11-06 00:15:54 +0800

    updated Main component - imported store.ts file

--- 15-redux-intro/redux-intro/src/components/Main.tsx
+++ 15-redux-intro/redux-intro/src/components/Main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import '../styles/Index.scss';
 
 import App from './App.tsx';
+import '../stores/store.ts';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
 	<StrictMode>
