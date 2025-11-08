de2a9b99456c700b7a1e228c10429985a12be689
Nino Akut <akutnino@gmail.com>
2025-11-09 03:41:18 +0800

    updated store.ts file - moved out the reducers and actions

--- 15-redux-intro/redux-intro/src/stores/store.ts
+++ 15-redux-intro/redux-intro/src/stores/store.ts
@@ -1,94 +1,8 @@
 import { combineReducers, legacy_createStore as createStore } from 'redux';
-import type {
-	AccountDepositActionType,
-	AccountPayLoanActionType,
-	AccountRequestLoanActionType,
-	AccountWithdrawActionType,
-	CustomerCreateCustomerActionType,
-	CustomerInitialStateType,
-	AccountReducerActionType,
-	ReduxBankInitialStateType,
-	CustomerReducerActionType,
-	CustomerUpdateNameActionType,
-} from '../types/stores/type.ts';
-
-const REDUX_BANK_INITIAL_STATE: ReduxBankInitialStateType = {
-	balance: 0,
-	loan: 0,
-	loanPurpose: '',
-};
-
-const CUSTOMER_INITIAL_STATE: CustomerInitialStateType = {
-	fullName: '',
-	nationalID: '',
-	createdAt: '',
-};
-
-function accountReducer(
-	currentState: ReduxBankInitialStateType = REDUX_BANK_INITIAL_STATE,
-	action: AccountReducerActionType
-): ReduxBankInitialStateType {
-	switch (action.type) {
-		case 'account/deposit': {
-			return {
-				...currentState,
-				balance: currentState.balance + action.payload,
-			};
-		}
-		case 'account/withdraw': {
-			return {
-				...currentState,
-				balance: currentState.balance - action.payload,
-			};
-		}
-		case 'account/requestLoan': {
-			if (currentState.loan > 0) return currentState;
-
-			return {
-				...currentState,
-				balance: currentState.balance + action.payload.amount,
-				loan: action.payload.amount,
-				loanPurpose: action.payload.purpose,
-			};
-		}
-		case 'account/payLoan': {
-			return {
-				...currentState,
-				balance: currentState.balance - currentState.loan,
-				loan: 0,
-				loanPurpose: '',
-			};
-		}
-		default: {
-			return currentState;
-		}
-	}
-}
-
-function customerReducer(
-	currentState: CustomerInitialStateType = CUSTOMER_INITIAL_STATE,
-	action: CustomerReducerActionType
-): CustomerInitialStateType {
-	switch (action.type) {
-		case 'customer/createCustomer': {
-			return {
-				...currentState,
-				fullName: action.payload.fullName,
-				nationalID: action.payload.nationalID,
-				createdAt: action.payload.createdAt,
-			};
-		}
-		case 'customer/updateName': {
-			return {
-				...currentState,
-				fullName: action.payload,
-			};
-		}
-		default: {
-			return currentState;
-		}
-	}
-}
+import { accountReducer } from './reducers/accountReducer.ts';
+import { customerReducer } from './reducers/customerReducer.ts';
+import { deposit, payLoan, requestLoan, withdraw } from './actions/accountActions.ts';
+import { createCustomer, updateName } from './actions/customerActions.ts';
 
 const rootReducer = combineReducers({
 	account: accountReducer,
@@ -97,63 +11,11 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
-function deposit(depositAmount: number): AccountDepositActionType {
-	return {
-		type: 'account/deposit',
-		payload: depositAmount,
-	};
-}
-
-function withdraw(withdrawAmount: number): AccountWithdrawActionType {
-	return {
-		type: 'account/withdraw',
-		payload: withdrawAmount,
-	};
-}
-
-function requestLoan(
-	requestLoanAmount: number,
-	loanPurpose: string
-): AccountRequestLoanActionType {
-	return {
-		type: 'account/requestLoan',
-		payload: {
-			amount: requestLoanAmount,
-			purpose: loanPurpose,
-		},
-	};
-}
-
-function payLoan(): AccountPayLoanActionType {
-	return { type: 'account/payLoan' };
-}
-
 store.dispatch(deposit(500));
 store.dispatch(withdraw(200));
 store.dispatch(requestLoan(1000, 'buy a car'));
 store.dispatch(payLoan());
 console.log(store.getState());
-
-function createCustomer(
-	fullName: string,
-	nationalID: string
-): CustomerCreateCustomerActionType {
-	return {
-		type: 'customer/createCustomer',
-		payload: {
-			fullName,
-			nationalID,
-			createdAt: new Date().toISOString(),
-		},
-	};
-}
-
-function updateName(updatedFullName: string): CustomerUpdateNameActionType {
-	return {
-		type: 'customer/updateName',
-		payload: updatedFullName,
-	};
-}
 
 store.dispatch(createCustomer('Nino Akut', '121212'));
 console.log(store.getState());
