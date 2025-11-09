8eacef32917cdaa858facb182beaf9597b3c6ea7
Nino Akut <akutnino@gmail.com>
2025-11-10 01:20:08 +0800

    created types.ts file for reducers - moved all related types from type.ts

---
+++ 15-redux-intro/redux-intro/src/types/stores/reducers/types.ts
@@ -0,0 +1,11 @@
+export type ReduxBankInitialStateType = {
+	balance: number;
+	loan: number;
+	loanPurpose: string;
+};
+
+export type CustomerInitialStateType = {
+	fullName: string;
+	nationalID: string;
+	createdAt: string;
+};
