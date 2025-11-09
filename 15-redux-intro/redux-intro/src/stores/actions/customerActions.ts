8f151052ec27e70b30510768fefa02c75db2ab1e
Nino Akut <akutnino@gmail.com>
2025-11-10 01:29:32 +0800

    created customerActions.ts file - moved related action creators from store.ts

---
+++ 15-redux-intro/redux-intro/src/stores/actions/customerActions.ts
@@ -0,0 +1,25 @@
+import type {
+	CustomerCreateCustomerActionType,
+	CustomerUpdateNameActionType,
+} from '../../types/stores/actions/types.ts';
+
+export function createCustomer(
+	fullName: string,
+	nationalID: string
+): CustomerCreateCustomerActionType {
+	return {
+		type: 'customer/createCustomer',
+		payload: {
+			fullName,
+			nationalID,
+			createdAt: new Date().toISOString(),
+		},
+	};
+}
+
+export function updateName(updatedFullName: string): CustomerUpdateNameActionType {
+	return {
+		type: 'customer/updateName',
+		payload: updatedFullName,
+	};
+}
