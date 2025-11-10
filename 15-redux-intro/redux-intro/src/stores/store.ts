85883b955c281533bcac2ef3830fcac76b818d3e
Nino Akut <akutnino@gmail.com>
2025-11-11 04:13:03 +0800

    updated store.ts file - removed redundant store dispatch functions

--- 15-redux-intro/redux-intro/src/stores/store.ts
+++ 15-redux-intro/redux-intro/src/stores/store.ts
@@ -1,8 +1,6 @@
 import { combineReducers, legacy_createStore as createStore } from 'redux';
 import { accountReducer } from './reducers/accountReducer.ts';
 import { customerReducer } from './reducers/customerReducer.ts';
-import { deposit, payLoan, requestLoan, withdraw } from './actions/accountActions.ts';
-import { createCustomer, updateName } from './actions/customerActions.ts';
 
 const rootReducer = combineReducers({
 	account: accountReducer,
@@ -11,13 +9,4 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
-store.dispatch(deposit(500));
-store.dispatch(withdraw(200));
-store.dispatch(requestLoan(1000, 'buy a car'));
-store.dispatch(payLoan());
-console.log(store.getState());
-
-store.dispatch(createCustomer('Nino Akut', '121212'));
-console.log(store.getState());
-store.dispatch(updateName('LeBron James'));
-console.log(store.getState());
+export default store;
