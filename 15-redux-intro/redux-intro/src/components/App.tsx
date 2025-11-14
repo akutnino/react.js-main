0be5d72436469f1fada06d347a8ccbdfad7fad3a
Nino Akut <akutnino@gmail.com>
2025-11-15 04:06:28 +0800

    updated App component - used useSelector, added dynamic logic

--- 15-redux-intro/redux-intro/src/components/App.tsx
+++ 15-redux-intro/redux-intro/src/components/App.tsx
@@ -1,16 +1,26 @@
+import { useSelector } from 'react-redux';
+import type { RootState } from '../types/stores/types.ts';
+
 import CreateCustomer from './CreateCustomer.tsx';
 import Customer from './Customer.tsx';
 import AccountOperations from './AccountOperations.tsx';
 import BalanceDisplay from './BalanceDisplay.tsx';
 
 function App() {
+	const customerName: string = useSelector((store: RootState) => store.customer.fullName);
+
 	return (
 		<div>
 			<h1>🏦 The React-Redux Bank ⚛️</h1>
-			<CreateCustomer />
-			<Customer />
-			<AccountOperations />
-			<BalanceDisplay />
+			{!customerName && <CreateCustomer />}
+
+			{customerName && (
+				<>
+					<Customer />
+					<AccountOperations />
+					<BalanceDisplay />
+				</>
+			)}
 		</div>
 	);
 }
