96c401a3c6c3e562722f0f1562f1f3a7a8e12f97
Nino Akut <akutnino@gmail.com>
2025-11-17 06:22:13 +0800

    updated CreateCustomer component - added programmatic redux logic

--- 15-redux-intro/redux-intro/src/components/CreateCustomer.tsx
+++ 15-redux-intro/redux-intro/src/components/CreateCustomer.tsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useState, type ChangeEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import type { Dispatch } from 'redux';
+import { createCustomer } from '../stores/actions/customerActions.ts';
 
-function Customer() {
+function CreateCustomer() {
 	const [fullName, setFullName] = useState<string>('');
 	const [nationalId, setNationalId] = useState<string>('');
+	const dispatch: Dispatch = useDispatch();
 
-	const handleClick = () => {};
+	const handleFullNameInput = (event: ChangeEvent<HTMLInputElement>) => {
+		setFullName(event.target.value);
+	};
+
+	const handleIDInput = (event: ChangeEvent<HTMLInputElement>) => {
+		setNationalId(event.target.value);
+	};
+
+	const handleCreateCustomer = () => {
+		if (!fullName || !nationalId) return;
+
+		dispatch(createCustomer(fullName, nationalId));
+	};
 
 	return (
 		<div>
@@ -14,20 +30,25 @@ function Customer() {
 					<label>Customer full name</label>
 					<input
 						value={fullName}
-						onChange={(event) => setFullName(event.target.value)}
+						onChange={handleFullNameInput}
 					/>
 				</div>
 				<div>
 					<label>National ID</label>
 					<input
 						value={nationalId}
-						onChange={(event) => setNationalId(event.target.value)}
+						onChange={handleIDInput}
 					/>
 				</div>
-				<button onClick={handleClick}>Create new customer</button>
+				<button
+					type='button'
+					onClick={handleCreateCustomer}
+				>
+					Create new customer
+				</button>
 			</div>
 		</div>
 	);
 }
 
-export default Customer;
+export default CreateCustomer;
