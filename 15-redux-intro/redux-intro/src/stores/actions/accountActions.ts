ccffdca4eaf1ae5e7c7ac42511327143589c409f
Nino Akut <akutnino@gmail.com>
2025-11-27 01:08:52 +0800

    updated accountActions.ts file - created ThunkMiddleware in deposit action creator

--- 15-redux-intro/redux-intro/src/stores/actions/accountActions.ts
+++ 15-redux-intro/redux-intro/src/stores/actions/accountActions.ts
@@ -1,3 +1,5 @@
+import type { AsyncThunkAction } from '../../types/stores/types.ts';
+import type { CurrencyType } from '../../types/components/types.ts';
 import type {
 	AccountDepositActionType,
 	AccountPayLoanActionType,
@@ -5,11 +7,56 @@ import type {
 	AccountWithdrawActionType,
 } from '../../types/stores/actions/types.ts';
 
-export function deposit(depositAmount: number): AccountDepositActionType {
-	return {
+export function deposit(
+	depositAmount: number,
+	currency: CurrencyType
+): AccountDepositActionType | AsyncThunkAction<number> {
+	const AccountDepositAction: AccountDepositActionType = {
 		type: 'account/deposit',
 		payload: depositAmount,
 	};
+
+	const ThunkMiddleware: AsyncThunkAction<number> = async (dispatch) => {
+		try {
+			dispatch({ type: 'account/convertingCurrency' });
+
+			const fetchURL: RequestInfo = `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`;
+			const fetchOptions: RequestInit = {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			};
+
+			const response: Response = await fetch(fetchURL, fetchOptions);
+			if (!response.ok) throw new Error('Fetch Response Failed');
+
+			const data: {
+				amount: number;
+				base: CurrencyType;
+				rates: {
+					USD: number;
+				};
+			} = await response.json();
+
+			const convertedAmount: number = Number((depositAmount * data.rates.USD).toFixed(2));
+
+			dispatch({
+				type: 'account/deposit',
+				payload: convertedAmount,
+			});
+
+			return convertedAmount;
+		} catch (error) {
+			if (error instanceof Error) {
+				throw error;
+			}
+		}
+	};
+
+	if (currency === 'USD') return AccountDepositAction;
+	return ThunkMiddleware;
 }
 
 export function withdraw(withdrawAmount: number): AccountWithdrawActionType {
