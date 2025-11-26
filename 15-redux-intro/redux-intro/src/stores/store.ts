3fcdce035904f300d7c9d5a8b398c606217a263f
Nino Akut <akutnino@gmail.com>
2025-11-27 00:23:57 +0800

    updated store.ts file - added types to rootReducer and store

--- 15-redux-intro/redux-intro/src/stores/store.ts
+++ 15-redux-intro/redux-intro/src/stores/store.ts
@@ -1,12 +1,40 @@
-import { combineReducers, legacy_createStore as createStore } from 'redux';
-import { accountReducer } from './reducers/accountReducer.ts';
-import { customerReducer } from './reducers/customerReducer.ts';
+import {
+	applyMiddleware,
+	combineReducers,
+	legacy_createStore as createStore,
+} from 'redux';
+import { thunk, type ThunkMiddleware } from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { accountReducer, REDUX_BANK_INITIAL_STATE } from './reducers/accountReducer.ts';
+import { customerReducer, CUSTOMER_INITIAL_STATE } from './reducers/customerReducer.ts';
+import type {
+	AppActions,
+	AppState,
+	RootReducerType,
+	StoreType,
+} from '../types/stores/types.ts';
+import type {
+	CustomerInitialStateType,
+	ReduxBankInitialStateType,
+} from '../types/stores/reducers/types.ts';
 
-const rootReducer = combineReducers({
+const preLoadedState: {
+	account: ReduxBankInitialStateType;
+	customer: CustomerInitialStateType;
+} = {
+	account: REDUX_BANK_INITIAL_STATE,
+	customer: CUSTOMER_INITIAL_STATE,
+};
+
+const rootReducer: RootReducerType = combineReducers({
 	account: accountReducer,
 	customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store: StoreType = createStore<AppState, AppActions, {}, {}>(
+	rootReducer,
+	preLoadedState,
+	composeWithDevTools(applyMiddleware(thunk as ThunkMiddleware<AppState, AppActions>))
+);
 
-export default store;
+export { store as default, rootReducer, preLoadedState };
