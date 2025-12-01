acfff81be702ad78681903aaf26e98d6a0089b8a
Nino Akut <akutnino@gmail.com>
2025-12-02 01:51:29 +0800

    updated types.ts for components - added DepositAmountType

--- 15-redux-intro/redux-intro/src/types/components/types.ts
+++ 15-redux-intro/redux-intro/src/types/components/types.ts
@@ -1,5 +1,7 @@
 import type { BareFetcher, SWRConfiguration } from 'swr';
 
+export type DepositAmountType = null | number;
+
 export type CurrencyType = 'USD' | 'EUR' | 'GBP';
 
 export type FetcherReturnType = SWRConfiguration<any, any, BareFetcher<any>>;
