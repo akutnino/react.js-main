881283a8279389b584eb56a4d9925283840e0d61
Nino Akut <akutnino@gmail.com>
2025-12-16 02:04:39 +0800

    created SpinnerFullPage component

---
+++ 16-fast-react-pizza/fast-react-pizza/src/components/common/SpinnerFullPage.tsx
@@ -0,0 +1,11 @@
+function SpinnerFullPage() {
+	return (
+		<div className={'spinnerFullpage'}>
+			<div className={'spinnerContainer'}>
+				<div className={'spinner'}></div>
+			</div>
+		</div>
+	);
+}
+
+export default SpinnerFullPage;
