a932558c9db2ddeead38103314acd40e16cbd9c4
Nino Akut <akutnino@gmail.com>
2025-12-16 02:06:19 +0800

    updated App component - added lazy loaded routes

--- 16-fast-react-pizza/fast-react-pizza/src/components/App.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/App.tsx
@@ -1,10 +1,48 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
+import { lazy } from 'react';
+
+import Layout from './common/Layout.tsx';
+
+const Home = lazy(() => import('./common/Home.tsx'));
+const Menu = lazy(() => import('./features/menu/Menu.tsx'));
+const Cart = lazy(() => import('./features/cart/Cart.tsx'));
+const CreateOrder = lazy(() => import('./features/order/CreateOrder.tsx'));
+const Order = lazy(() => import('./features/order/Order.tsx'));
+const Error = lazy(() => import('./common/Error.tsx'));
 
 function App() {
 	return (
 		<BrowserRouter basename='/'>
 			<Routes>
-				<Route />
+				<Route
+					path='/'
+					element={<Layout />}
+				>
+					<Route
+						index={true}
+						element={<Home />}
+					/>
+					<Route
+						path='menu'
+						element={<Menu />}
+					/>
+					<Route
+						path='cart'
+						element={<Cart />}
+					/>
+					<Route
+						path='order/new'
+						element={<CreateOrder />}
+					/>
+					<Route
+						path='order/:orderId'
+						element={<Order />}
+					/>
+					<Route
+						path='*'
+						element={<Error />}
+					/>
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
