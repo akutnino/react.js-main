1f32bf119c7482dc68b0bd89992fd4ed5a20019a
Nino Akut <akutnino@gmail.com>
2025-12-17 23:45:52 +0800

    updated calcMinutesLeft.ts file - added types to its variables

--- 16-fast-react-pizza/fast-react-pizza/src/utilities/calcMinutesLeft.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/utilities/calcMinutesLeft.ts
@@ -1,6 +1,6 @@
 function calcMinutesLeft(date: string): number {
-	const d1 = new Date().getTime();
-	const d2 = new Date(date).getTime();
+	const d1: number = new Date().getTime();
+	const d2: number = new Date(date).getTime();
 	return Math.round((d2 - d1) / 60000);
 }
 
