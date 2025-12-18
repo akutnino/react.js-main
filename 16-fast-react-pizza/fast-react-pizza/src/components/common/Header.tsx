13baeb72552c05c99bcf5888b66e64f6590bf7c5
Nino Akut <akutnino@gmail.com>
2025-12-18 23:57:03 +0800

    created Header component

---
+++ 16-fast-react-pizza/fast-react-pizza/src/components/common/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router';
+
+function Header() {
+	return (
+		<header>
+			<Link to={'/'}>Fast React Pizza Co.</Link>
+
+			<p>Nino Akut</p>
+		</header>
+	);
+}
+
+export default Header;
