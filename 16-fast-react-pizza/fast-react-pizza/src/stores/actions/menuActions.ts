026bde816d12b7f2864b9fd3376aedcacf98eb34
Nino Akut <akutnino@gmail.com>
2025-12-22 04:38:37 +0800

    created menuActions.ts file - created fetchData action

---
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/actions/menuActions.ts
@@ -0,0 +1,45 @@
+import type { FetchDataResponseType } from '../../types/stores/actions/menu-types.ts';
+import type { AsyncThunkAction } from '../../types/stores/types.ts';
+
+export function fetchData(): AsyncThunkAction {
+	const ThunkMiddleWare: AsyncThunkAction = async (dispatch) => {
+		try {
+			dispatch({
+				type: 'menu/fetchStart',
+			});
+
+			const fetchURL: RequestInfo = import.meta.env.VITE_MENU_API;
+			const fetchOptions: RequestInit = {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			};
+
+			const response: Response = await fetch(fetchURL, fetchOptions);
+			if (!response.ok) throw new Error('Menu Data Fetching Failed');
+
+			const data: FetchDataResponseType = await response.json();
+			if (data.status !== 'success') throw new Error(`Status Not Successful: ${data.status}`); // prettier-ignore
+
+			dispatch({
+				type: 'menu/fetchSuccess',
+				payload: data,
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				dispatch({
+					type: 'menu/fetchError',
+					payload: error.message,
+				});
+			}
+		} finally {
+			dispatch({
+				type: 'menu/fetchEnd',
+			});
+		}
+	};
+
+	return ThunkMiddleWare;
+}
