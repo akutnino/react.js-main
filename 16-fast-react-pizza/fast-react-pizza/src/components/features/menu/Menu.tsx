ad86701bb748e5895b8d6f0d6d11642a47321024
Nino Akut <akutnino@gmail.com>
2025-12-23 23:36:22 +0800

    updated Menu component - added swr for fetching data

--- 16-fast-react-pizza/fast-react-pizza/src/components/features/menu/Menu.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/features/menu/Menu.tsx
@@ -1,5 +1,36 @@
+import useSWR from 'swr';
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, AppState } from '../../../types/stores/types.ts';
+import { fetchData } from '../../../stores/actions/menuActions.ts';
+import type {
+	MenuDataType,
+	MenuInitialStateType,
+} from '../../../types/stores/reducers/types.ts';
+
+const MenuKey = (menu: MenuDataType | null) => {
+	return () => {
+		if (menu !== null) return null;
+		return import.meta.env.VITE_MENU_API;
+	};
+};
+
+const MenuFetcher = (dispatch: AppDispatch, menu: MenuDataType | null) => {
+	return () => {
+		if (menu !== null) return;
+		return dispatch(fetchData());
+	};
+};
+
 function Menu() {
-  return <h1>Menu</h1>;
+	const dispatch: AppDispatch = useDispatch();
+	const { errorMessage, isLoading, menu }: MenuInitialStateType = useSelector(
+		(store: AppState) => store.menu
+	);
+
+	useSWR(MenuKey(menu), MenuFetcher(dispatch, menu));
+	console.log(`is Error? ${errorMessage ? 'yes' : 'no'}`, isLoading, menu);
+
+	return <h1>Menu</h1>;
 }
 
 export default Menu;
