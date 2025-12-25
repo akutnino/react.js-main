abdaaeae052f0cdb9fda01e6207a0def2ce02173
Nino Akut <akutnino@gmail.com>
2025-12-26 03:48:35 +0800

    updated Header component - added SearchOrder component

--- 16-fast-react-pizza/fast-react-pizza/src/components/common/Header.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/common/Header.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router';
+import SearchOrder from '../features/order/SearchOrder.tsx';
 
 function Header() {
 	return (
 		<header>
 			<Link to={'/'}>Fast React Pizza Co.</Link>
-
+			<SearchOrder />
 			<p>Nino Akut</p>
 		</header>
 	);
