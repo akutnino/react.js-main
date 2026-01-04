21c76aa83f3afc81151470dced53ab1dd9db4b00
Nino Akut <akutnino@gmail.com>
2026-01-05 04:56:22 +0800

    updated Layout component - added useLayoutEffect

--- 16-fast-react-pizza/fast-react-pizza/src/components/common/Layout.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/common/Layout.tsx
@@ -1,6 +1,11 @@
-import { Suspense } from 'react';
-import { Outlet, useLocation, type Location } from 'react-router';
+import { Suspense, useLayoutEffect } from 'react';
+import { Outlet, useLocation, useParams, type Location, type Params } from 'react-router';
 import { ErrorBoundary } from 'react-error-boundary';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectOrder } from '../../stores/selectors/orderSelectors.ts';
+import { clearError } from '../../stores/actions/orderActions.ts';
+import type { OrderInitialStateType } from '../../types/stores/reducers/order-types.ts';
+import type { AppDispatch } from '../../types/stores/types.ts';
 
 import { Error } from '../App.tsx';
 import SpinnerFullPage from './SpinnerFullPage.tsx';
@@ -8,15 +13,24 @@ import Header from './Header.tsx';
 import CartOverview from '../features/cart/CartOverview.tsx';
 
 function Layout() {
-	const loaction: Location = useLocation();
+	const { errorMessage }: OrderInitialStateType = useSelector(selectOrder);
+	const { orderId } = useParams<Readonly<Params<'orderId'>>>();
+
+	const dispatch: AppDispatch = useDispatch();
+	const location: Location = useLocation();
+
+	useLayoutEffect(() => {
+		if (orderId === undefined && errorMessage !== null) dispatch(clearError());
+		return () => {};
+	}, [orderId, errorMessage, dispatch]);
 
 	return (
 		<ErrorBoundary
-			key={loaction.key}
+			key={location.key}
 			fallback={<Error />}
 		>
 			<Suspense
-				key={loaction.key}
+				key={location.key}
 				fallback={<SpinnerFullPage />}
 			>
 				<Header />
