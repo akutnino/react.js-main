641e4e8f4853d42532b30eda5d68c7633d57e06f
Nino Akut <akutnino@gmail.com>
2026-01-05 04:54:52 +0800

    updated Error component - added dispatch and selectors

--- 16-fast-react-pizza/fast-react-pizza/src/components/common/Error.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/common/Error.tsx
@@ -1,16 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, type NavigateFunction } from 'react-router';
 import { selectMenu } from '../../stores/selectors/menuSelectors.ts';
+import { selectOrder } from '../../stores/selectors/orderSelectors.ts';
+import { resetState } from '../../stores/actions/orderActions.ts';
 import type { MenuInitialStateType } from '../../types/stores/reducers/menu-types.ts';
+import type { OrderInitialStateType } from '../../types/stores/reducers/order-types.ts';
+import type { AppDispatch } from '../../types/stores/types.ts';
 
 function Error() {
+	const dispatch: AppDispatch = useDispatch();
 	const navigate: NavigateFunction = useNavigate();
-	const { errorMessage }: MenuInitialStateType = useSelector(selectMenu);
+	const { errorMessage: menuErrorMessage }: MenuInitialStateType = useSelector(selectMenu); // prettier-ignore
+	const { errorMessage: orderErrorMessage }: OrderInitialStateType = useSelector(selectOrder); // prettier-ignore
 
-	const isError: boolean = errorMessage !== '';
+	const isError: boolean = menuErrorMessage !== '' || orderErrorMessage !== '';
+	const errorMessage = menuErrorMessage || orderErrorMessage;
 
 	const handleBack = () => {
 		navigate('/', { replace: true });
+		dispatch(resetState());
 	};
 
 	return (
