9a8f793bc73f1d22f3c3d391af202790a82a939b
Nino Akut <akutnino@gmail.com>
2026-01-05 04:48:27 +0800

    created orderReducer.ts file - created orderReducer function

---
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/reducers/orderReducer.ts
@@ -0,0 +1,76 @@
+import type { OrderReducerActionType } from '../../types/stores/actions/order-types.ts';
+import type { OrderInitialStateType } from '../../types/stores/reducers/order-types.ts';
+
+const ORDER_INITIAL_STATE: OrderInitialStateType = {
+	order: null,
+	isLoading: false,
+	errorMessage: null,
+};
+
+function orderReducer(
+	currentState = ORDER_INITIAL_STATE,
+	action: OrderReducerActionType
+): OrderInitialStateType {
+	switch (action.type) {
+		case 'order/fetchStart': {
+			return {
+				...currentState,
+				isLoading: true,
+				errorMessage: null,
+			};
+		}
+		case 'order/fetchSuccess': {
+			return {
+				...currentState,
+				isLoading: false,
+				order: action.payload.data,
+			};
+		}
+		case 'order/createOrderSuccess': {
+			return {
+				...currentState,
+				isLoading: false,
+				order: action.payload.data,
+			};
+		}
+		case 'order/updateOrderSuccess': {
+			return {
+				...currentState,
+				isLoading: false,
+			};
+		}
+		case 'order/fetchError': {
+			return {
+				...currentState,
+				errorMessage: action.payload,
+				isLoading: false,
+				order: null,
+			};
+		}
+		case 'order/fetchEnd': {
+			return {
+				...currentState,
+				isLoading: false,
+			};
+		}
+		case 'order/clearError': {
+			return {
+				...currentState,
+				errorMessage: null,
+			};
+		}
+		case 'order/resetState': {
+			return {
+				...currentState,
+				order: null,
+				isLoading: false,
+				errorMessage: null,
+			};
+		}
+		default: {
+			return currentState;
+		}
+	}
+}
+
+export { orderReducer, ORDER_INITIAL_STATE };
