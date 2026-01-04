50a8ff653e88edeecbfae1af9c15f872d52c143b
Nino Akut <akutnino@gmail.com>
2026-01-05 05:03:27 +0800

    updated Order component - added selector

--- 16-fast-react-pizza/fast-react-pizza/src/components/features/order/Order.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/features/order/Order.tsx
@@ -1,76 +1,61 @@
-// Test ID: IIDSAT
+// Test ID: IIDSAT, CQE92U
+// Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
 
+import { useSelector } from 'react-redux';
 import { calcMinutesLeft } from '../../../utilities/calcMinutesLeft.ts';
 import { formatCurrency } from '../../../utilities/formatCurrency.ts';
 import { formatDate } from '../../../utilities/formatDate.ts';
+import { selectOrder } from '../../../stores/selectors/orderSelectors.ts';
+import type { OrderInitialStateType } from '../../../types/stores/reducers/order-types.ts';
 
-const order = {
-	id: 'ABCDEF',
-	customer: 'Jonas',
-	phone: '123456789',
-	address: 'Arroios, Lisbon , Portugal',
-	priority: true,
-	estimatedDelivery: '2027-04-25T10:00:00',
-	cart: [
-		{
-			pizzaId: 7,
-			name: 'Napoli',
-			quantity: 3,
-			unitPrice: 16,
-			totalPrice: 48,
-		},
-		{
-			pizzaId: 5,
-			name: 'Diavola',
-			quantity: 2,
-			unitPrice: 16,
-			totalPrice: 32,
-		},
-		{
-			pizzaId: 3,
-			name: 'Romana',
-			quantity: 1,
-			unitPrice: 15,
-			totalPrice: 15,
-		},
-	],
-	position: '-9.000,38.000',
-	orderPrice: 95,
-	priorityPrice: 19,
-};
+import Error from '../../common/Error.tsx';
+import LoadingIndicator from '../../common/LoadingIndicator.tsx';
 
 function Order() {
-	// Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
-	const { id, status, priority, priorityPrice, orderPrice, estimatedDelivery, cart } =
-		order;
-	const deliveryIn = calcMinutesLeft(estimatedDelivery);
+	const { errorMessage, isLoading, order }: OrderInitialStateType = useSelector(selectOrder); // prettier-ignore
+
+	const isError: boolean = !isLoading && errorMessage !== null;
+	const isOrderLoaded: boolean = !isLoading && !isError && order !== null;
 
 	return (
-		<div>
-			<div>
-				<h2>Status</h2>
+		<>
+			{isLoading && <LoadingIndicator />}
 
+			{isOrderLoaded && (
 				<div>
-					{priority && <span>Priority</span>}
-					<span>{status} order</span>
+					<div>
+						<h2>Status</h2>
+
+						<div>
+							{order!.priority && <span>Priority</span>}
+							<span>{status} order</span>
+						</div>
+					</div>
+
+					<div>
+						<p>
+							{calcMinutesLeft(order!.estimatedDelivery) >= 0
+								? `Only ${calcMinutesLeft(order!.estimatedDelivery)} minutes left 😃`
+								: 'Order should have arrived'}
+						</p>
+						<p>(Estimated delivery: {formatDate(order!.estimatedDelivery)})</p>
+					</div>
+
+					<div>
+						<p>Price pizza: {formatCurrency(order!.orderPrice)}</p>
+						{order!.priority && (
+							<p>Price priority: {formatCurrency(order!.priorityPrice)}</p>
+						)}
+						<p>
+							To pay on delivery:{' '}
+							{formatCurrency(order!.orderPrice + order!.priorityPrice)}
+						</p>
+					</div>
 				</div>
-			</div>
+			)}
 
-			<div>
-				<p>
-					{deliveryIn >= 0
-						? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-						: 'Order should have arrived'}
-				</p>
-				<p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
-			</div>
-
-			<div>
-				<p>Price pizza: {formatCurrency(orderPrice)}</p>
-				{priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-				<p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
-			</div>
-		</div>
+			{isError && <Error />}
+		</>
 	);
 }
 
