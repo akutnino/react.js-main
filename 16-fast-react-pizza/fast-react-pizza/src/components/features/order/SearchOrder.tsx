18bc1c1abc3cc210c4a5ee4968b9be8b0e742e6e
Nino Akut <akutnino@gmail.com>
2026-01-05 05:06:54 +0800

    updated SearchOrder component - added if checks in submit handler

--- 16-fast-react-pizza/fast-react-pizza/src/components/features/order/SearchOrder.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/features/order/SearchOrder.tsx
@@ -1,8 +1,17 @@
 import { useState, type ChangeEvent, type FormEvent } from 'react';
-import { useNavigate, type NavigateFunction } from 'react-router';
+import { useNavigate, useParams, type NavigateFunction, type Params } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchOrderData } from '../../../stores/actions/orderActions.ts';
+import { selectOrder } from '../../../stores/selectors/orderSelectors.ts';
+import type { AppDispatch } from '../../../types/stores/types.ts';
+import type { OrderInitialStateType } from '../../../types/stores/reducers/order-types.ts';
 
 function SearchOrder() {
 	const [query, setQuery] = useState<string>('');
+	const { orderId } = useParams<Readonly<Params<'orderId'>>>();
+	const { order }: OrderInitialStateType = useSelector(selectOrder);
+
+	const dispatch: AppDispatch = useDispatch();
 	const navigate: NavigateFunction = useNavigate();
 
 	const handleInputNumber = (event: ChangeEvent<HTMLInputElement>) => {
@@ -12,8 +21,23 @@ function SearchOrder() {
 	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 
+		// const QueryEqualsPathParams = query === orderId;
+
 		if (!query) return;
-		navigate(`order/${query}`, { replace: true });
+
+		if (query === orderId && query === order?.id) {
+			navigate(`order/${query}`, { replace: true });
+		}
+
+		if (query !== orderId && query !== order?.id) {
+			navigate(`order/${query}`, { replace: true });
+			dispatch(fetchOrderData(query));
+		}
+
+		if (query !== orderId && query === order?.id) {
+			navigate(`order/${query}`, { replace: true });
+		}
+
 		setQuery('');
 	};
 
