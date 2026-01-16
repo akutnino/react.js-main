f7a2f2832b5faf93a91641c72c8711eca91e86e4
Nino Akut <akutnino@gmail.com>
2026-01-17 03:22:23 +0800

    updated Username component - added selectUser selector

--- 16-fast-react-pizza/fast-react-pizza/src/components/features/user/Username.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/features/user/Username.tsx
@@ -1,5 +1,12 @@
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../../stores/selectors/userSelectors.ts';
+import type { UserInitialStateType } from '../../../types/stores/reducers/user-types.ts';
+
 function Username() {
-	return <div className='hidden text-sm font-semibold md:block'>Nino</div>;
+	const { username }: UserInitialStateType = useSelector(selectUser);
+
+	if (username === null) return;
+	return <div className='hidden text-sm font-semibold md:block'>{username}</div>;
 }
 
 export default Username;
