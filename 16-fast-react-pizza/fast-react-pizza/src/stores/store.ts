c7d8f25e4f9ba771375ff98e56e51987b1fbd10c
Nino Akut <akutnino@gmail.com>
2026-01-19 04:30:10 +0800

    updated store.ts file - added cart to rootReducer and preLoadedState

--- 16-fast-react-pizza/fast-react-pizza/src/stores/store.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/store.ts
@@ -8,9 +8,11 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 import { MENU_INITIAL_STATE, menuReducer } from './reducers/menuReducer.ts';
 import { ORDER_INITIAL_STATE, orderReducer } from './reducers/orderReducer.ts';
 import { USER_INITIAL_STATE, userReducer } from './reducers/userReducer.ts';
+import { CART_INITIAL_STATE, cartReducer } from './reducers/cartReducer.ts';
 import type { UserInitialStateType } from '../types/stores/reducers/user-types.ts';
 import type { MenuInitialStateType } from '../types/stores/reducers/menu-types.ts';
 import type { OrderInitialStateType } from '../types/stores/reducers/order-types.ts';
+import type { CartInitialStateType } from '../types/stores/reducers/cart-types.ts';
 import type {
 	AppActions,
 	AppState,
@@ -22,16 +24,19 @@ const preLoadedState: {
 	menu: MenuInitialStateType;
 	order: OrderInitialStateType;
 	user: UserInitialStateType;
+	cart: CartInitialStateType;
 } = {
 	menu: MENU_INITIAL_STATE,
 	order: ORDER_INITIAL_STATE,
 	user: USER_INITIAL_STATE,
+	cart: CART_INITIAL_STATE,
 };
 
 const rootReducer: RootReducerType = combineReducers({
 	menu: menuReducer,
 	order: orderReducer,
 	user: userReducer,
+	cart: cartReducer,
 });
 
 const store: StoreType = createStore(
