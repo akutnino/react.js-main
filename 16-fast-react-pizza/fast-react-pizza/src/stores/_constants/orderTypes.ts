68f30c37d713980197b17dea743a87a641cbaa65
Nino Akut <akutnino@gmail.com>
2026-01-21 01:42:29 +0800

    created orderTypes.ts file - created orderTypes

---
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/_constants/orderTypes.ts
@@ -0,0 +1,17 @@
+export const orderTypes: {
+	ORDER_FETCH: 'order/fetchStart';
+	ORDER_FETCH_SUCCESS: 'order/fetchSuccess';
+	ORDER_FETCH_ERROR: 'order/fetchError';
+	ORDER_CREATE: 'order/createOrderSuccess';
+	ORDER_UPDATE: 'order/updateOrderSuccess';
+	ORDER_CLEAR_ERROR: 'order/clearError';
+	ORDER_RESET_STATE: 'order/resetState';
+} = {
+	ORDER_FETCH: 'order/fetchStart',
+	ORDER_FETCH_SUCCESS: 'order/fetchSuccess',
+	ORDER_FETCH_ERROR: 'order/fetchError',
+	ORDER_CREATE: 'order/createOrderSuccess',
+	ORDER_UPDATE: 'order/updateOrderSuccess',
+	ORDER_CLEAR_ERROR: 'order/clearError',
+	ORDER_RESET_STATE: 'order/resetState',
+};
