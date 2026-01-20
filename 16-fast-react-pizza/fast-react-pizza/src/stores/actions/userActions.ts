a35d2a4ed9db1aa5911e1ea4133a438014db4eb2
Nino Akut <akutnino@gmail.com>
2026-01-21 01:59:34 +0800

    updated userActions.ts file - import userTypes

--- 16-fast-react-pizza/fast-react-pizza/src/stores/actions/userActions.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/actions/userActions.ts
@@ -1,8 +1,9 @@
 import type { UserUpdateUsernameActionType } from '../../types/stores/actions/user-types.ts';
+import { userTypes } from '../_constants/userTypes.ts';
 
 export function updatedUsername(newUsername: string): UserUpdateUsernameActionType {
 	return {
-		type: 'user/updateUsername',
+		type: userTypes.USER_UPDATE_USERNAME,
 		payload: newUsername,
 	};
 }
