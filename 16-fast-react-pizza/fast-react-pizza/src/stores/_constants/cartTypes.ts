eec5778d7ce8ee6ccd7d205f7d598eae315ff07f
Nino Akut <akutnino@gmail.com>
2026-01-22 04:28:04 +0800

    created cartTypes.ts file - created cartTypes

---
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/_constants/cartTypes.ts
@@ -0,0 +1,13 @@
+export const cartTypes: {
+	CART_ADD_ITEM: 'cart/addCartItem';
+	CART_DELETE_ITEM: 'cart/deleteCartItem';
+	CART_INCREASE_ITEM_QUANTITY: 'cart/increaseCartItemQuantity';
+	CART_DECREASE_ITEM_QUANTITY: 'cart/decreaseCartItemQuantity';
+	CART_CLEAR: 'cart/clearCart';
+} = {
+	CART_ADD_ITEM: 'cart/addCartItem',
+	CART_DELETE_ITEM: 'cart/deleteCartItem',
+	CART_INCREASE_ITEM_QUANTITY: 'cart/increaseCartItemQuantity',
+	CART_DECREASE_ITEM_QUANTITY: 'cart/decreaseCartItemQuantity',
+	CART_CLEAR: 'cart/clearCart',
+};
