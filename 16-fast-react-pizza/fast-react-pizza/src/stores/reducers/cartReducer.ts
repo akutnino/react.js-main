65f56c585e1f0e740e26dfa69e56acfc27f4013a
Nino Akut <akutnino@gmail.com>
2026-01-22 04:31:58 +0800

    updated cartReducers.ts file - added case logic to all cases

--- 16-fast-react-pizza/fast-react-pizza/src/stores/reducers/cartReducer.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/reducers/cartReducer.ts
@@ -1,8 +1,41 @@
 import type { CartReducerActionType } from '../../types/stores/actions/cart-types.ts';
-import type { CartInitialStateType } from '../../types/stores/reducers/cart-types.ts';
+import type {
+	CartInitialStateType,
+	CartItemType,
+	CartType,
+} from '../../types/stores/reducers/cart-types.ts';
+import { cartTypes } from '../_constants/cartTypes.ts';
 
 const CART_INITIAL_STATE: CartInitialStateType = {
-	cart: null,
+	cart: [
+		{
+			addIngredients: [],
+			removeIngredients: [],
+			pizzaId: 12,
+			name: 'Mediterranean',
+			quantity: 2,
+			unitPrice: 16,
+			totalPrice: 32,
+		},
+		{
+			addIngredients: [],
+			removeIngredients: [],
+			pizzaId: 6,
+			name: 'Vegetale',
+			quantity: 1,
+			unitPrice: 13,
+			totalPrice: 13,
+		},
+		{
+			addIngredients: [],
+			removeIngredients: [],
+			pizzaId: 11,
+			name: 'Spinach and Mushroom',
+			quantity: 1,
+			unitPrice: 15,
+			totalPrice: 15,
+		},
+	],
 };
 
 function cartReducer(
@@ -10,29 +43,72 @@ function cartReducer(
 	action: CartReducerActionType
 ): CartInitialStateType {
 	switch (action.type) {
-		case 'cart/addCartItem': {
+		case cartTypes.CART_ADD_ITEM: {
+			if (currentState.cart === null) return { ...currentState };
+
 			return {
 				...currentState,
+				cart: [...currentState.cart, action.payload],
 			};
 		}
-		case 'cart/deleteCartItem': {
+		case cartTypes.CART_DELETE_ITEM: {
+			if (currentState.cart === null) return { ...currentState };
+
+			const filteredCart: CartItemType[] = currentState.cart.filter(
+				(item) => item.pizzaId !== action.payload
+			);
+
+			const isFilteredCartEmpty: boolean = filteredCart?.length === 0;
+			const updatedCart: CartType = isFilteredCartEmpty ? null : filteredCart;
+
 			return {
 				...currentState,
+				cart: updatedCart,
 			};
 		}
-		case 'cart/increaseCartItemQuantity': {
+		case cartTypes.CART_INCREASE_ITEM_QUANTITY: {
+			if (currentState.cart === null) return { ...currentState };
+
+			const cartItem = currentState.cart.find((item) => item.pizzaId === action.payload);
+			if (cartItem === undefined) return { ...currentState };
+
+			const updatedItemQuantity: number = cartItem.quantity + 1;
+			const updatedItemTotalPrice: number = updatedItemQuantity * cartItem.unitPrice;
+
+			const updatedCartItem: CartItemType = {
+				...cartItem,
+				quantity: updatedItemQuantity,
+				totalPrice: updatedItemTotalPrice,
+			};
+
 			return {
 				...currentState,
+				cart: [...currentState.cart, updatedCartItem],
 			};
 		}
-		case 'cart/decreaseCartItemQuantity': {
+		case cartTypes.CART_DECREASE_ITEM_QUANTITY: {
+			if (currentState.cart === null) return { ...currentState };
+
+			const cartItem = currentState.cart.find((item) => item.pizzaId === action.payload);
+			if (cartItem === undefined) return { ...currentState };
+
+			const updatedItemQuantity: number = cartItem.quantity - 1;
+			const updatedItemTotalPrice: number = updatedItemQuantity * cartItem.unitPrice;
+
+			const updatedCartItem: CartItemType = {
+				...cartItem,
+				quantity: updatedItemQuantity,
+				totalPrice: updatedItemTotalPrice,
+			};
+
 			return {
-				...currentState,
+				cart: [...currentState.cart, updatedCartItem],
 			};
 		}
-		case 'cart/clearCart': {
+		case cartTypes.CART_CLEAR: {
 			return {
 				...currentState,
+				cart: null,
 			};
 		}
 		default: {
