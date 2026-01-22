1c03a46da3fe34aec73a3a30cd455f6b4987c7db
Nino Akut <akutnino@gmail.com>
2026-01-23 03:54:20 +0800

    updated CartItem component - created handleDeleteItem

--- 16-fast-react-pizza/fast-react-pizza/src/components/features/cart/CartItem.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/features/cart/CartItem.tsx
@@ -1,9 +1,18 @@
-import type { CartItemType } from '../../../types/stores/reducers/cart-types.ts';
+import { useDispatch } from 'react-redux';
+import { cartDeleteItem } from '../../../stores/actions/cartActions.ts';
 import { formatCurrency } from '../../../utilities/formatCurrency.ts';
+import type { CartItemType } from '../../../types/stores/reducers/cart-types.ts';
+import type { AppDispatch } from '../../../types/stores/types.ts';
+
 import Button from '../../common/Button.tsx';
 
 function CartItem({ item }: { item: CartItemType }) {
-	const { name, quantity, totalPrice } = item;
+	const { name, quantity, totalPrice, pizzaId } = item;
+	const dispatch: AppDispatch = useDispatch();
+
+	const handleDeleteItem = () => {
+		dispatch(cartDeleteItem(pizzaId));
+	};
 
 	return (
 		<li className='py-3 sm:flex sm:items-center sm:justify-between'>
@@ -12,7 +21,12 @@ function CartItem({ item }: { item: CartItemType }) {
 			</p>
 			<div className='flex items-center justify-between sm:gap-6'>
 				<p className='text-sm font-bold'>{formatCurrency(totalPrice)}</p>
-				<Button type='small'>Delete</Button>
+				<Button
+					type='small'
+					onClick={handleDeleteItem}
+				>
+					Delete
+				</Button>
 			</div>
 		</li>
 	);
