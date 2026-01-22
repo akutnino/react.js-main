33add0f83c767912639af107558081a6539853f0
Nino Akut <akutnino@gmail.com>
2026-01-23 05:47:08 +0800

    updated Button component - added round type prop

--- 16-fast-react-pizza/fast-react-pizza/src/components/common/Button.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/common/Button.tsx
@@ -11,7 +11,7 @@ function Button({
 	children: ReactNode;
 	disabled?: boolean;
 	to?: string;
-	type: 'primary' | 'small' | 'secondary';
+	type: 'primary' | 'small' | 'secondary' | 'round';
 	onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 }) {
 	const base =
@@ -21,11 +21,13 @@ function Button({
 		primary: string;
 		small: string;
 		secondary: string;
+		round: string;
 	} = {
 		primary: base + ' px-4 py-3 md:px-6 md:py-4',
 		small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
 		secondary:
 			'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
+		round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
 	};
 
 	if (to)
