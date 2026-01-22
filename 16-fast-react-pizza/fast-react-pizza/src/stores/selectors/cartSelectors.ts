1a3bf1b4237fe718bcb2cb5b7455bfcde90fcc11
Nino Akut <akutnino@gmail.com>
2026-01-23 01:56:14 +0800

    updated cartSelector.ts file - created getTotalCartQuantity anc getTotalCartPrice

--- 16-fast-react-pizza/fast-react-pizza/src/stores/selectors/cartSelectors.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/selectors/cartSelectors.ts
@@ -1,6 +1,19 @@
-import type { CartInitialStateType } from '../../types/stores/reducers/cart-types.ts';
+import type {
+	CartInitialStateType,
+	CartItemType,
+} from '../../types/stores/reducers/cart-types.ts';
 import type { AppState } from '../../types/stores/types.ts';
 
 const selectCart = (store: AppState): CartInitialStateType => store.cart;
 
-export { selectCart };
+const getTotalCartQuantity = (store: AppState): number => {
+	if (store.cart.cart === null) return 0;
+	return store.cart.cart.reduce((acc, curr: CartItemType) => acc + curr.quantity, 0);
+};
+
+const getTotalCartPrice = (store: AppState): number => {
+	if (store.cart.cart === null) return 0;
+	return store.cart.cart.reduce((acc, curr: CartItemType) => acc + curr.totalPrice, 0);
+};
+
+export { selectCart, getTotalCartQuantity, getTotalCartPrice };
