d54e951d595798093fcce88663b83589e0ff7033
Nino Akut <akutnino@gmail.com>
2026-01-23 01:57:47 +0800

    updated CartOverview component - added totalCartQuantity and totalCartPrice

--- 16-fast-react-pizza/fast-react-pizza/src/components/features/cart/CartOverview.tsx
+++ 16-fast-react-pizza/fast-react-pizza/src/components/features/cart/CartOverview.tsx
@@ -1,14 +1,27 @@
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router';
+import {
+	getTotalCartPrice,
+	getTotalCartQuantity,
+} from '../../../stores/selectors/cartSelectors.ts';
+import { formatCurrency } from '../../../utilities/formatCurrency.ts';
 
 function CartOverview() {
+	const totalCartQuantity: number = useSelector(getTotalCartQuantity);
+	const totalCartPrice: number = useSelector(getTotalCartPrice);
+
 	return (
-		<div className='flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base'>
-			<p className='space-x-4 font-semibold text-stone-300 sm:space-x-6'>
-				<span>23 pizzas</span>
-				<span>$23.45</span>
-			</p>
-			<Link to='/cart'>Open cart &rarr;</Link>
-		</div>
+		<>
+			{totalCartQuantity !== 0 && (
+				<div className='flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base'>
+					<p className='space-x-4 font-semibold text-stone-300 sm:space-x-6'>
+						<span>{totalCartQuantity} pizzas</span>
+						<span>{formatCurrency(totalCartPrice)}</span>
+					</p>
+					<Link to='/cart'>Open cart &rarr;</Link>
+				</div>
+			)}
+		</>
 	);
 }
 
