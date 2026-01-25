929773804520ddfdda062a0b8ac3395338e5b1c5
Nino Akut <akutnino@gmail.com>
2026-01-26 04:20:15 +0800

    created cartTypes.ts file - added types for user fetch address

--- 16-fast-react-pizza/fast-react-pizza/src/stores/_constants/userTypes.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/_constants/userTypes.ts
@@ -1,5 +1,11 @@
 export const userTypes: {
 	USER_UPDATE_USERNAME: 'user/updateUsername';
+	USER_FETCH_ADDRESS: 'user/fetchAddress';
+	USER_FETCH_ADDRESS_SUCCESS: 'user/fetchAddressSuccess';
+	USER_FETCH_ADDRESS_ERROR: 'user/fetchAddressError';
 } = {
 	USER_UPDATE_USERNAME: 'user/updateUsername',
+	USER_FETCH_ADDRESS: 'user/fetchAddress',
+	USER_FETCH_ADDRESS_SUCCESS: 'user/fetchAddressSuccess',
+	USER_FETCH_ADDRESS_ERROR: 'user/fetchAddressError',
 };
