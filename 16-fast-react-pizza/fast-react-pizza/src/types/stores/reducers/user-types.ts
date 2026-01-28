6273c9c8e87f97a18df86b3ca34dbec84108e2e1
Nino Akut <akutnino@gmail.com>
2026-01-28 23:12:37 +0800

    updated user-types.ts for reducers - updated UserInitialStateType

--- 16-fast-react-pizza/fast-react-pizza/src/types/stores/reducers/user-types.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/types/stores/reducers/user-types.ts
@@ -1,3 +1,12 @@
+export type UserPositionStateType = {
+	latitude: string;
+	longitude: string;
+};
+
 export type UserInitialStateType = {
 	username: null | string;
+	isLoading: boolean;
+	position: null | UserPositionStateType;
+	address: null | string;
+	errorMessage: null | string;
 };
