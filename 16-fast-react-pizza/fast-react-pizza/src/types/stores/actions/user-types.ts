3b50fc81478a891e4957b192b85f110466401e17
Nino Akut <akutnino@gmail.com>
2026-01-28 23:13:47 +0800

    updated user-types.ts for actions - updated UserReducerActionType

--- 16-fast-react-pizza/fast-react-pizza/src/types/stores/actions/user-types.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/types/stores/actions/user-types.ts
@@ -1,8 +1,50 @@
 import type { userTypes } from '../../../stores/_constants/userTypes.ts';
+import type { UserPositionStateType } from '../reducers/user-types.ts';
+
+export type UserAddressObjectType = {
+	position: UserPositionStateType;
+	address: string;
+};
+
+export type UserFetchAddressResponseType = {
+	city: string;
+	continent: string;
+	continentCode: string;
+	countryCode: string;
+	countryName: string;
+	latitude: number;
+	locality: string;
+	localityInfo: { administrative: unknown[]; informative: unknown[] };
+	localityLanguageRequested: string;
+	longitude: number;
+	lookupSource: string;
+	plusCode: string;
+	postcode: string;
+	principalSubdivision: string;
+	principalSubdivisionCode: string;
+};
 
 export type UserUpdateUsernameActionType = {
 	type: typeof userTypes.USER_UPDATE_USERNAME;
 	payload: string;
 };
 
-export type UserReducerActionType = UserUpdateUsernameActionType;
+export type UserFetchAddressStartActionType = {
+	type: typeof userTypes.USER_FETCH_ADDRESS;
+};
+
+export type UserFetchAddressSuccessActionType = {
+	type: typeof userTypes.USER_FETCH_ADDRESS_SUCCESS;
+	payload: UserAddressObjectType;
+};
+
+export type UserFetchAddressErrorActionType = {
+	type: typeof userTypes.USER_FETCH_ADDRESS_ERROR;
+	payload: string;
+};
+
+export type UserReducerActionType =
+	| UserUpdateUsernameActionType
+	| UserFetchAddressStartActionType
+	| UserFetchAddressSuccessActionType
+	| UserFetchAddressErrorActionType;
