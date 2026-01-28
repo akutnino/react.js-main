2877c318090bbd8e6229861e5c442299abde0507
Nino Akut <akutnino@gmail.com>
2026-01-28 23:16:00 +0800

    updated userActions.ts file - created fetchUserAddress

--- 16-fast-react-pizza/fast-react-pizza/src/stores/actions/userActions.ts
+++ 16-fast-react-pizza/fast-react-pizza/src/stores/actions/userActions.ts
@@ -1,4 +1,10 @@
-import type { UserUpdateUsernameActionType } from '../../types/stores/actions/user-types.ts';
+import type {
+	UserAddressObjectType,
+	UserFetchAddressResponseType,
+	UserUpdateUsernameActionType,
+} from '../../types/stores/actions/user-types.ts';
+import type { UserPositionStateType } from '../../types/stores/reducers/user-types.ts';
+import type { AsyncThunkAction } from '../../types/stores/types.ts';
 import { userTypes } from '../_constants/userTypes.ts';
 
 export function updatedUsername(newUsername: string): UserUpdateUsernameActionType {
@@ -6,4 +12,63 @@ export function updatedUsername(newUsername: string): UserUpdateUsernameActionTy
 		type: userTypes.USER_UPDATE_USERNAME,
 		payload: newUsername,
 	};
+}
+
+export function fetchUserAddress(): AsyncThunkAction {
+	const ThunkMiddleware: AsyncThunkAction = async (dispatch) => {
+		try {
+			dispatch({ type: userTypes.USER_FETCH_ADDRESS });
+
+			const userGeolocationPosition: GeolocationPosition = await new Promise(
+				(resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject)
+			);
+
+			const permissionStatus: PermissionStatus = await navigator.permissions.query({
+				name: 'geolocation',
+			});
+
+			// if (permissionStatus.state === 'denied') throw new Error('Hardware Device Location Access Denied');
+			if (permissionStatus.state === 'denied') console.log('Hardware Device Location Access Denied'); //prettier-ignore
+
+			const userPosition: UserPositionStateType = {
+				latitude: `${userGeolocationPosition.coords.latitude}`,
+				longitude: `${userGeolocationPosition.coords.longitude}`,
+			};
+
+			const FETCH_PARAMS: string = `latitude=${userPosition.latitude}&longitude=${userPosition.longitude}`;
+			const fetchURL: RequestInfo = `${import.meta.env.VITE_LOCATION_API}${FETCH_PARAMS}`;
+			const fetchOptions: RequestInit = {
+				method: 'GET',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			};
+
+			const response: Response = await fetch(fetchURL, fetchOptions);
+			if (!response.ok) throw new Error('Fetching User Address Failed');
+
+			const data: UserFetchAddressResponseType = await response.json();
+			if (!data.countryName) throw new Error('Invalid Response: Country Name Not Found');
+
+			const userAddressObject: UserAddressObjectType = {
+				position: userPosition,
+				address: `${data.locality}, ${data.city} ${data.postcode}, ${data.countryName}`,
+			};
+
+			dispatch({
+				type: userTypes.USER_FETCH_ADDRESS_SUCCESS,
+				payload: userAddressObject,
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				dispatch({
+					type: userTypes.USER_FETCH_ADDRESS_ERROR,
+					payload: error.message,
+				});
+			}
+		}
+	};
+
+	return ThunkMiddleware;
 }
